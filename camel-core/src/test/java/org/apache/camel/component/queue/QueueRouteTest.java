e829d2684a15c5df7139531a2f3d24cad86d183e
Robert Davies <rajdavies@apache.org>
2007-03-21 19:19:01 +0000

    changed package
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@520974 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 5f875a33b20c3f221e747f332e93d20a82676f76

--- camel-core/src/test/java/org/apache/camel/component/queue/QueueRouteTest.java
+++ camel-core/src/test/java/org/apache/camel/component/queue/QueueRouteTest.java
@@ -4,13 +4,15 @@ comment|/**  *  * Licensed to the Apache Software Foundation (ASF) under one or 
 end_comment
 
 begin_package
-DECL|package|org.apache.camel.queue
+DECL|package|org.apache.camel.component.queue
 package|package
 name|org
 operator|.
 name|apache
 operator|.
 name|camel
+operator|.
+name|component
 operator|.
 name|queue
 package|;
