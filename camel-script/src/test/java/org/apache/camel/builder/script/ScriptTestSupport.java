bdb18fdc0bd8533f38495d8f4e5cc11d9cd5ceea
James Strachan <jstrachan@apache.org>
2007-03-23 11:20:01 +0000

    added a script module for JSR 223 support; also tidied up the spring dependency pom stuff
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@521675 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 278a2a3dbedfb7d57a77ddedda40027bacd9cc76

---
+++ camel-script/src/test/java/org/apache/camel/builder/script/ScriptTestSupport.java
@@ -0,0 +1,47 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/**  *  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+
+begin_package
+DECL|package|org.apache.camel.builder.script
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|camel
+operator|.
+name|builder
+operator|.
+name|script
+package|;
+end_package
+
+begin_import
+import|import
+name|junit
+operator|.
+name|framework
+operator|.
+name|TestCase
+import|;
+end_import
+
+begin_comment
+comment|/**  * @version $Revision$  */
+end_comment
+
+begin_class
+DECL|class|ScriptTestSupport
+specifier|public
+specifier|abstract
+class|class
+name|ScriptTestSupport
+extends|extends
+name|TestCase
+block|{ }
+end_class
+
+end_unit
+
