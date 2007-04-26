100b91de968fa1ca6b9e6c93573c7f2ea1f96874
James Strachan <jstrachan@apache.org>
2007-04-26 08:24:06 +0000

    added some load balancer helper classes together with the basics of a processor component for turning a Processor into an Endpoint (so to turn say a transformer into an endpoint)
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@532654 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 826a263be5ca26b0b5fdcafa875262c6853179f2

--- camel-core/src/main/java/org/apache/camel/CamelContext.java
+++ camel-core/src/main/java/org/apache/camel/CamelContext.java
@@ -233,6 +233,31 @@ name|Injector
 name|getInjector
 parameter_list|()
 function_decl|;
+comment|/**      * Adds the endpoint to the context using the given URI      *      * @param uri the URI to be used to resolve this endpoint      * @param endpoint the endpoint to be added to the context      * @return the old endpoint that was previously registered to the context if there was      * already an endpoint for that URI      * @throws Exception if the new endpoint could not be started or the old endpoint could not be stopped      */
+DECL|method|addEndpoint (String uri, Endpoint endpoint)
+name|Endpoint
+name|addEndpoint
+parameter_list|(
+name|String
+name|uri
+parameter_list|,
+name|Endpoint
+name|endpoint
+parameter_list|)
+throws|throws
+name|Exception
+function_decl|;
+comment|/**      * Removes the endpoint with the given URI      *      * @param uri the URI to be used to remove      * @return the endpoint that was removed or null if there is no endpoint for this URI      * @throws Exception if endpoint could not be stopped      */
+DECL|method|removeEndpoint (String uri)
+name|Endpoint
+name|removeEndpoint
+parameter_list|(
+name|String
+name|uri
+parameter_list|)
+throws|throws
+name|Exception
+function_decl|;
 block|}
 end_interface
 
