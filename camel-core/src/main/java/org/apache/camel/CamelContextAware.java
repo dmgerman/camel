b5c0b63d7f382dec5731da6136b13335b5597eac
James Strachan <jstrachan@apache.org>
2007-07-08 06:10:04 +0000

    added an initial implementation of CAMEL-68
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@554322 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: d14c4d3117f22e5256fa39961bdfce23a47be327

---
+++ camel-core/src/main/java/org/apache/camel/CamelContextAware.java
@@ -0,0 +1,41 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/*  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+
+begin_package
+DECL|package|org.apache.camel
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|camel
+package|;
+end_package
+
+begin_comment
+comment|/**  * An interface to represent an object which wishes to be injected with  * a {@link CamelContext} such as when working with Spring or Guice  *  * @version $Revision: $  */
+end_comment
+
+begin_interface
+DECL|interface|CamelContextAware
+specifier|public
+interface|interface
+name|CamelContextAware
+block|{
+comment|/**      * Injects the {@link CamelContext}      *      * @param camelContext      */
+DECL|method|setCamelContext (CamelContext camelContext)
+specifier|public
+name|void
+name|setCamelContext
+parameter_list|(
+name|CamelContext
+name|camelContext
+parameter_list|)
+function_decl|;
+block|}
+end_interface
+
+end_unit
+
