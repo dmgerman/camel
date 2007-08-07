d9b16abd67e84923c89a09a5a0891ceebfa0924d
James Strachan <jstrachan@apache.org>
2007-08-07 18:40:20 +0000

    migrated the Camel destination code from the camel project into ActiveMQ as it makes more sense to host it there - and avoids a circular dependency issue when releasing ActiveMQ 5.
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@563615 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: ce0728c268d957bdfff2a69a22448aa2abc0a45d

--- components/camel-activemq/src/test/java/org/apache/camel/jms/CamelJmsTest.java
+++ components/camel-activemq/src/test/java/org/apache/camel/jms/CamelJmsTest.java
@@ -506,7 +506,7 @@ return|return
 operator|new
 name|ClassPathXmlApplicationContext
 argument_list|(
-literal|"org/apache/camel/jms/spring.xml"
+literal|"org/apache/activemq/camel/spring.xml"
 argument_list|)
 return|;
 block|}
