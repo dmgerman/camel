7c8e75add86abdd44bea4089908876d03e7b9624
James Strachan <jstrachan@apache.org>
2007-09-14 09:58:50 +0000

    minor javadoc fix
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@575602 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 8a3a9d149a951ad86d5b6251b2c43c94010f2fbd

--- camel-core/src/main/java/org/apache/camel/AsyncProcessor.java
+++ camel-core/src/main/java/org/apache/camel/AsyncProcessor.java
@@ -26,7 +26,7 @@ name|AsyncProcessor
 extends|extends
 name|Processor
 block|{
-comment|/**      * Processes the message exchange.  Similar to {@see Processor.process}, but      * the caller supports having the exchange asynchronously processed.      *       * @param  The @{see AsyncCallback} will be invoked when the processing       *         of the exchange is completed. If the exchange is completed synchronously, then the       *         callback is also invoked synchronously.  The callback should therefore be careful of      *         starting recursive loop.      *               * @return true if the processing was completed synchronously.      */
+comment|/**      * Processes the message exchange.  Similar to {@see Processor.process}, but      * the caller supports having the exchange asynchronously processed.      *      * @param exchange the exchange to process      * @param  callback The @{see AsyncCallback} will be invoked when the processing      *         of the exchange is completed. If the exchange is completed synchronously, then the       *         callback is also invoked synchronously.  The callback should therefore be careful of      *         starting recursive loop.      *               * @return true if the processing was completed synchronously.      */
 DECL|method|process (Exchange exchange, AsyncCallback callback)
 name|boolean
 name|process
