9ce9b795d24721b51e3d933b7495c107fe7252aa
James Strachan <jstrachan@apache.org>
2007-10-01 15:17:32 +0000

    fixed javadoc for CAMEL-161
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@580990 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 093b1f8dc81290ded4b77ca679f8d346366cb096

--- camel-core/src/main/java/org/apache/camel/Processor.java
+++ camel-core/src/main/java/org/apache/camel/Processor.java
@@ -15,7 +15,7 @@ package|;
 end_package
 
 begin_comment
-comment|/**  * A<a href="http://activemq.apache.org/camel/processor.html">processor</a> is  * used to implement the<a  * href="http://activemq.apache.org/camel/event-driven-consumer.html">Event  * Driven Consumer</a> and<a  * href="http://activemq.apache.org/camel/message-transformer.html">Message  * Transformer</a> patterns and to process message exchanges.  *   * @version $Revision$  */
+comment|/**  * A<a href="http://activemq.apache.org/camel/processor.html">processor</a> is  * used to implement the<a  * href="http://activemq.apache.org/camel/event-driven-consumer.html">Event  * Driven Consumer</a> and<a  * href="http://activemq.apache.org/camel/message-translator.html">Message  * Translater</a> patterns and to process message exchanges.  *   * @version $Revision$  */
 end_comment
 
 begin_interface
