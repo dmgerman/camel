787e652b6d7dbb5f238cc0738aa335ed0d6d327c
Willem Ning Jiang <ningjiang@apache.org>
2008-01-28 08:37:55 +0000

    CAMEL-259 Added an example to show how to use camel transport
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@615759 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 6a0fea0ff04e420f97cabe3cd507e070544fa477

---
+++ examples/camel-example-cxf/src/main/java/org/apache/camel/example/camel/transport/CamelTransportExample.java
@@ -0,0 +1,106 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+
+begin_package
+DECL|package|org.apache.camel.example.camel.transport
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|camel
+operator|.
+name|example
+operator|.
+name|camel
+operator|.
+name|transport
+package|;
+end_package
+
+begin_class
+DECL|class|CamelTransportExample
+specifier|public
+class|class
+name|CamelTransportExample
+block|{
+DECL|method|CamelTransportExample ()
+specifier|private
+name|CamelTransportExample
+parameter_list|()
+block|{     }
+DECL|method|main (String args[])
+specifier|public
+specifier|static
+name|void
+name|main
+parameter_list|(
+name|String
+name|args
+index|[]
+parameter_list|)
+throws|throws
+name|Exception
+block|{
+comment|//Server server = new Server();
+try|try
+block|{
+comment|// setup the camel context for the camel transport
+comment|//server.prepare();
+comment|// start the endpoints
+comment|//server.start();
+comment|// set the client's service access point
+name|Client
+name|client
+init|=
+operator|new
+name|Client
+argument_list|(
+literal|"http://localhost:9090/GreeterContext/GreeterPort"
+argument_list|)
+decl_stmt|;
+comment|// invoking the services
+name|client
+operator|.
+name|invock
+argument_list|()
+expr_stmt|;
+name|Thread
+operator|.
+name|sleep
+argument_list|(
+literal|1000
+argument_list|)
+expr_stmt|;
+block|}
+catch|catch
+parameter_list|(
+name|Exception
+name|ex
+parameter_list|)
+block|{
+name|ex
+operator|.
+name|printStackTrace
+argument_list|()
+expr_stmt|;
+block|}
+finally|finally
+block|{
+comment|//server.stop();
+name|System
+operator|.
+name|exit
+argument_list|(
+literal|0
+argument_list|)
+expr_stmt|;
+block|}
+block|}
+block|}
+end_class
+
+end_unit
+
