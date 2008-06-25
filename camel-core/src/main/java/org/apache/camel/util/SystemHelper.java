b34a6d0e39d4affa042775afcbff38dcb95547ca
Willem Ning Jiang <ningjiang@apache.org>
2008-06-25 05:01:47 +0000

    Fixed some CS errors
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@671429 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 34bba6b89f2f0d456bfc78018a6ff6c03c83e908

--- camel-core/src/main/java/org/apache/camel/util/SystemHelper.java
+++ camel-core/src/main/java/org/apache/camel/util/SystemHelper.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/**  *  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -22,6 +22,20 @@ name|org
 operator|.
 name|apache
 operator|.
+name|camel
+operator|.
+name|converter
+operator|.
+name|ObjectConverter
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
 name|commons
 operator|.
 name|logging
@@ -44,20 +58,6 @@ name|LogFactory
 import|;
 end_import
 
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|camel
-operator|.
-name|converter
-operator|.
-name|ObjectConverter
-import|;
-end_import
-
 begin_comment
 comment|/**  * A helper class for various {@link System} related methods  *  * @version $Revision: 1.1 $  */
 end_comment
@@ -65,6 +65,7 @@ end_comment
 begin_class
 DECL|class|SystemHelper
 specifier|public
+specifier|final
 class|class
 name|SystemHelper
 block|{
@@ -85,6 +86,13 @@ operator|.
 name|class
 argument_list|)
 decl_stmt|;
+DECL|method|SystemHelper ()
+specifier|private
+name|SystemHelper
+parameter_list|()
+block|{
+comment|// Helper class
+block|}
 comment|/**      * Looks up the given system property name returning null if any exceptions occur      */
 DECL|method|getSystemProperty (String name)
 specifier|public
