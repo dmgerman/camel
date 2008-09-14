6c49a58495e69d357291ca31cafb0e7ed58f65a2
Willem Ning Jiang <ningjiang@apache.org>
2008-09-14 03:34:43 +0000

    Fixed the CS errors of camel-rest
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@695107 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 703e237a27d7a4a564460874f67a95586e1a5f85

--- components/camel-rest/src/test/java/org/apache/camel/rest/resources/TestSupport.java
+++ components/camel-rest/src/test/java/org/apache/camel/rest/resources/TestSupport.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/**  *  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -30,15 +30,17 @@ end_import
 
 begin_import
 import|import
-name|org
+name|com
 operator|.
-name|apache
+name|sun
 operator|.
-name|camel
+name|jersey
 operator|.
-name|rest
+name|api
 operator|.
-name|Main
+name|client
+operator|.
+name|Client
 import|;
 end_import
 
@@ -54,9 +56,7 @@ name|api
 operator|.
 name|client
 operator|.
-name|config
-operator|.
-name|DefaultClientConfig
+name|WebResource
 import|;
 end_import
 
@@ -90,23 +90,23 @@ name|api
 operator|.
 name|client
 operator|.
-name|Client
+name|config
+operator|.
+name|DefaultClientConfig
 import|;
 end_import
 
 begin_import
 import|import
-name|com
+name|org
 operator|.
-name|sun
+name|apache
 operator|.
-name|jersey
+name|camel
 operator|.
-name|api
+name|rest
 operator|.
-name|client
-operator|.
-name|WebResource
+name|Main
 import|;
 end_import
 
