0e21d4724bc4faaceeb1584e0d1814d38c19058f
Willem Ning Jiang <ningjiang@apache.org>
2008-12-03 01:06:47 +0000

    Changed the jetty server port to fix the permission error of the JmsHttpJmsTest in non-windows box
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@722708 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 21de596f7dac0f8b7fd3b2b70c9c59b59cd85f08

--- tests/camel-itest/src/test/java/org/apache/camel/itest/jms/JmsHttpJmsTest.java
+++ tests/camel-itest/src/test/java/org/apache/camel/itest/jms/JmsHttpJmsTest.java
@@ -250,7 +250,7 @@ argument_list|)
 operator|.
 name|to
 argument_list|(
-literal|"http://localhost/myservice"
+literal|"http://localhost:9000/myservice"
 argument_list|)
 operator|.
 name|to
@@ -260,7 +260,7 @@ argument_list|)
 expr_stmt|;
 name|from
 argument_list|(
-literal|"jetty:http://localhost/myservice"
+literal|"jetty:http://localhost:9000/myservice"
 argument_list|)
 operator|.
 name|transform
