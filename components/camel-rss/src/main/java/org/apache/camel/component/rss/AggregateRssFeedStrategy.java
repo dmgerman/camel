656d0bb3eeb80fd7072c53cea21b64313ab8d371
Jonathan Anstey <janstey@apache.org>
2008-12-09 17:59:47 +0000

    Fix CS errors
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@724800 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 6c61fdc0d3c94a587232d05cb930a3b18f93a94a

--- components/camel-rss/src/main/java/org/apache/camel/component/rss/AggregateRssFeedStrategy.java
+++ components/camel-rss/src/main/java/org/apache/camel/component/rss/AggregateRssFeedStrategy.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -20,22 +20,6 @@ end_package
 
 begin_import
 import|import
-name|com
-operator|.
-name|sun
-operator|.
-name|syndication
-operator|.
-name|feed
-operator|.
-name|synd
-operator|.
-name|SyndFeed
-import|;
-end_import
-
-begin_import
-import|import
 name|java
 operator|.
 name|util
@@ -51,6 +35,22 @@ operator|.
 name|util
 operator|.
 name|List
+import|;
+end_import
+
+begin_import
+import|import
+name|com
+operator|.
+name|sun
+operator|.
+name|syndication
+operator|.
+name|feed
+operator|.
+name|synd
+operator|.
+name|SyndFeed
 import|;
 end_import
 
@@ -118,12 +118,12 @@ name|AggregateRssFeedStrategy
 implements|implements
 name|AggregationStrategy
 block|{
-DECL|field|LOG
+DECL|field|log
 specifier|protected
 specifier|final
 specifier|transient
 name|Log
-name|LOG
+name|log
 init|=
 name|LogFactory
 operator|.
@@ -244,7 +244,7 @@ expr_stmt|;
 block|}
 else|else
 block|{
-name|LOG
+name|log
 operator|.
 name|debug
 argument_list|(
