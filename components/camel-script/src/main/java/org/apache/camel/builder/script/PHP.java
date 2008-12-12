2d0f7e69ce11176fce98e24110bc31739fc5749b
Willem Ning Jiang <ningjiang@apache.org>
2008-12-12 02:47:51 +0000

    Fixed the cs errors of components
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@725893 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 0744208acfcc3afb60aa7c4bae0c6c680cc4a2d4

--- components/camel-script/src/main/java/org/apache/camel/builder/script/PHP.java
+++ components/camel-script/src/main/java/org/apache/camel/builder/script/PHP.java
@@ -26,6 +26,18 @@ name|lang
 operator|.
 name|annotation
 operator|.
+name|Documented
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|lang
+operator|.
+name|annotation
+operator|.
 name|ElementType
 import|;
 end_import
@@ -63,18 +75,6 @@ operator|.
 name|annotation
 operator|.
 name|Target
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|lang
-operator|.
-name|annotation
-operator|.
-name|Documented
 import|;
 end_import
 
