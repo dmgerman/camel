a2fb892b8f3db173aa4013db772e857cd5643f0b
Claus Ibsen <davsclaus@apache.org>
2009-01-15 19:54:12 +0000

    Fixed checkstyle
    git-svn-id: https://svn.apache.org/repos/asf/activemq/camel/trunk@734789 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: bcf8fc66929cbe3777a52e0293ef9340d8150d92

--- camel-core/src/main/java/org/apache/camel/processor/interceptor/TraceInterceptor.java
+++ camel-core/src/main/java/org/apache/camel/processor/interceptor/TraceInterceptor.java
@@ -56,6 +56,18 @@ name|apache
 operator|.
 name|camel
 operator|.
+name|Endpoint
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|camel
+operator|.
 name|Exchange
 import|;
 end_import
@@ -81,18 +93,6 @@ operator|.
 name|camel
 operator|.
 name|Producer
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|camel
-operator|.
-name|Endpoint
 import|;
 end_import
 
--- camel-core/src/main/java/org/apache/camel/processor/interceptor/Tracer.java
+++ camel-core/src/main/java/org/apache/camel/processor/interceptor/Tracer.java
@@ -48,6 +48,18 @@ name|apache
 operator|.
 name|camel
 operator|.
+name|Endpoint
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|camel
+operator|.
 name|Predicate
 import|;
 end_import
@@ -61,18 +73,6 @@ operator|.
 name|camel
 operator|.
 name|Processor
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|camel
-operator|.
-name|Endpoint
 import|;
 end_import
 
