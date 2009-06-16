8566ce14c6616880d8f3f44c5ad62855602a8d9a
Willem Ning Jiang <ningjiang@apache.org>
2009-06-16 07:27:43 +0000

    CAMEL-1712 Upgraded the camel-csv JUnit version to 4.6
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@785100 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: beb419c116c4f880e5e04c3969b424c681984e7a

--- components/camel-csv/src/test/java/org/apache/camel/dataformat/csv/CsvRouteTest.java
+++ components/camel-csv/src/test/java/org/apache/camel/dataformat/csv/CsvRouteTest.java
@@ -72,18 +72,6 @@ name|apache
 operator|.
 name|camel
 operator|.
-name|ContextTestSupport
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|camel
-operator|.
 name|Exchange
 import|;
 end_import
@@ -127,6 +115,22 @@ operator|.
 name|mock
 operator|.
 name|MockEndpoint
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|camel
+operator|.
+name|test
+operator|.
+name|junit4
+operator|.
+name|CamelTestSupport
 import|;
 end_import
 
@@ -190,6 +194,16 @@ name|LogFactory
 import|;
 end_import
 
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
+name|Test
+import|;
+end_import
+
 begin_comment
 comment|/**  * @version $Revision$  */
 end_comment
@@ -200,7 +214,7 @@ specifier|public
 class|class
 name|CsvRouteTest
 extends|extends
-name|ContextTestSupport
+name|CamelTestSupport
 block|{
 DECL|field|LOG
 specifier|private
@@ -219,6 +233,8 @@ operator|.
 name|class
 argument_list|)
 decl_stmt|;
+annotation|@
+name|Test
 DECL|method|testSendMessage ()
 specifier|public
 name|void
@@ -371,6 +387,8 @@ argument_list|)
 expr_stmt|;
 block|}
 block|}
+annotation|@
+name|Test
 DECL|method|testMultipleMessages ()
 specifier|public
 name|void
@@ -619,6 +637,8 @@ argument_list|)
 expr_stmt|;
 block|}
 block|}
+annotation|@
+name|Test
 DECL|method|testPresetConfig ()
 specifier|public
 name|void
@@ -836,6 +856,8 @@ argument_list|()
 argument_list|)
 expr_stmt|;
 block|}
+annotation|@
+name|Test
 DECL|method|testUnMarshal ()
 specifier|public
 name|void
