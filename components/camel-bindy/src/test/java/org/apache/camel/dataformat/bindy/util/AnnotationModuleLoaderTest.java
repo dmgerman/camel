7f7fc3146938e381cdc3df950abed21900f53662
Willem Ning Jiang <ningjiang@apache.org>
2009-06-16 07:28:43 +0000

    CAMEL-1712 Upgraded the camel-bindy JUnit version to 4.6
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@785101 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 5efbd7297b593d6dbbdef2cc1f9d2319d74f251f

--- components/camel-bindy/src/test/java/org/apache/camel/dataformat/bindy/util/AnnotationModuleLoaderTest.java
+++ components/camel-bindy/src/test/java/org/apache/camel/dataformat/bindy/util/AnnotationModuleLoaderTest.java
@@ -32,16 +32,6 @@ end_import
 
 begin_import
 import|import
-name|junit
-operator|.
-name|framework
-operator|.
-name|Assert
-import|;
-end_import
-
-begin_import
-import|import
 name|org
 operator|.
 name|apache
@@ -95,6 +85,16 @@ operator|.
 name|impl
 operator|.
 name|DefaultPackageScanClassResolver
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
+name|Assert
 import|;
 end_import
 
