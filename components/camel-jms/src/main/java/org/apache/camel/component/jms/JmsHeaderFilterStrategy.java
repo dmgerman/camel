5a93801ff1e1635abbbdc55a7303aa3da3397e5a
Claus Ibsen <davsclaus@apache.org>
2009-07-02 09:33:10 +0000

    CAMEL-1783: polished a bit.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@790520 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 61beca0cef1faecefacb00af9d1c1cde9525a0b6

--- components/camel-jms/src/main/java/org/apache/camel/component/jms/JmsHeaderFilterStrategy.java
+++ components/camel-jms/src/main/java/org/apache/camel/component/jms/JmsHeaderFilterStrategy.java
@@ -32,20 +32,6 @@ name|DefaultHeaderFilterStrategy
 import|;
 end_import
 
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|camel
-operator|.
-name|util
-operator|.
-name|ObjectHelper
-import|;
-end_import
-
 begin_comment
 comment|/**  * @version $Revision$  */
 end_comment
