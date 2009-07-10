d6b2a8892c9c1ec9f723cab83c05fa5482fa4f8c
Claus Ibsen <davsclaus@apache.org>
2009-07-10 05:17:53 +0000

    added labels so they appear in tracer
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@792806 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: ef666a7b30a380872b924557a8b4180e0de88066

--- camel-core/src/main/java/org/apache/camel/model/SetHeaderDefinition.java
+++ camel-core/src/main/java/org/apache/camel/model/SetHeaderDefinition.java
@@ -332,6 +332,23 @@ return|;
 block|}
 annotation|@
 name|Override
+DECL|method|getLabel ()
+specifier|public
+name|String
+name|getLabel
+parameter_list|()
+block|{
+return|return
+literal|"setHeader["
+operator|+
+name|getHeaderName
+argument_list|()
+operator|+
+literal|"]"
+return|;
+block|}
+annotation|@
+name|Override
 DECL|method|createProcessor (RouteContext routeContext)
 specifier|public
 name|Processor
