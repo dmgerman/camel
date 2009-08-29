5f9d4d3e841a08316ff938885c9de250f1c87526
Willem Ning Jiang <ningjiang@apache.org>
2009-08-29 07:38:12 +0000

    Fixed the CS error of CamelContextFactoryBean
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@809091 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: e2c0a70ebaa622299d3e8bfae039b8bc38e1083c

--- components/camel-spring/src/main/java/org/apache/camel/spring/CamelContextFactoryBean.java
+++ components/camel-spring/src/main/java/org/apache/camel/spring/CamelContextFactoryBean.java
@@ -58,16 +58,6 @@ end_import
 
 begin_import
 import|import
-name|java
-operator|.
-name|util
-operator|.
-name|HashMap
-import|;
-end_import
-
-begin_import
-import|import
 name|javax
 operator|.
 name|xml
