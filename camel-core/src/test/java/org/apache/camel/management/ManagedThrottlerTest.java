0f3cef6f03d578553d5e4b2045c863056ac9c250
Willem Ning Jiang <ningjiang@apache.org>
2009-09-11 01:23:36 +0000

    Try to fixed the ManagedThrottlerTest failure on slower box
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@813645 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 1fca51b7de16efe9dae84bb67a554b388b9af4d8

--- camel-core/src/test/java/org/apache/camel/management/ManagedThrottlerTest.java
+++ camel-core/src/test/java/org/apache/camel/management/ManagedThrottlerTest.java
@@ -294,13 +294,13 @@ argument_list|)
 decl_stmt|;
 name|assertTrue
 argument_list|(
-literal|"Should take at most 1.5 sec: was "
+literal|"Should take at most 2.0 sec: was "
 operator|+
 name|total
 argument_list|,
 name|total
 operator|<
-literal|1500
+literal|2000
 argument_list|)
 expr_stmt|;
 comment|// change the throttler using JMX
