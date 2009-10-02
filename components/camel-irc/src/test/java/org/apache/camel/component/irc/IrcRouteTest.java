d78e1f96879e0d522cde9b0f520c7d1b62da5b97
Claus Ibsen <davsclaus@apache.org>
2009-10-02 06:42:36 +0000

    Disabled test that requries online and manual testing.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@820901 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 5953acc6a64ff7355b5cb830b9fc22f28364953f

--- components/camel-irc/src/test/java/org/apache/camel/component/irc/IrcRouteTest.java
+++ components/camel-irc/src/test/java/org/apache/camel/component/irc/IrcRouteTest.java
@@ -104,6 +104,16 @@ name|org
 operator|.
 name|junit
 operator|.
+name|Ignore
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
 name|Test
 import|;
 end_import
@@ -144,6 +154,8 @@ specifier|private
 name|boolean
 name|sentMessages
 decl_stmt|;
+annotation|@
+name|Ignore
 annotation|@
 name|Test
 DECL|method|testIrcMessages ()
