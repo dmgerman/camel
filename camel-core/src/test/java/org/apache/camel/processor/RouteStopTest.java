d0913e340de41057980351d701ed9fa634eea7e1
Claus Ibsen <davsclaus@apache.org>
2009-10-17 06:22:01 +0000

    added code snippets for wiki documentation
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@826180 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: e7b1a76c566c6118da84395321def317fcabc8e0

--- camel-core/src/test/java/org/apache/camel/processor/RouteStopTest.java
+++ camel-core/src/test/java/org/apache/camel/processor/RouteStopTest.java
@@ -315,6 +315,7 @@ parameter_list|()
 throws|throws
 name|Exception
 block|{
+comment|// START SNIPPET: e1
 name|from
 argument_list|(
 literal|"direct:start"
@@ -374,6 +375,7 @@ argument_list|(
 literal|"mock:result"
 argument_list|)
 expr_stmt|;
+comment|// END SNIPPET: e1
 name|from
 argument_list|(
 literal|"direct:foo"
