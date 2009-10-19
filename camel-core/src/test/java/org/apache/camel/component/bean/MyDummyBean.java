68e4e903612ed348e6521ceb38aec3531a8ecb12
Claus Ibsen <davsclaus@apache.org>
2009-10-19 06:37:30 +0000

    CAMEL-2074, CAMEL-2075, CAMEL-2071: Bean component does not handle select methods using the method name given (if any given). Also getter/setter is skipped if Camel have to pick one itself.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@826591 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: cf66e15f251b5cf052cea545b5ab20164c93458c

---
+++ camel-core/src/test/java/org/apache/camel/component/bean/MyDummyBean.java
@@ -0,0 +1,101 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+
+begin_package
+DECL|package|org.apache.camel.component.bean
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|camel
+operator|.
+name|component
+operator|.
+name|bean
+package|;
+end_package
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|camel
+operator|.
+name|Handler
+import|;
+end_import
+
+begin_comment
+comment|/**  * @version $Revision$  */
+end_comment
+
+begin_class
+DECL|class|MyDummyBean
+specifier|public
+class|class
+name|MyDummyBean
+block|{
+DECL|method|hello (String s)
+specifier|public
+name|String
+name|hello
+parameter_list|(
+name|String
+name|s
+parameter_list|)
+block|{
+return|return
+literal|"Hello "
+operator|+
+name|s
+return|;
+block|}
+DECL|method|hello (String s, String t)
+specifier|public
+name|String
+name|hello
+parameter_list|(
+name|String
+name|s
+parameter_list|,
+name|String
+name|t
+parameter_list|)
+block|{
+return|return
+literal|"Hello "
+operator|+
+name|s
+operator|+
+literal|" and "
+operator|+
+name|t
+return|;
+block|}
+annotation|@
+name|Handler
+DECL|method|bye (String s)
+specifier|public
+name|String
+name|bye
+parameter_list|(
+name|String
+name|s
+parameter_list|)
+block|{
+return|return
+literal|"Bye "
+operator|+
+name|s
+return|;
+block|}
+block|}
+end_class
+
+end_unit
+
