0adf8c4302bc2c163114d59de84e99ffe9ec590d
Willem Ning Jiang <ningjiang@apache.org>
2009-12-10 11:49:29 +0000

    Fixed CS error of SimpleLanguage and EtlRoutes
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@889209 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 2051aa91ec7ef477ca7297d4a22a4738caa4dd97

--- examples/camel-example-etl/src/main/java/org/apache/camel/example/etl/EtlRoutes.java
+++ examples/camel-example-etl/src/main/java/org/apache/camel/example/etl/EtlRoutes.java
@@ -45,34 +45,6 @@ import|;
 end_import
 
 begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|camel
-operator|.
-name|LoggingLevel
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|camel
-operator|.
-name|processor
-operator|.
-name|interceptor
-operator|.
-name|Tracer
-import|;
-end_import
-
-begin_import
 import|import static
 name|org
 operator|.
@@ -114,7 +86,6 @@ parameter_list|()
 throws|throws
 name|Exception
 block|{
-comment|/** 	Tracer tracer = new Tracer();         tracer.setLogLevel(LoggingLevel.FATAL);         tracer.setLogName("org.apache.camel.example.etl");         tracer.setLogStackTrace(true);         tracer.setTraceExceptions(true);         **/
 name|from
 argument_list|(
 literal|"file:src/data?noop=true"
