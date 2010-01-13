8a487f45cf6510eac141f3ae82c605e056e55e70
Claus Ibsen <davsclaus@apache.org>
2010-01-13 06:15:06 +0000

    Fixed unit test
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@898655 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 298964dc444ecebcd230db6f6547bf62c9019a95

--- camel-core/src/test/java/org/apache/camel/management/PublishEventNotifierTest.java
+++ camel-core/src/test/java/org/apache/camel/management/PublishEventNotifierTest.java
@@ -160,7 +160,7 @@ argument_list|)
 operator|.
 name|expectedMessageCount
 argument_list|(
-literal|5
+literal|6
 argument_list|)
 expr_stmt|;
 name|template
@@ -191,7 +191,7 @@ argument_list|)
 operator|.
 name|expectedMessageCount
 argument_list|(
-literal|5
+literal|6
 argument_list|)
 expr_stmt|;
 try|try
