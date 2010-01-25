7a9009535b6d0ca076e417ff4478baeb9803e1f6
Willem Ning Jiang <ningjiang@apache.org>
2010-01-25 03:00:51 +0000

    CAMEL-2397 copy the inMessage hears to the outMessage in CxfProducer
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@902688 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 0df726cad29d941c02a8bf36b49007c852e292fd

--- components/camel-cxf/src/main/java/org/apache/camel/component/cxf/CxfProducer.java
+++ components/camel-cxf/src/main/java/org/apache/camel/component/cxf/CxfProducer.java
@@ -778,6 +778,26 @@ name|isOneWay
 argument_list|()
 condition|)
 block|{
+comment|// copy the InMessage header to OutMessage header
+name|camelExchange
+operator|.
+name|getOut
+argument_list|()
+operator|.
+name|getHeaders
+argument_list|()
+operator|.
+name|putAll
+argument_list|(
+name|camelExchange
+operator|.
+name|getIn
+argument_list|()
+operator|.
+name|getHeaders
+argument_list|()
+argument_list|)
+expr_stmt|;
 name|binding
 operator|.
 name|populateExchangeFromCxfResponse
