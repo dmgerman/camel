270bd01e91e3b9031db43e5430e904564b52bea4
Hadrian Zbarcea <hadrian@apache.org>
2010-02-03 04:50:52 +0000

    Preparing for release... checkstyle fixes
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@905886 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: bf235d3270ead526750fda5a68ba0ddb9c4281ed

--- camel-core/src/test/java/org/apache/camel/component/file/FileConsumerAbsolutePathDefaultMoveTest.java
+++ camel-core/src/test/java/org/apache/camel/component/file/FileConsumerAbsolutePathDefaultMoveTest.java
@@ -20,6 +20,16 @@ end_package
 
 begin_import
 import|import
+name|java
+operator|.
+name|io
+operator|.
+name|File
+import|;
+end_import
+
+begin_import
+import|import
 name|org
 operator|.
 name|apache
@@ -69,16 +79,6 @@ operator|.
 name|mock
 operator|.
 name|MockEndpoint
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|io
-operator|.
-name|File
 import|;
 end_import
 
