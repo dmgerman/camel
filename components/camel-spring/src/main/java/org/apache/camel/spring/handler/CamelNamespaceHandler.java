50a1aa9019c9129cb7617b12d2180140db35ca6e
Willem Ning Jiang <ningjiang@apache.org>
2010-07-14 10:00:35 +0000

    CAMEL-2941 changed the osgi detection log as Claus suggested
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@963996 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 8dcf386ed4fd959c03b73f59b2bc35e836257686

--- components/camel-spring/src/main/java/org/apache/camel/spring/handler/CamelNamespaceHandler.java
+++ components/camel-spring/src/main/java/org/apache/camel/spring/handler/CamelNamespaceHandler.java
@@ -1016,7 +1016,7 @@ name|LOG
 operator|.
 name|info
 argument_list|(
-literal|"Found the OSGi environment, deloying the camel context into OSGi container"
+literal|"OSGi environment detected."
 argument_list|)
 expr_stmt|;
 block|}
@@ -1026,7 +1026,7 @@ name|LOG
 operator|.
 name|info
 argument_list|(
-literal|"Cannot find the OSGi environment, deploying the camel context into stand alone JVM"
+literal|"OSGi environment not detected."
 argument_list|)
 expr_stmt|;
 block|}
