5c3e4de95328a4ba00cb54a02e87bda3e03e58da
Jonathan Anstey <janstey@apache.org>
2010-09-28 18:38:54 +0000

    checkstyle fixes
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1002314 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 83b710449218c6d04501edcdb3527385e8bddd90

--- components/camel-spring/src/test/java/org/apache/camel/spring/placeholder/SimpleLanguageWithSprinPropertyPlaceholderRouteTest.java
+++ components/camel-spring/src/test/java/org/apache/camel/spring/placeholder/SimpleLanguageWithSprinPropertyPlaceholderRouteTest.java
@@ -19,18 +19,6 @@ package|;
 end_package
 
 begin_import
-import|import static
-name|org
-operator|.
-name|junit
-operator|.
-name|Assert
-operator|.
-name|assertTrue
-import|;
-end_import
-
-begin_import
 import|import
 name|java
 operator|.
@@ -159,6 +147,18 @@ operator|.
 name|junit4
 operator|.
 name|AbstractJUnit4SpringContextTests
+import|;
+end_import
+
+begin_import
+import|import static
+name|org
+operator|.
+name|junit
+operator|.
+name|Assert
+operator|.
+name|assertTrue
 import|;
 end_import
 
