595454383dfb49839f9dc5d3ea01d86bec657ff9
Claus Ibsen <davsclaus@apache.org>
2010-12-08 08:51:32 +0000

    Added exec test using quoted arguments.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1043337 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 69b4627267a58abf0578aaf42c46cf0b190ef550

--- components/camel-exec/src/test/java/org/apache/camel/component/exec/ExecTestUtils.java
+++ components/camel-exec/src/test/java/org/apache/camel/component/exec/ExecTestUtils.java
@@ -202,9 +202,6 @@ block|{
 name|String
 name|javaHome
 init|=
-operator|(
-name|String
-operator|)
 name|System
 operator|.
 name|getenv
@@ -219,6 +216,15 @@ operator|==
 literal|null
 condition|)
 block|{
+name|System
+operator|.
+name|out
+operator|.
+name|println
+argument_list|(
+literal|"The Exec component tests will fail, because the environment variable JAVA_HOME is not set!"
+argument_list|)
+expr_stmt|;
 throw|throw
 operator|new
 name|IllegalStateException
