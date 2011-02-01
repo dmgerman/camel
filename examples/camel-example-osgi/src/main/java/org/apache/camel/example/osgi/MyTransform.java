2abbe5a02b866d4c9475958466d864b9bdad9498
Claus Ibsen <davsclaus@apache.org>
2011-02-01 09:49:27 +0000

    CAMEL-3605: Switching to slf4j. Migrated tests, examples and tooling.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1065963 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 8dbe547c1ff18ad01b75caded45fdd1242005bf1

--- examples/camel-example-osgi/src/main/java/org/apache/camel/example/osgi/MyTransform.java
+++ examples/camel-example-osgi/src/main/java/org/apache/camel/example/osgi/MyTransform.java
@@ -32,13 +32,9 @@ begin_import
 import|import
 name|org
 operator|.
-name|apache
+name|slf4j
 operator|.
-name|commons
-operator|.
-name|logging
-operator|.
-name|Log
+name|Logger
 import|;
 end_import
 
@@ -46,13 +42,9 @@ begin_import
 import|import
 name|org
 operator|.
-name|apache
+name|slf4j
 operator|.
-name|commons
-operator|.
-name|logging
-operator|.
-name|LogFactory
+name|LoggerFactory
 import|;
 end_import
 
@@ -71,12 +63,12 @@ specifier|private
 specifier|static
 specifier|final
 specifier|transient
-name|Log
+name|Logger
 name|LOG
 init|=
-name|LogFactory
+name|LoggerFactory
 operator|.
-name|getLog
+name|getLogger
 argument_list|(
 name|MyTransform
 operator|.
--- tests/camel-itest-karaf/src/test/java/org/apache/camel/itest/karaf/AbstractFeatureTest.java
+++ tests/camel-itest-karaf/src/test/java/org/apache/camel/itest/karaf/AbstractFeatureTest.java
@@ -76,13 +76,9 @@ begin_import
 import|import
 name|org
 operator|.
-name|apache
+name|slf4j
 operator|.
-name|commons
-operator|.
-name|logging
-operator|.
-name|Log
+name|Logger
 import|;
 end_import
 
@@ -90,13 +86,9 @@ begin_import
 import|import
 name|org
 operator|.
-name|apache
+name|slf4j
 operator|.
-name|commons
-operator|.
-name|logging
-operator|.
-name|LogFactory
+name|LoggerFactory
 import|;
 end_import
 
@@ -323,12 +315,12 @@ DECL|field|log
 specifier|protected
 specifier|final
 specifier|transient
-name|Log
+name|Logger
 name|log
 init|=
-name|LogFactory
+name|LoggerFactory
 operator|.
-name|getLog
+name|getLogger
 argument_list|(
 name|getClass
 argument_list|()
