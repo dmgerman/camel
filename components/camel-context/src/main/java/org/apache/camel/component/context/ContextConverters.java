4a324607379e5cad4ed155adc4e22c5bd21c0ad8
Claus Ibsen <davsclaus@apache.org>
2011-02-11 06:00:51 +0000

    Fixed CS. Polished.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1069678 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 69323678e053ef4c4a3d5f03fe96cc13cc2c0ddc

--- components/camel-context/src/main/java/org/apache/camel/component/context/ContextConverters.java
+++ components/camel-context/src/main/java/org/apache/camel/component/context/ContextConverters.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/**  *  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -43,7 +43,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * Converts from a CamelContext to a ContextComponent so we can easily refer to external camel contexts in the Registry  * as a Component  */
+comment|/**  * Converts from a {@link CamelContext} to a {@link LocalContextComponent} so we can easily refer to  * external {@link CamelContext}s in the {@link org.apache.camel.spi.Registry} as a {@link org.apache.camel.Component}  */
 end_comment
 
 begin_class
@@ -51,9 +51,15 @@ annotation|@
 name|Converter
 DECL|class|ContextConverters
 specifier|public
+specifier|final
 class|class
 name|ContextConverters
 block|{
+DECL|method|ContextConverters ()
+specifier|private
+name|ContextConverters
+parameter_list|()
+block|{     }
 annotation|@
 name|Converter
 DECL|method|toComponent (CamelContext localContext)
--- components/camel-context/src/main/java/org/apache/camel/component/context/QualifiedContextComponent.java
+++ components/camel-context/src/main/java/org/apache/camel/component/context/QualifiedContextComponent.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/**  *  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -17,6 +17,16 @@ operator|.
 name|context
 package|;
 end_package
+
+begin_import
+import|import
+name|java
+operator|.
+name|util
+operator|.
+name|Map
+import|;
+end_import
 
 begin_import
 import|import
@@ -90,18 +100,8 @@ name|LoggerFactory
 import|;
 end_import
 
-begin_import
-import|import
-name|java
-operator|.
-name|util
-operator|.
-name|Map
-import|;
-end_import
-
 begin_comment
-comment|/**  * Supports the explicit and verbose URIs of the form<b>context:camelContextId:someEndpoint</b> to access  * a local endpoint inside an external CamelContext.  *<p/>  * Typically there's no need to use this level of verbosity, you can just use<b>camelContextId:someEndpoint</b>  */
+comment|/**  * Supports the explicit and verbose URIs of the form<tt>context:camelContextId:someEndpoint</tt> to access  * a local endpoint inside an external {@link org.apache.camel.CamelContext}.  *<p/>  * Typically there's no need to use this level of verbosity, you can just use<tt>camelContextId:someEndpoint</tt>  */
 end_comment
 
 begin_class
@@ -212,6 +212,14 @@ operator|!=
 literal|null
 condition|)
 block|{
+if|if
+condition|(
+name|LOG
+operator|.
+name|isDebugEnabled
+argument_list|()
+condition|)
+block|{
 name|LOG
 operator|.
 name|debug
@@ -225,6 +233,7 @@ operator|+
 name|component
 argument_list|)
 expr_stmt|;
+block|}
 return|return
 name|component
 operator|.
