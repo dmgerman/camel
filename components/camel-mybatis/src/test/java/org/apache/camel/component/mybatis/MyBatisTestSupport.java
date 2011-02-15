bc834a3bc81c19d8ca238e3263ad748808491294
Claus Ibsen <davsclaus@apache.org>
2011-02-15 08:13:10 +0000

    CAMEL-2954: added consumer to mybatis component.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1070801 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 26895b28c50d7d48c2f58bca5fb5f4676a69772f

--- components/camel-mybatis/src/test/java/org/apache/camel/component/mybatis/MyBatisTestSupport.java
+++ components/camel-mybatis/src/test/java/org/apache/camel/component/mybatis/MyBatisTestSupport.java
@@ -87,6 +87,16 @@ name|MyBatisTestSupport
 extends|extends
 name|CamelTestSupport
 block|{
+DECL|method|createTestData ()
+specifier|protected
+name|boolean
+name|createTestData
+parameter_list|()
+block|{
+return|return
+literal|true
+return|;
+block|}
 annotation|@
 name|Override
 annotation|@
@@ -131,6 +141,12 @@ operator|.
 name|close
 argument_list|()
 expr_stmt|;
+if|if
+condition|(
+name|createTestData
+argument_list|()
+condition|)
+block|{
 name|Account
 name|account
 init|=
@@ -218,6 +234,7 @@ argument_list|,
 name|account
 argument_list|)
 expr_stmt|;
+block|}
 block|}
 annotation|@
 name|Override
