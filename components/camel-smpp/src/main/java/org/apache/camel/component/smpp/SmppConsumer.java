477a9b87e5792a4d92b2f04115579ddea5ebe559
Christian Mueller <cmueller@apache.org>
2011-02-28 18:41:22 +0000

    CAMEL-3650: SMSC initiated unbind spawns exponential amounts of reconnect threads
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1075473 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 57ee2ed303e6fa5f30ff8b06ec376e1a45169664

--- components/camel-smpp/src/main/java/org/apache/camel/component/smpp/SmppConsumer.java
+++ components/camel-smpp/src/main/java/org/apache/camel/component/smpp/SmppConsumer.java
@@ -1073,11 +1073,45 @@ condition|)
 block|{
 name|session
 operator|.
+name|removeSessionStateListener
+argument_list|(
+name|this
+operator|.
+name|sessionStateListener
+argument_list|)
+expr_stmt|;
+comment|// remove this hack after http://code.google.com/p/jsmpp/issues/detail?id=93 is fixed
+try|try
+block|{
+name|Thread
+operator|.
+name|sleep
+argument_list|(
+literal|1000
+argument_list|)
+expr_stmt|;
+name|session
+operator|.
 name|unbindAndClose
 argument_list|()
 expr_stmt|;
-comment|// throws the java.util.ConcurrentModificationException
-comment|//session.removeSessionStateListener(this.sessionStateListener);
+block|}
+catch|catch
+parameter_list|(
+name|Exception
+name|e
+parameter_list|)
+block|{
+name|LOG
+operator|.
+name|warn
+argument_list|(
+literal|"Could not close session "
+operator|+
+name|session
+argument_list|)
+expr_stmt|;
+block|}
 name|session
 operator|=
 literal|null
