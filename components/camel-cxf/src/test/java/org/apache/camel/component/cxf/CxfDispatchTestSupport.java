a77fa40eab169627ed157040e2ddae571dadeb6a
Willem Ning Jiang <ningjiang@apache.org>
2011-03-31 07:23:21 +0000

    Fixed the CS errors of camel-cxf
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1087204 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: bfd6fad9cfaaed5044e25b824ca195d18de5c512

--- components/camel-cxf/src/test/java/org/apache/camel/component/cxf/CxfDispatchTestSupport.java
+++ components/camel-cxf/src/test/java/org/apache/camel/component/cxf/CxfDispatchTestSupport.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -82,26 +82,6 @@ begin_import
 import|import
 name|org
 operator|.
-name|junit
-operator|.
-name|AfterClass
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|junit
-operator|.
-name|BeforeClass
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
 name|apache
 operator|.
 name|camel
@@ -126,6 +106,26 @@ name|GreeterImpl
 import|;
 end_import
 
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
+name|AfterClass
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
+name|BeforeClass
+import|;
+end_import
+
 begin_comment
 comment|/**  * Base class for testing arbitrary payload  */
 end_comment
@@ -144,12 +144,6 @@ specifier|protected
 specifier|static
 name|Endpoint
 name|endpoint
-decl_stmt|;
-DECL|field|documentBuilderFactory
-specifier|private
-specifier|static
-name|DocumentBuilderFactory
-name|documentBuilderFactory
 decl_stmt|;
 DECL|field|DISPATCH_NS
 specifier|protected
@@ -221,6 +215,12 @@ operator|+
 name|PAYLOAD_ONEWAY_TEMPLATE
 operator|+
 literal|"</soap:Body></soap:Envelope>"
+decl_stmt|;
+DECL|field|documentBuilderFactory
+specifier|private
+specifier|static
+name|DocumentBuilderFactory
+name|documentBuilderFactory
 decl_stmt|;
 annotation|@
 name|Override
