3db57f11c1cb30d63b6ed3c39c23455c35302b45
Claus Ibsen <davsclaus@apache.org>
2011-05-21 14:36:52 +0000

    Ignore test that requires online internet access. Fixed other test.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1125714 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: bea28ce16d3365bdc9384e113fefa1a99ad46f42

--- tests/camel-itest/src/test/java/org/apache/camel/itest/http/HttpEndpointTest.java
+++ tests/camel-itest/src/test/java/org/apache/camel/itest/http/HttpEndpointTest.java
@@ -76,6 +76,16 @@ name|org
 operator|.
 name|junit
 operator|.
+name|Ignore
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
 name|Test
 import|;
 end_import
@@ -129,6 +139,11 @@ end_import
 begin_class
 annotation|@
 name|ContextConfiguration
+annotation|@
+name|Ignore
+argument_list|(
+literal|"Requires online access to the internet"
+argument_list|)
 DECL|class|HttpEndpointTest
 specifier|public
 class|class
