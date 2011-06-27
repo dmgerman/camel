257000e5ee55fe17271acbacaececc15bf93aad4
Claus Ibsen <davsclaus@apache.org>
2011-06-27 09:05:40 +0000

    Added note in javadoc about getOriginalInMessage is subject for change in later Camel release
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1140061 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 976f4178e770a88b550eb9278606ab25f79a3651

--- camel-core/src/main/java/org/apache/camel/spi/UnitOfWork.java
+++ camel-core/src/main/java/org/apache/camel/spi/UnitOfWork.java
@@ -130,7 +130,7 @@ name|String
 name|getId
 parameter_list|()
 function_decl|;
-comment|/**      * Gets the original IN {@link Message} this Unit of Work was started with.      *      * @return the original IN {@link Message}      */
+comment|/**      * Gets the original IN {@link Message} this Unit of Work was started with.      *<p/>      *<b>Important:</b> This is subject for change in a later Camel release, where we plan to only      * support getting the original IN message if you have enabled this option explicit.      *      * @return the original IN {@link Message}, may return<tt>null</tt> in a later Camel release (see important note).      */
 DECL|method|getOriginalInMessage ()
 name|Message
 name|getOriginalInMessage
