c02772c813503c49293276fce19d6bad57c8f428
Willem Ning Jiang <ningjiang@apache.org>
2011-07-15 12:55:53 +0000

    CAMEL-4228 Use the customer configuration for camel-itest-osgi
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1147133 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 265c94d3a523b8de2a3780c0ff4a32c94d8dad69

--- tests/camel-itest-osgi/src/test/java/org/apache/camel/itest/osgi/blueprint/BlueprintPropertiesRouteTest.java
+++ tests/camel-itest-osgi/src/test/java/org/apache/camel/itest/osgi/blueprint/BlueprintPropertiesRouteTest.java
@@ -465,19 +465,8 @@ name|options
 init|=
 name|combine
 argument_list|(
-comment|// Default karaf environment
-name|Helper
-operator|.
-name|getDefaultOptions
-argument_list|(
-comment|// this is how you set the default log level when using pax logging (logProfile)
-name|Helper
-operator|.
-name|setLogLevel
-argument_list|(
-literal|"INFO"
-argument_list|)
-argument_list|)
+name|getDefaultCamelKarafOptions
+argument_list|()
 argument_list|,
 name|bundle
 argument_list|(
@@ -519,68 +508,14 @@ operator|.
 name|noStart
 argument_list|()
 argument_list|,
-comment|// install the spring dm profile
-name|profile
-argument_list|(
-literal|"spring.dm"
-argument_list|)
-operator|.
-name|version
-argument_list|(
-literal|"1.2.0"
-argument_list|)
-argument_list|,
-name|mavenBundle
-argument_list|(
-literal|"org.apache.aries.blueprint"
-argument_list|,
-literal|"org.apache.aries.blueprint"
-argument_list|,
-literal|"0.2-incubating"
-argument_list|)
-argument_list|,
-comment|// install blueprint requirements
-name|mavenBundle
-argument_list|(
-literal|"org.apache.felix"
-argument_list|,
-literal|"org.apache.felix.configadmin"
-argument_list|)
-argument_list|,
-comment|// install the spring, http features first
-name|scanFeatures
-argument_list|(
-name|getKarafFeatureUrl
-argument_list|()
-argument_list|,
-literal|"spring"
-argument_list|,
-literal|"spring-dm"
-argument_list|,
-literal|"jetty"
-argument_list|)
-argument_list|,
 comment|// using the features to install the camel components
 name|scanFeatures
 argument_list|(
 name|getCamelKarafFeatureUrl
 argument_list|()
 argument_list|,
-literal|"camel-core"
-argument_list|,
 literal|"camel-blueprint"
-argument_list|,
-literal|"camel-test"
 argument_list|)
-argument_list|,
-name|workingDirectory
-argument_list|(
-literal|"target/paxrunner/"
-argument_list|)
-argument_list|,
-name|felix
-argument_list|()
-comment|/*, equinox()*/
 argument_list|)
 decl_stmt|;
 return|return
