7829a1e69f03025da7be1d8985c4a84fd7b9ed0d
Claus Ibsen <davsclaus@apache.org>
2011-07-18 14:56:39 +0000

    Fixed one eclipse warning in examples.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1147911 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 3f1cb6bb3f1929adcc2434257d6cddac746c1457

--- examples/camel-example-jmx/src/main/java/org/apache/camel/example/jmx/SimpleBean.java
+++ examples/camel-example-jmx/src/main/java/org/apache/camel/example/jmx/SimpleBean.java
@@ -72,6 +72,15 @@ name|NotificationBroadcasterSupport
 implements|implements
 name|ISimpleMXBean
 block|{
+DECL|field|serialVersionUID
+specifier|private
+specifier|static
+specifier|final
+name|long
+name|serialVersionUID
+init|=
+literal|1L
+decl_stmt|;
 DECL|field|sequence
 specifier|private
 name|int
