1cfa35da003aee34b7dbb758ef0126bf72540092
Benjamin P. O'Day <boday@apache.org>
2011-08-04 03:30:08 +0000

    CAMEL-4118 added support for manually completing all message groups with a signal message
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1153737 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: e2d3cee0e131aede7bb87b91def6cd9073913b05

--- camel-core/src/main/java/org/apache/camel/Exchange.java
+++ camel-core/src/main/java/org/apache/camel/Exchange.java
@@ -120,6 +120,12 @@ name|AGGREGATION_STRATEGY
 init|=
 literal|"CamelAggregationStrategy"
 decl_stmt|;
+DECL|field|AGGREGATION_COMPLETE_ALL_GROUPS
+name|String
+name|AGGREGATION_COMPLETE_ALL_GROUPS
+init|=
+literal|"CamelAggregationCompleteAllGroups"
+decl_stmt|;
 DECL|field|ASYNC_WAIT
 name|String
 name|ASYNC_WAIT
