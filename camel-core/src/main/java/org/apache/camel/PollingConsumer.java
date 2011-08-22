8285f7b804afc72f6b1e9aa98e1fe0336fbd1057
Claus Ibsen <davsclaus@apache.org>
2011-08-22 11:16:18 +0000

    Polished javadoc and added note about done UoW from PollingConsumer
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1160205 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 59e4ae9253ac10a732ee18c660dfc3f62ac57685

--- camel-core/src/main/java/org/apache/camel/PollingConsumer.java
+++ camel-core/src/main/java/org/apache/camel/PollingConsumer.java
@@ -15,7 +15,7 @@ package|;
 end_package
 
 begin_comment
-comment|/**  * Represents a<a  * href="http://camel.apache.org/polling-consumer.html">Polling  * Consumer</a> where the caller polls for messages when it is ready.  *   * @version   */
+comment|/**  * Represents a<a  * href="http://camel.apache.org/polling-consumer.html">Polling  * Consumer</a> where the caller polls for messages when it is ready.  *<p/>  * When you are done with the returned {@link Exchange} you must ensure to invoke  * {@link org.apache.camel.spi.UnitOfWork#done(Exchange)} to signal to Camel that the {@link Exchange} is done.  *<p/>  * This is needed to ensure any {@link org.apache.camel.spi.Synchronization} works is being executed.  * For example if you consumed from a file endpoint, then the consumed file is only moved/delete when  * you done the {@link Exchange}.  *  * @version   */
 end_comment
 
 begin_interface
@@ -26,19 +26,19 @@ name|PollingConsumer
 extends|extends
 name|Consumer
 block|{
-comment|/**      * Waits until a message is available and then returns it. Warning that this      * method could block indefinitely if no messages are available.      *<p/>      * Will return<tt>null</tt> if the consumer is not started      *       * @return the message exchange received.      */
+comment|/**      * Waits until a message is available and then returns it. Warning that this      * method could block indefinitely if no messages are available.      *<p/>      * Will return<tt>null</tt> if the consumer is not started      *<p/>      *<b>Important:</b> See the class javadoc about the need for done the {@link org.apache.camel.spi.UnitOfWork}      * on the returned {@link Exchange}      *      * @return the message exchange received.      */
 DECL|method|receive ()
 name|Exchange
 name|receive
 parameter_list|()
 function_decl|;
-comment|/**      * Attempts to receive a message exchange immediately without waiting and      * returning<tt>null</tt> if a message exchange is not available yet.      *       * @return the message exchange if one is immediately available otherwise      *<tt>null</tt>      */
+comment|/**      * Attempts to receive a message exchange immediately without waiting and      * returning<tt>null</tt> if a message exchange is not available yet.      *<p/>      *<b>Important:</b> See the class javadoc about the need for done the {@link org.apache.camel.spi.UnitOfWork}      * on the returned {@link Exchange}      *      * @return the message exchange if one is immediately available otherwise      *<tt>null</tt>      */
 DECL|method|receiveNoWait ()
 name|Exchange
 name|receiveNoWait
 parameter_list|()
 function_decl|;
-comment|/**      * Attempts to receive a message exchange, waiting up to the given timeout      * to expire if a message is not yet available      *       * @param timeout the amount of time in milliseconds to wait for a message      *                before timing out and returning<tt>null</tt>      *       * @return the message exchange if one iwas available within the timeout      *         period, or<tt>null</tt> if the timeout expired      */
+comment|/**      * Attempts to receive a message exchange, waiting up to the given timeout      * to expire if a message is not yet available.      *<p/>      *<b>Important:</b> See the class javadoc about the need for done the {@link org.apache.camel.spi.UnitOfWork}      * on the returned {@link Exchange}      *       * @param timeout the amount of time in milliseconds to wait for a message      *                before timing out and returning<tt>null</tt>      *       * @return the message exchange if one iwas available within the timeout      *         period, or<tt>null</tt> if the timeout expired      */
 DECL|method|receive (long timeout)
 name|Exchange
 name|receive
