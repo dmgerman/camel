ceedce4e8f60c00265bc48df7c34637e30e447d4
Christian Schneider <cschneider@apache.org>
2011-08-29 13:49:44 +0000

    Fix checkstyle error
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1162802 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: f345e06cdd26afdf266552762204156035bc9506

--- components/camel-cxf/src/test/java/org/apache/camel/component/cxf/spring/CxfEndpointBeanTest.java
+++ components/camel-cxf/src/test/java/org/apache/camel/component/cxf/spring/CxfEndpointBeanTest.java
@@ -22,6 +22,18 @@ end_package
 
 begin_import
 import|import
+name|javax
+operator|.
+name|xml
+operator|.
+name|namespace
+operator|.
+name|QName
+import|;
+end_import
+
+begin_import
+import|import
 name|org
 operator|.
 name|apache
@@ -59,18 +71,6 @@ operator|.
 name|junit
 operator|.
 name|Test
-import|;
-end_import
-
-begin_import
-import|import
-name|javax
-operator|.
-name|xml
-operator|.
-name|namespace
-operator|.
-name|QName
 import|;
 end_import
 
