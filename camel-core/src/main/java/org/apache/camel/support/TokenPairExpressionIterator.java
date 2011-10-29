7748d4228b23f3e04bad3104e6d3ab5fa4376065
Claus Ibsen <davsclaus@apache.org>
2011-10-29 14:50:49 +0000

    Moved class to support
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1194898 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 0f6912b40981f8bf9aaf7c62016f5315419cab84

--- camel-core/src/main/java/org/apache/camel/support/TokenPairExpressionIterator.java
+++ camel-core/src/main/java/org/apache/camel/support/TokenPairExpressionIterator.java
@@ -4,7 +4,7 @@ comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or mor
 end_comment
 
 begin_package
-DECL|package|org.apache.camel.processor
+DECL|package|org.apache.camel.support
 package|package
 name|org
 operator|.
@@ -12,7 +12,7 @@ name|apache
 operator|.
 name|camel
 operator|.
-name|processor
+name|support
 package|;
 end_package
 
@@ -67,20 +67,6 @@ operator|.
 name|camel
 operator|.
 name|InvalidPayloadException
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|camel
-operator|.
-name|support
-operator|.
-name|ExpressionAdapter
 import|;
 end_import
 
