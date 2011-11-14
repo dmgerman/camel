894bed7a86e87c4da2bd2478fd7f3dbf5e2b76c0
Claus Ibsen <davsclaus@apache.org>
2011-11-14 09:08:03 +0000

    CAMEL-4437: Upgraded to HAPI 1.2
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1201637 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: a9f4a854ea2e8607b103083ccc653cf452fedfe9

--- tests/camel-itest-osgi/src/test/java/org/apache/camel/itest/osgi/hl7/HL7MLLPCodec2Test.java
+++ tests/camel-itest-osgi/src/test/java/org/apache/camel/itest/osgi/hl7/HL7MLLPCodec2Test.java
@@ -102,16 +102,6 @@ name|org
 operator|.
 name|junit
 operator|.
-name|Ignore
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|junit
-operator|.
 name|Test
 import|;
 end_import
@@ -233,11 +223,6 @@ argument_list|(
 name|JUnit4TestRunner
 operator|.
 name|class
-argument_list|)
-annotation|@
-name|Ignore
-argument_list|(
-literal|"HAPI bundle is wrong"
 argument_list|)
 DECL|class|HL7MLLPCodec2Test
 specifier|public
