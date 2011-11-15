98915813d052a9e3f68880050d96f966df6e3ad1
Willem Ning Jiang <ningjiang@apache.org>
2011-11-15 14:23:32 +0000

    CAMEL-4671 Fixed the osgi itest errors
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1202206 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: f8e08904bd716bbb5d5b768a6042acc6320907b0

--- tests/camel-itest-osgi/src/test/java/org/apache/camel/itest/osgi/rss/RssPollingConsumerTest.java
+++ tests/camel-itest-osgi/src/test/java/org/apache/camel/itest/osgi/rss/RssPollingConsumerTest.java
@@ -150,6 +150,16 @@ name|org
 operator|.
 name|junit
 operator|.
+name|Ignore
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
 name|Test
 import|;
 end_import
@@ -255,6 +265,11 @@ argument_list|(
 name|JUnit4TestRunner
 operator|.
 name|class
+argument_list|)
+annotation|@
+name|Ignore
+argument_list|(
+literal|"abdera-core bundle has a wrong stax api dependency"
 argument_list|)
 DECL|class|RssPollingConsumerTest
 specifier|public
