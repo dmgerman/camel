1a2e044dcfef27c887f2804da57f86545e139d9c
Claus Ibsen <davsclaus@apache.org>
2012-01-02 15:59:58 +0000

    CAMEL-3471: camel-mina2 component. Thanks to Chad Beaulac for the patch.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1226449 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 12a6102d606fb927fc345b091ea36843858a8a92

---
+++ components/camel-mina2/src/main/java/org/apache/camel/component/mina2/Mina2Constants.java
@@ -0,0 +1,86 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+
+begin_package
+DECL|package|org.apache.camel.component.mina2
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|camel
+operator|.
+name|component
+operator|.
+name|mina2
+package|;
+end_package
+
+begin_comment
+comment|/**  * Mina constants  *  * @version   */
+end_comment
+
+begin_class
+DECL|class|Mina2Constants
+specifier|public
+specifier|final
+class|class
+name|Mina2Constants
+block|{
+DECL|field|MINA_CLOSE_SESSION_WHEN_COMPLETE
+specifier|public
+specifier|static
+specifier|final
+specifier|transient
+name|String
+name|MINA_CLOSE_SESSION_WHEN_COMPLETE
+init|=
+literal|"CamelMina2CloseSessionWhenComplete"
+decl_stmt|;
+comment|/** The key of the IoSession which is stored in the message header*/
+DECL|field|MINA_IOSESSION
+specifier|public
+specifier|static
+specifier|final
+specifier|transient
+name|String
+name|MINA_IOSESSION
+init|=
+literal|"CamelMina2IoSession"
+decl_stmt|;
+comment|/** The socket address of local machine that received the message. */
+DECL|field|MINA_LOCAL_ADDRESS
+specifier|public
+specifier|static
+specifier|final
+specifier|transient
+name|String
+name|MINA_LOCAL_ADDRESS
+init|=
+literal|"CamelMina2LocalAddress"
+decl_stmt|;
+comment|/** The socket address of the remote machine that send the message. */
+DECL|field|MINA_REMOTE_ADDRESS
+specifier|public
+specifier|static
+specifier|final
+specifier|transient
+name|String
+name|MINA_REMOTE_ADDRESS
+init|=
+literal|"CamelMina2RemoteAddress"
+decl_stmt|;
+DECL|method|Mina2Constants ()
+specifier|private
+name|Mina2Constants
+parameter_list|()
+block|{
+comment|// Utility class
+block|}
+block|}
+end_class
+
+end_unit
+
