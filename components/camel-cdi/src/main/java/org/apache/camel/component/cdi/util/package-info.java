784a6485a7e5a5d47acfb3905ceddb0b580920b1
Moulliard Charles <cmoulliard@apache.org>
2012-02-06 11:08:44 +0000

    Add BeanManagerProvider and services (javax.enterprise.inject.spi.extension)
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1240976 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: d66d63361c73b5d28de11ad83456791b722835e4

---
+++ components/camel-cdi/src/main/java/org/apache/camel/component/cdi/util/package-info.java
@@ -0,0 +1,24 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/**  * mainly copied from deltaspike to avoid to bring to whole  * package when only 2 classes are useful.  */
+end_comment
+
+begin_package
+DECL|package|org.apache.camel.component.cdi.util
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|camel
+operator|.
+name|component
+operator|.
+name|cdi
+operator|.
+name|util
+package|;
+end_package
+
+end_unit
+
