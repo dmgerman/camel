f13df8729bf46b752ff1f4f19736ce64a3d2c18b
Christian Mueller <cmueller@apache.org>
2012-03-23 12:12:58 +0000

    Revert "[CAMEL-4987] Minor documentation tweaks and fix of a false positive unit test in Spring Test support."
    [CAMEL-4987] Reverting changes which are impossible to use with Spring 3.0 and 3.1
    
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1304300 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: ad7e43430927bfa41f22405a10c6c8e647331897

--- components/camel-spring/src/test/java/org/apache/camel/spring/management/SpringManagedRouteRemoveRouteScopedErrorHandlerTest.java
+++ components/camel-spring/src/test/java/org/apache/camel/spring/management/SpringManagedRouteRemoveRouteScopedErrorHandlerTest.java
@@ -62,10 +62,6 @@ name|createSpringCamelContext
 import|;
 end_import
 
-begin_comment
-comment|/**  * @version   */
-end_comment
-
 begin_class
 DECL|class|SpringManagedRouteRemoveRouteScopedErrorHandlerTest
 specifier|public
