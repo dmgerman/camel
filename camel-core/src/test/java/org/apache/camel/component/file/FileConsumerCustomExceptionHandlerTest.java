d937f096ed3f0f3e55cc9b03f618b2afdfff76f0
Claus Ibsen <davsclaus@apache.org>
2012-04-18 11:35:09 +0000

    CAMEL-5178: File consumer should use exception handler to handle all component specific exceptions. Add example how to plugin a custom exception handler and send that to a Camel route so Camel routing onException can deal with the exception. Reduce the out of the box logging exception handler to WARN level.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1327450 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: aa8627c0acfbe524401b95ff316d4f9491643398

--- camel-core/src/test/java/org/apache/camel/component/file/FileConsumerCustomExceptionHandlerTest.java
+++ camel-core/src/test/java/org/apache/camel/component/file/FileConsumerCustomExceptionHandlerTest.java
@@ -370,7 +370,7 @@ block|}
 block|}
 return|;
 block|}
-comment|// END SNIPPET: e1
+comment|// END SNIPPET: e2
 comment|// START SNIPPET: e1
 comment|/**      * Custom {@link ExceptionHandler} to be used on the file consumer, to send      * exceptions to a Camel route, to let Camel deal with the error.      */
 DECL|class|MyExceptionHandler
