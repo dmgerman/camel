a648c8705d8ced5f373319a5a72ddf4a34e962c2
Claus Ibsen <davsclaus@apache.org>
2012-05-05 09:43:01 +0000

    Polished
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1334366 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 523732fd2bd7f4ebe995072ac14a030a23bc3a0a

--- components/camel-krati/src/main/java/org/apache/camel/component/krati/processor/idempotent/KratiIdempotentRepository.java
+++ components/camel-krati/src/main/java/org/apache/camel/component/krati/processor/idempotent/KratiIdempotentRepository.java
@@ -392,7 +392,7 @@ name|LOG
 operator|.
 name|warn
 argument_list|(
-literal|"Error adding item to krati idempotent repository. This exception is ignored."
+literal|"Error checking item exists in krati idempotent repository. This exception is ignored."
 argument_list|,
 name|e
 argument_list|)
@@ -452,7 +452,7 @@ name|LOG
 operator|.
 name|warn
 argument_list|(
-literal|"Error adding item to krati idempotent repository. This exception is ignored."
+literal|"Error removing item from krati idempotent repository. This exception is ignored."
 argument_list|,
 name|e
 argument_list|)
