c53b3e801a8386d189a63a4068ed70c93adea302
Daniel Kulp <dkulp@apache.org>
2012-05-08 01:11:16 +0000

    Make sure we pick up the version of osgi-core that contains the ServiceReference class we need.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1335315 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 948ee4a3a43d26e66560e6c3018241aa860e48d6

--- components/camel-test-blueprint/src/main/java/org/apache/camel/test/blueprint/CamelBlueprintHelper.java
+++ components/camel-test-blueprint/src/main/java/org/apache/camel/test/blueprint/CamelBlueprintHelper.java
@@ -1188,9 +1188,6 @@ expr_stmt|;
 for|for
 control|(
 name|ServiceReference
-argument_list|<
-name|?
-argument_list|>
 name|ref
 range|:
 name|asCollection
@@ -1221,9 +1218,6 @@ block|}
 for|for
 control|(
 name|ServiceReference
-argument_list|<
-name|?
-argument_list|>
 name|ref
 range|:
 name|asCollection
@@ -1397,22 +1391,16 @@ argument_list|()
 return|;
 block|}
 comment|/**      * Provides an iterable collection of references, even if the original array is<code>null</code>.      */
-DECL|method|asCollection (ServiceReference<?>[] references)
+DECL|method|asCollection (ServiceReference[] references)
 specifier|private
 specifier|static
 name|Collection
 argument_list|<
 name|ServiceReference
-argument_list|<
-name|?
-argument_list|>
 argument_list|>
 name|asCollection
 parameter_list|(
 name|ServiceReference
-argument_list|<
-name|?
-argument_list|>
 index|[]
 name|references
 parameter_list|)
@@ -1426,9 +1414,6 @@ operator|new
 name|ArrayList
 argument_list|<
 name|ServiceReference
-argument_list|<
-name|?
-argument_list|>
 argument_list|>
 argument_list|(
 literal|0
