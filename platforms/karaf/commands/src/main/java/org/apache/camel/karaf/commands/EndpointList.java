ae1ef1ab8e27d1bce8b192f3c9a48a59e0544c1c
Willem Ning Jiang <ningjiang@apache.org>
2012-05-17 04:14:57 +0000

    Fixed the CS errors
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1339473 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: b53f607002102eea40fb87efd39890c4ed64acd3

--- platforms/karaf/commands/src/main/java/org/apache/camel/karaf/commands/EndpointList.java
+++ platforms/karaf/commands/src/main/java/org/apache/camel/karaf/commands/EndpointList.java
@@ -20,6 +20,16 @@ end_package
 
 begin_import
 import|import
+name|java
+operator|.
+name|util
+operator|.
+name|List
+import|;
+end_import
+
+begin_import
+import|import
 name|org
 operator|.
 name|apache
@@ -95,16 +105,6 @@ operator|.
 name|console
 operator|.
 name|OsgiCommandSupport
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|util
-operator|.
-name|List
 import|;
 end_import
 
