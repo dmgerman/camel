b6b21054e00c69eb371da81da4c0de4c02a8738a
Claus Ibsen <davsclaus@apache.org>
2012-06-12 11:02:38 +0000

    CAMEL-5360: Ignore spring-ws test as it requires Spring 3.1
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1349251 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 155a5d6b2f3f05534d39c4c787e1dac2596dd50f

--- tests/camel-itest-karaf/src/test/java/org/apache/camel/itest/karaf/CamelSpringWebServiceTest.java
+++ tests/camel-itest-karaf/src/test/java/org/apache/camel/itest/karaf/CamelSpringWebServiceTest.java
@@ -24,6 +24,16 @@ name|org
 operator|.
 name|junit
 operator|.
+name|Ignore
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
 name|Test
 import|;
 end_import
@@ -93,6 +103,11 @@ argument_list|(
 name|JUnit4TestRunner
 operator|.
 name|class
+argument_list|)
+annotation|@
+name|Ignore
+argument_list|(
+literal|"Requires Spring 3.1"
 argument_list|)
 DECL|class|CamelSpringWebServiceTest
 specifier|public
