708548aaf50c44c93b2f3fa321e4def354905f19
James Strachan <jstrachan@apache.org>
2012-08-22 08:11:31 +0000

    allow eventNotifiers to be added asynchronously to a CamelContext while a context is starting up to avoid the dreaded ConcurrentModificationException being thrown, fixes CAMEL-5531
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1375936 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 5a3f3cd4db5719cabce5a0d68306309c66e2fcbc

--- camel-core/src/main/java/org/apache/camel/management/DefaultManagementStrategy.java
+++ camel-core/src/main/java/org/apache/camel/management/DefaultManagementStrategy.java
@@ -22,16 +22,6 @@ name|java
 operator|.
 name|util
 operator|.
-name|ArrayList
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|util
-operator|.
 name|EventObject
 import|;
 end_import
@@ -43,6 +33,18 @@ operator|.
 name|util
 operator|.
 name|List
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|util
+operator|.
+name|concurrent
+operator|.
+name|CopyOnWriteArrayList
 import|;
 end_import
 
@@ -263,7 +265,7 @@ argument_list|>
 name|eventNotifiers
 init|=
 operator|new
-name|ArrayList
+name|CopyOnWriteArrayList
 argument_list|<
 name|EventNotifier
 argument_list|>
