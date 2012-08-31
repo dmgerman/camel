47bea601e2ec0c870f11ac5576b2cc222cd81ffe
Claus Ibsen <davsclaus@apache.org>
2012-08-31 07:10:29 +0000

    CAMEL-5480: Quickfix engines is deferred to be started after CamelContext has been fully started. To avoid any side effects with engines firing too fast.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1379324 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 8d84fe5280f9733a0d860c1c805aa8dcf499cfa0

--- components/camel-quickfix/src/main/java/org/apache/camel/component/quickfixj/QuickfixjComponent.java
+++ components/camel-quickfix/src/main/java/org/apache/camel/component/quickfixj/QuickfixjComponent.java
@@ -50,20 +50,6 @@ end_import
 
 begin_import
 import|import
-name|java
-operator|.
-name|util
-operator|.
-name|concurrent
-operator|.
-name|atomic
-operator|.
-name|AtomicBoolean
-import|;
-end_import
-
-begin_import
-import|import
 name|org
 operator|.
 name|apache
