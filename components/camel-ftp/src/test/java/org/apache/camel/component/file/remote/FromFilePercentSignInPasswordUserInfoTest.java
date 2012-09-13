9b10c2a6cc159d8426f4144f13ef72fc38786418
Claus Ibsen <davsclaus@apache.org>
2012-09-13 14:39:57 +0000

    CAMEL-5603: Added unit test with ftp endpoints, based on user forum issue.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1384346 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: f9ac759ae184c90408cabf874a049607ea4d420e

---
+++ components/camel-ftp/src/test/java/org/apache/camel/component/file/remote/FromFilePercentSignInPasswordUserInfoTest.java
@@ -0,0 +1,56 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+
+begin_package
+DECL|package|org.apache.camel.component.file.remote
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|camel
+operator|.
+name|component
+operator|.
+name|file
+operator|.
+name|remote
+package|;
+end_package
+
+begin_comment
+comment|/**  * @version   */
+end_comment
+
+begin_class
+DECL|class|FromFilePercentSignInPasswordUserInfoTest
+specifier|public
+class|class
+name|FromFilePercentSignInPasswordUserInfoTest
+extends|extends
+name|FromFilePercentSignInPasswordTest
+block|{
+DECL|method|getFtpUrl ()
+specifier|protected
+name|String
+name|getFtpUrl
+parameter_list|()
+block|{
+comment|// the user info is not encoded, but we should be forgiving and allow the user to use
+comment|// the user name and password out of the box as is
+return|return
+literal|"ftp://us@r:t%st@localhost:"
+operator|+
+name|getPort
+argument_list|()
+operator|+
+literal|"/tmp3/camel?consumer.initialDelay=3000"
+return|;
+block|}
+block|}
+end_class
+
+end_unit
+
