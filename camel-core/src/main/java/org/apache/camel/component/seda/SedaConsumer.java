f99a788c2e319843979afc184ce2494ef0324b15
Jonathan Anstey <janstey@apache.org>
2012-10-03 10:57:34 +0000

    Polish shutdown code
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1393404 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: e32c732354a8916eae228603ffade96155bddc4b

--- camel-core/src/main/java/org/apache/camel/component/seda/SedaConsumer.java
+++ camel-core/src/main/java/org/apache/camel/component/seda/SedaConsumer.java
@@ -1391,33 +1391,9 @@ argument_list|(
 name|this
 argument_list|)
 expr_stmt|;
-comment|// need to shutdown executor here as well or each time this endpoint is
-comment|// started a new thread will be created
-if|if
-condition|(
-name|executor
-operator|!=
-literal|null
-condition|)
-block|{
-name|endpoint
-operator|.
-name|getCamelContext
+name|shutdownExecutor
 argument_list|()
-operator|.
-name|getExecutorServiceManager
-argument_list|()
-operator|.
-name|shutdownNow
-argument_list|(
-name|executor
-argument_list|)
 expr_stmt|;
-name|executor
-operator|=
-literal|null
-expr_stmt|;
-block|}
 block|}
 annotation|@
 name|Override
@@ -1429,7 +1405,16 @@ parameter_list|()
 throws|throws
 name|Exception
 block|{
-comment|// only shutdown thread pool when we shutdown
+name|shutdownExecutor
+argument_list|()
+expr_stmt|;
+block|}
+DECL|method|shutdownExecutor ()
+specifier|private
+name|void
+name|shutdownExecutor
+parameter_list|()
+block|{
 if|if
 condition|(
 name|executor
