e24bca1c55b7cc430ff4cc51b2e86e0a366ddbdc
Claus Ibsen <davsclaus@apache.org>
2012-10-31 08:30:25 +0000

    Dont use varargs in logger as thats only from slf4j 1.7, and we want to support 1.6 also.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1404025 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 3dda28cacb2726e8a96a45f4c3cc225a60a1f609

--- components/camel-test-spring/src/main/java/org/apache/camel/test/spring/CamelSpringTestContextLoader.java
+++ components/camel-test-spring/src/main/java/org/apache/camel/test/spring/CamelSpringTestContextLoader.java
@@ -1653,11 +1653,16 @@ name|info
 argument_list|(
 literal|"Setting shutdown timeout to [{} {}] on CamelContext with name [{}]."
 argument_list|,
+operator|new
+name|Object
+index|[]
+block|{
 name|shutdownTimeout
-argument_list|,
+block|,
 name|shutdownTimeUnit
-argument_list|,
+block|,
 name|contextName
+block|}
 argument_list|)
 expr_stmt|;
 name|camelContext
