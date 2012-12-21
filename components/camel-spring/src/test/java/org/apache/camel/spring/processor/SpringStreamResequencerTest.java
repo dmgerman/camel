e5b08450526ba702132977a7395965abe592d879
Babak Vahdat <bvahdat@apache.org>
2012-12-21 19:26:13 +0000

    CAMEL-5900: Clean-up the test being mentioned by the user.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1425102 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: e095dde79ba293a6376966a9eb1469a0abd9f041

--- components/camel-spring/src/test/java/org/apache/camel/spring/processor/SpringStreamResequencerTest.java
+++ components/camel-spring/src/test/java/org/apache/camel/spring/processor/SpringStreamResequencerTest.java
@@ -70,6 +70,8 @@ name|SpringStreamResequencerTest
 extends|extends
 name|StreamResequencerTest
 block|{
+annotation|@
+name|Override
 DECL|method|createCamelContext ()
 specifier|protected
 name|CamelContext
