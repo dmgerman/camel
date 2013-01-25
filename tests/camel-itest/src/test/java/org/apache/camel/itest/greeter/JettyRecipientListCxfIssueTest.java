d8edcc5b13d6d00d715659c1bf9979d67f9db088
Willem Ning Jiang <ningjiang@apache.org>
2013-01-25 07:13:39 +0000

    Fixed the issue of using Deprecated SpringTestSupport in tests
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1438360 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: c798b28055a699dda96a14d56862b038e8267db0

--- tests/camel-itest/src/test/java/org/apache/camel/itest/greeter/JettyRecipientListCxfIssueTest.java
+++ tests/camel-itest/src/test/java/org/apache/camel/itest/greeter/JettyRecipientListCxfIssueTest.java
@@ -76,7 +76,7 @@ name|camel
 operator|.
 name|test
 operator|.
-name|junit4
+name|spring
 operator|.
 name|CamelSpringTestSupport
 import|;
--- tests/camel-itest/src/test/java/org/apache/camel/itest/greeter/RecipientListCxf2Test.java
+++ tests/camel-itest/src/test/java/org/apache/camel/itest/greeter/RecipientListCxf2Test.java
@@ -110,7 +110,7 @@ name|camel
 operator|.
 name|test
 operator|.
-name|junit4
+name|spring
 operator|.
 name|CamelSpringTestSupport
 import|;
