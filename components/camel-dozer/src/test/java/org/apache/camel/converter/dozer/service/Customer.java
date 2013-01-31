3ec14f86ca68de74041f0d830a54155b240e0fa8
Claus Ibsen <davsclaus@apache.org>
2013-01-31 13:06:20 +0000

    Upgraded to dozer 5.4.0
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1440943 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: d7817f9bdad67cc1b0389ee81b403a21c5696218

--- components/camel-dozer/src/test/java/org/apache/camel/converter/dozer/service/Customer.java
+++ components/camel-dozer/src/test/java/org/apache/camel/converter/dozer/service/Customer.java
@@ -28,7 +28,7 @@ name|apache
 operator|.
 name|commons
 operator|.
-name|lang
+name|lang3
 operator|.
 name|builder
 operator|.
@@ -44,7 +44,7 @@ name|apache
 operator|.
 name|commons
 operator|.
-name|lang
+name|lang3
 operator|.
 name|builder
 operator|.
@@ -60,7 +60,7 @@ name|apache
 operator|.
 name|commons
 operator|.
-name|lang
+name|lang3
 operator|.
 name|builder
 operator|.
