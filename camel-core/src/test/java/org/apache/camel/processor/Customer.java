46273d404733dd188d14302576e493b21a24fd9f
Claus Ibsen <davsclaus@apache.org>
2013-02-02 10:22:03 +0000

    CAMEL-6016: Added type function to simple language.
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1441724 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: be43c17f0d2093887af4e71a9d1888743cd467ef

---
+++ camel-core/src/test/java/org/apache/camel/processor/Customer.java
@@ -0,0 +1,49 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+
+begin_package
+DECL|package|org.apache.camel.processor
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|camel
+operator|.
+name|processor
+package|;
+end_package
+
+begin_comment
+comment|/**  *  */
+end_comment
+
+begin_comment
+comment|// START SNIPPET: e1
+end_comment
+
+begin_enum
+DECL|enum|Customer
+specifier|public
+enum|enum
+name|Customer
+block|{
+DECL|enumConstant|GOLD
+DECL|enumConstant|SILVER
+DECL|enumConstant|BRONZE
+name|GOLD
+block|,
+name|SILVER
+block|,
+name|BRONZE
+block|}
+end_enum
+
+begin_comment
+comment|// END SNIPPET: e1
+end_comment
+
+end_unit
+
