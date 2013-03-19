315d3e13478a96b1a25f97aeae9e59ac7fac84e7
Daniel Kulp <dkulp@apache.org>
2013-03-19 19:22:19 +0000

    Fix a checkstyle issue
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1458458 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: bb1fe8939a1ba4addfa3005afa260683497a782a

--- components/camel-jackson/src/main/java/org/apache/camel/component/jackson/JacksonDataFormat.java
+++ components/camel-jackson/src/main/java/org/apache/camel/component/jackson/JacksonDataFormat.java
@@ -60,6 +60,20 @@ end_import
 
 begin_import
 import|import
+name|com
+operator|.
+name|fasterxml
+operator|.
+name|jackson
+operator|.
+name|databind
+operator|.
+name|ObjectMapper
+import|;
+end_import
+
+begin_import
+import|import
 name|org
 operator|.
 name|apache
@@ -81,20 +95,6 @@ operator|.
 name|spi
 operator|.
 name|DataFormat
-import|;
-end_import
-
-begin_import
-import|import
-name|com
-operator|.
-name|fasterxml
-operator|.
-name|jackson
-operator|.
-name|databind
-operator|.
-name|ObjectMapper
 import|;
 end_import
 
