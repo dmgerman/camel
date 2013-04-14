1b104252970742606b1f03ea301e1c6c2c9f0a6f
Claus Ibsen <davsclaus@apache.org>
2013-04-14 10:06:52 +0000

    Fixed CS
    git-svn-id: https://svn.apache.org/repos/asf/camel/trunk@1467757 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 2f892940a5d236d215d29bc7f18647a95c79e03f

--- camel-core/src/test/java/org/apache/camel/impl/TypeConverterRegistryPerformanceTest.java
+++ camel-core/src/test/java/org/apache/camel/impl/TypeConverterRegistryPerformanceTest.java
@@ -68,6 +68,18 @@ begin_import
 import|import
 name|org
 operator|.
+name|w3c
+operator|.
+name|dom
+operator|.
+name|Document
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
 name|apache
 operator|.
 name|camel
@@ -87,18 +99,6 @@ operator|.
 name|util
 operator|.
 name|StopWatch
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|w3c
-operator|.
-name|dom
-operator|.
-name|Document
 import|;
 end_import
 
