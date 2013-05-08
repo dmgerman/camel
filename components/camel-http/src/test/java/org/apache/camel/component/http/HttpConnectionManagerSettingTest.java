fe5d29f9e4fd50b63ae4600aa73986cc881b0aa7
Willem Jiang <ningjiang@apache.org>
2013-05-08 14:33:55 +0800

    Fixed the CS errors
    Former-commit-id: 4736fd6d45a24e17491912e31cb616e5ffb533bd

--- components/camel-http/src/test/java/org/apache/camel/component/http/HttpConnectionManagerSettingTest.java
+++ components/camel-http/src/test/java/org/apache/camel/component/http/HttpConnectionManagerSettingTest.java
@@ -19,24 +19,6 @@ package|;
 end_package
 
 begin_import
-import|import static
-name|org
-operator|.
-name|apache
-operator|.
-name|camel
-operator|.
-name|component
-operator|.
-name|http
-operator|.
-name|HttpMethods
-operator|.
-name|POST
-import|;
-end_import
-
-begin_import
 import|import
 name|org
 operator|.
@@ -135,6 +117,24 @@ operator|.
 name|junit
 operator|.
 name|Test
+import|;
+end_import
+
+begin_import
+import|import static
+name|org
+operator|.
+name|apache
+operator|.
+name|camel
+operator|.
+name|component
+operator|.
+name|http
+operator|.
+name|HttpMethods
+operator|.
+name|POST
 import|;
 end_import
 
