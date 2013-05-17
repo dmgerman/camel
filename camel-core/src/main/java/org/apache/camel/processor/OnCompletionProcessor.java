fcad2a611f8afd89779541780847e014e9a7b706
Claus Ibsen <davsclaus@apache.org>
2013-05-17 17:03:34 +0200

    On completion should not double wrap UoW
    Former-commit-id: e2b22608cad4f15c242b38ef7bcd6953bc9a4149

--- camel-core/src/main/java/org/apache/camel/processor/OnCompletionProcessor.java
+++ camel-core/src/main/java/org/apache/camel/processor/OnCompletionProcessor.java
@@ -358,16 +358,11 @@ name|camelContext
 operator|=
 name|camelContext
 expr_stmt|;
-comment|// wrap processor in UnitOfWork so what we send out runs in a UoW
 name|this
 operator|.
 name|processor
 operator|=
-operator|new
-name|UnitOfWorkProcessor
-argument_list|(
 name|processor
-argument_list|)
 expr_stmt|;
 name|this
 operator|.
