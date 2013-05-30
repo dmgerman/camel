d00e7386657541114149c1f95a87c5a326974607
cmueller <cmueller@apache.org>
2013-05-30 14:27:03 +0200

    fixed CS issues
    Former-commit-id: 4d1753eab5697eeaf6dc94dcbb049d0ac72ba49f

--- components/camel-mvel/src/main/java/org/apache/camel/language/mvel/MvelComponent.java
+++ components/camel-mvel/src/main/java/org/apache/camel/language/mvel/MvelComponent.java
@@ -20,6 +20,16 @@ end_package
 
 begin_import
 import|import
+name|java
+operator|.
+name|util
+operator|.
+name|Map
+import|;
+end_import
+
+begin_import
+import|import
 name|org
 operator|.
 name|apache
@@ -69,16 +79,6 @@ operator|.
 name|util
 operator|.
 name|ResourceHelper
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|util
-operator|.
-name|Map
 import|;
 end_import
 
