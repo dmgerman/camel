5ee6ec665c9803b929c5d22b9f3b6aa9e68c91e1
Claus Ibsen <davsclaus@apache.org>
2013-07-17 16:05:42 +0200

    CAMEL-6476: Introducing StreamCachingStrategy SPI to make it easier to configure and allow 3rd party to plugin custom strategies. Work in progress.
    Former-commit-id: d0235489fd77031c70501d6e859807ae72117034

--- camel-core/src/main/java/org/apache/camel/spi/StreamCachingStrategy.java
+++ camel-core/src/main/java/org/apache/camel/spi/StreamCachingStrategy.java
@@ -36,6 +36,7 @@ specifier|public
 interface|interface
 name|StreamCachingStrategy
 block|{
+comment|/**      * Sets the temporary directory to use for overflow and spooling to disk.      *<p/>      * If no temporary directory has been explicit configured, then a directory      * is created in the<tt>java.io.tmpdir</tt> directory.      */
 DECL|method|setTemporaryDirectory (File path)
 name|void
 name|setTemporaryDirectory
@@ -47,6 +48,70 @@ function_decl|;
 DECL|method|getTemporaryDirectory ()
 name|File
 name|getTemporaryDirectory
+parameter_list|()
+function_decl|;
+DECL|method|setTemporaryDirectory (String path)
+name|void
+name|setTemporaryDirectory
+parameter_list|(
+name|String
+name|path
+parameter_list|)
+function_decl|;
+comment|/**      * Threshold in bytes when overflow to disk is activated.      *<p/>      * The default threshold is {@link org.apache.camel.StreamCache#DEFAULT_SPOOL_THRESHOLD} bytes (eg 128kb).      * Use<tt>-1</tt> to disable overflow to disk.      */
+DECL|method|setSpoolThreshold (long threshold)
+name|void
+name|setSpoolThreshold
+parameter_list|(
+name|long
+name|threshold
+parameter_list|)
+function_decl|;
+DECL|method|getSpoolThreshold ()
+name|long
+name|getSpoolThreshold
+parameter_list|()
+function_decl|;
+comment|/**      * Sets the buffer size to use when copying between buffers.      *<p/>      * The default size is {@link org.apache.camel.util.IOHelper#DEFAULT_BUFFER_SIZE}      */
+DECL|method|setBufferSize (int bufferSize)
+name|void
+name|setBufferSize
+parameter_list|(
+name|int
+name|bufferSize
+parameter_list|)
+function_decl|;
+DECL|method|getBufferSize ()
+name|int
+name|getBufferSize
+parameter_list|()
+function_decl|;
+comment|/**      * Sets a chiper name to use when spooling to disk to write with encryption.      *<p/>      * By default the data is not encrypted.      */
+DECL|method|setSpoolChiper (String chiper)
+name|void
+name|setSpoolChiper
+parameter_list|(
+name|String
+name|chiper
+parameter_list|)
+function_decl|;
+DECL|method|getSpoolChiper ()
+name|String
+name|getSpoolChiper
+parameter_list|()
+function_decl|;
+comment|/**      * Whether to remove the temporary directory when stopping.      *<p/>      * This option is default<tt>true</tt>      */
+DECL|method|setRemoveTemporaryDirectoryWhenStopping (boolean remove)
+name|void
+name|setRemoveTemporaryDirectoryWhenStopping
+parameter_list|(
+name|boolean
+name|remove
+parameter_list|)
+function_decl|;
+DECL|method|isRemoveTemporaryDirectoryWhenStopping ()
+name|boolean
+name|isRemoveTemporaryDirectoryWhenStopping
 parameter_list|()
 function_decl|;
 block|}
