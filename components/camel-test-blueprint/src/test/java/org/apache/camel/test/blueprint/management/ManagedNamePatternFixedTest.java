c4f7a713d4fb3362e2cff305b4b908ab3a41120f
Claus Ibsen <davsclaus@apache.org>
2013-07-30 13:51:00 +0200

    Fixed test as it should indicate to use JMX for testing
    Former-commit-id: 4b07f9115d4671c5af20b07b02393fe578ec618e

--- components/camel-test-blueprint/src/test/java/org/apache/camel/test/blueprint/management/ManagedNamePatternFixedTest.java
+++ components/camel-test-blueprint/src/test/java/org/apache/camel/test/blueprint/management/ManagedNamePatternFixedTest.java
@@ -76,6 +76,18 @@ name|CamelBlueprintTestSupport
 block|{
 annotation|@
 name|Override
+DECL|method|useJmx ()
+specifier|protected
+name|boolean
+name|useJmx
+parameter_list|()
+block|{
+return|return
+literal|true
+return|;
+block|}
+annotation|@
+name|Override
 DECL|method|getBlueprintDescriptor ()
 specifier|protected
 name|String
