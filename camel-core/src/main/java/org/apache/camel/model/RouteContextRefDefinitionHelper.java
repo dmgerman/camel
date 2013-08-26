3b8d102fb469d03b0ef707da9a4f906045f1c59e
Claus Ibsen <davsclaus@apache.org>
2013-08-26 14:31:26 +0200

    CAMEL-6291: Fixed <routeContextRef> to not share the routes between the <camelContext>s as that can cause side-effects.
    Former-commit-id: 90af871ddfd2e65b31b584147d701ab79680542f

--- camel-core/src/main/java/org/apache/camel/model/RouteContextRefDefinitionHelper.java
+++ camel-core/src/main/java/org/apache/camel/model/RouteContextRefDefinitionHelper.java
@@ -207,7 +207,17 @@ specifier|static
 name|JAXBContext
 name|jaxbContext
 decl_stmt|;
+DECL|method|RouteContextRefDefinitionHelper ()
+specifier|private
+name|RouteContextRefDefinitionHelper
+parameter_list|()
+block|{     }
 comment|/**      * Lookup the routes from the {@link RouteContextRefDefinition}.      *<p/>      * This implmementation must be used to lookup the routes as it performs a deep clone of the routes      * as a {@link RouteContextRefDefinition} can be re-used with multiple {@link CamelContext} and each      * context should have their own instances of the routes. This is to ensure no side-effefts and sharing      * of instances between the contexts. For example such as property placeholders may be context specific      * so the routes should not use placeholders from another {@link CamelContext}.      *      * @param camelContext the CamelContext      * @param ref          the id of the {@link RouteContextRefDefinition} to lookup and get the rotues.      * @return the routes.      */
+annotation|@
+name|SuppressWarnings
+argument_list|(
+literal|"unchecked"
+argument_list|)
 DECL|method|lookupRoutes (CamelContext camelContext, String ref)
 specifier|public
 specifier|static
