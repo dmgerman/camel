4cf1f0dda892874fbe2fe5a621a74797b32275be
Babak Vahdat <bvahdat@apache.org>
2013-10-08 21:52:55 +0200

    Fixed the compilation error by JsonPath karaf-itest. Also upgraded the karaf-itests to make use of Karaf "2.3.3".
    Former-commit-id: 3210a8d16a3360684d4a77ffa562735fff3a3d89

--- tests/camel-itest-karaf/src/test/java/org/apache/camel/itest/karaf/AbstractFeatureTest.java
+++ tests/camel-itest-karaf/src/test/java/org/apache/camel/itest/karaf/AbstractFeatureTest.java
@@ -941,7 +941,7 @@ comment|//This version doesn't affect the version of karaf we use
 operator|.
 name|karafVersion
 argument_list|(
-literal|"2.3.1"
+literal|"2.3.3"
 argument_list|)
 operator|.
 name|name
