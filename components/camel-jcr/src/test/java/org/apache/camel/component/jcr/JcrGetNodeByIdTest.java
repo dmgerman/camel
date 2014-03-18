860cbf439aaf5122a4b19b86987c081e4f396776
Gregor Zurowski <gregor@zurowski.org>
2014-03-18 14:41:17 -0400

    Refactor and simplify JUnit test cases for camel-jcr
    Signed-off-by: Gregor Zurowski <gregor@zurowski.org>
    Former-commit-id: 86181743bb4874c98f8615125d8e5def20932215

--- components/camel-jcr/src/test/java/org/apache/camel/component/jcr/JcrGetNodeByIdTest.java
+++ components/camel-jcr/src/test/java/org/apache/camel/component/jcr/JcrGetNodeByIdTest.java
@@ -167,11 +167,6 @@ parameter_list|()
 throws|throws
 name|Exception
 block|{
-name|deleteDirectory
-argument_list|(
-literal|"target/repository"
-argument_list|)
-expr_stmt|;
 name|super
 operator|.
 name|setUp
