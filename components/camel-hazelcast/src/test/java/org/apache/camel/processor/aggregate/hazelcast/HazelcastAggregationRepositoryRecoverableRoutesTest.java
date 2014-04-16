664b4a9dac1fc679cd24fd98228e1ee5dfea5298
Willem Jiang <willem.jiang@gmail.com>
2014-04-16 15:37:17 +0800

    CAMEL-7118 Fixed some CS errors
    Former-commit-id: e53c66a9fdeb81ee6081fefc016a12420a01d8e0

--- components/camel-hazelcast/src/test/java/org/apache/camel/processor/aggregate/hazelcast/HazelcastAggregationRepositoryRecoverableRoutesTest.java
+++ components/camel-hazelcast/src/test/java/org/apache/camel/processor/aggregate/hazelcast/HazelcastAggregationRepositoryRecoverableRoutesTest.java
@@ -1,4 +1,8 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+
 begin_package
 DECL|package|org.apache.camel.processor.aggregate.hazelcast
 package|package
@@ -91,10 +95,6 @@ operator|.
 name|Test
 import|;
 end_import
-
-begin_comment
-comment|/**  * @author Alexander Lomov  *         Date: 04.01.14  *         Time: 4:37  */
-end_comment
 
 begin_class
 DECL|class|HazelcastAggregationRepositoryRecoverableRoutesTest
@@ -541,19 +541,24 @@ name|assertIsSatisfied
 argument_list|()
 expr_stmt|;
 block|}
-annotation|@
-name|SuppressWarnings
-argument_list|(
-literal|"unused"
-argument_list|)
 DECL|class|EverythingIsLostException
 specifier|private
 specifier|static
+specifier|final
 class|class
 name|EverythingIsLostException
 extends|extends
 name|Exception
 block|{
+DECL|field|serialVersionUID
+specifier|private
+specifier|static
+specifier|final
+name|long
+name|serialVersionUID
+init|=
+literal|1L
+decl_stmt|;
 DECL|method|EverythingIsLostException ()
 specifier|private
 name|EverythingIsLostException
