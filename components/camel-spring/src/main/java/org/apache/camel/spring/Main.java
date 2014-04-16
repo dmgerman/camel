ea6f808758df412485d78eaa89a0f28769ae4c2b
Claus Ibsen <davsclaus@apache.org>
2014-04-16 07:45:50 -0700

    CAMEL-7361: Spring main should be able to detect additional spring xml locations on the classpath without any additional configuration.
    Former-commit-id: 5c25b19245ab4902781747a0fcbdd041c3085e7a

--- components/camel-spring/src/main/java/org/apache/camel/spring/Main.java
+++ components/camel-spring/src/main/java/org/apache/camel/spring/Main.java
@@ -277,7 +277,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * A command line tool for booting up a CamelContext using an optional Spring  * ApplicationContext  */
+comment|/**  * A command line tool for booting up a CamelContext using an optional Spring  * {@link org.springframework.context.ApplicationContext}.  *<p/>  * By placing a file in the {@link #LOCATION_PROPERTIES} directory of any JARs on the classpath,  * allows this Main class to load those additional Spring XML files as Spring  * {@link org.springframework.context.ApplicationContext} to be included.  *<p/>  * Each line in the {@link #LOCATION_PROPERTIES} is a reference to a Spring XML file to include,  * which by default gets loaded from classpath.  */
 end_comment
 
 begin_class
