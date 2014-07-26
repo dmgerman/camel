face2a91797ea83ff9cb7b8aae03ee8f7f898ed8
Claus Ibsen <davsclaus@apache.org>
2014-07-26 10:41:02 +0200

    CAMEL-7354: Rest DSL. Integrate with camel-jetty. Work in progress.
    Former-commit-id: 60fd9e978f876c48721378664a1135fe66532ac9

--- components/camel-jetty/src/main/java/org/apache/camel/component/jetty/CamelContinuationServlet.java
+++ components/camel-jetty/src/main/java/org/apache/camel/component/jetty/CamelContinuationServlet.java
@@ -327,9 +327,15 @@ comment|// is there a consumer registered for the request.
 name|HttpConsumer
 name|consumer
 init|=
+name|getServletResolveConsumerStrategy
+argument_list|()
+operator|.
 name|resolve
 argument_list|(
 name|request
+argument_list|,
+name|getConsumers
+argument_list|()
 argument_list|)
 decl_stmt|;
 if|if
