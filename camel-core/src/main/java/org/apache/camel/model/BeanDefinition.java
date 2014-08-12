366dfc77c34e2b7eab4f45cd143784b8c649a2cc
Willem Jiang <willem.jiang@gmail.com>
2014-08-12 16:00:58 +0800

    CAMEL-7682 Added multiParameterArray option to bean DSL
    Former-commit-id: 759a4d62bf6bc95bc240f075813541235e918305

--- camel-core/src/main/java/org/apache/camel/model/BeanDefinition.java
+++ camel-core/src/main/java/org/apache/camel/model/BeanDefinition.java
@@ -310,6 +310,13 @@ name|Boolean
 name|cache
 decl_stmt|;
 annotation|@
+name|XmlAttribute
+DECL|field|multiParameterArray
+specifier|private
+name|Boolean
+name|multiParameterArray
+decl_stmt|;
+annotation|@
 name|XmlTransient
 DECL|field|beanClass
 specifier|private
@@ -642,6 +649,32 @@ operator|.
 name|cache
 operator|=
 name|cache
+expr_stmt|;
+block|}
+DECL|method|getMultiParameterArray ()
+specifier|public
+name|Boolean
+name|getMultiParameterArray
+parameter_list|()
+block|{
+return|return
+name|multiParameterArray
+return|;
+block|}
+DECL|method|setMultiParameterArray (Boolean multiParameterArray)
+specifier|public
+name|void
+name|setMultiParameterArray
+parameter_list|(
+name|Boolean
+name|multiParameterArray
+parameter_list|)
+block|{
+name|this
+operator|.
+name|multiParameterArray
+operator|=
+name|multiParameterArray
 expr_stmt|;
 block|}
 comment|// Fluent API
@@ -1028,6 +1061,22 @@ operator|new
 name|BeanProcessor
 argument_list|(
 name|beanHolder
+argument_list|)
+expr_stmt|;
+block|}
+comment|// check for multiParameterArray setting
+if|if
+condition|(
+name|multiParameterArray
+operator|!=
+literal|null
+condition|)
+block|{
+name|answer
+operator|.
+name|setMultiParameterArray
+argument_list|(
+name|multiParameterArray
 argument_list|)
 expr_stmt|;
 block|}
