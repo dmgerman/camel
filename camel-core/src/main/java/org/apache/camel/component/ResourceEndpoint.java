63df7f426c208134325839c3068dd38d406db7a3
Claus Ibsen <davsclaus@apache.org>
2014-11-12 19:57:00 +0100

    CAMEL-8038: Component endpoint options should provide @UriPath. Also components extending others should be separate classes as minimum
    Former-commit-id: 0a98687b9c36877af505a74aa1148f946045ef21

--- camel-core/src/main/java/org/apache/camel/component/ResourceEndpoint.java
+++ camel-core/src/main/java/org/apache/camel/component/ResourceEndpoint.java
@@ -174,6 +174,20 @@ name|apache
 operator|.
 name|camel
 operator|.
+name|spi
+operator|.
+name|UriPath
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|camel
+operator|.
 name|util
 operator|.
 name|IOHelper
@@ -258,7 +272,12 @@ index|[]
 name|buffer
 decl_stmt|;
 annotation|@
-name|UriParam
+name|UriPath
+argument_list|(
+name|description
+operator|=
+literal|"path to the resource"
+argument_list|)
 DECL|field|resourceUri
 specifier|private
 name|String
