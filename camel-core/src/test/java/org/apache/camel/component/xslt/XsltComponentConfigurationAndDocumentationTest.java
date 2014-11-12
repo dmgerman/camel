4d1a79c93f6bd1c2b37990851ff15689743cd659
Claus Ibsen <davsclaus@apache.org>
2014-11-12 08:30:20 +0100

    CAMEL-8034: xslt component like the others.
    Former-commit-id: 438700a1e8f1a7cf8c170155a2d709b70931e84a

--- camel-core/src/test/java/org/apache/camel/component/xslt/XsltComponentConfigurationAndDocumentationTest.java
+++ camel-core/src/test/java/org/apache/camel/component/xslt/XsltComponentConfigurationAndDocumentationTest.java
@@ -183,7 +183,7 @@ name|json
 operator|.
 name|contains
 argument_list|(
-literal|"\"resourceUri\": { \"type\": \"string\""
+literal|"\"contentCache\": { \"type\": \"boolean\", \"javaType\": \"boolean\" }"
 argument_list|)
 argument_list|)
 expr_stmt|;
