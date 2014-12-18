ef1c86717ccb59a391170d3205d634cf20c07a73
Claus Ibsen <davsclaus@apache.org>
2014-12-18 10:13:05 +0100

    CAMEL-7999: More components include documentation
    Former-commit-id: 1d763cd4679dca110c6cb2531e73ad655875650b

--- components/camel-geocoder/src/main/java/org/apache/camel/component/geocoder/GeoCoderComponent.java
+++ components/camel-geocoder/src/main/java/org/apache/camel/component/geocoder/GeoCoderComponent.java
@@ -50,7 +50,7 @@ name|camel
 operator|.
 name|impl
 operator|.
-name|DefaultComponent
+name|UriEndpointComponent
 import|;
 end_import
 
@@ -64,8 +64,21 @@ specifier|public
 class|class
 name|GeoCoderComponent
 extends|extends
-name|DefaultComponent
+name|UriEndpointComponent
 block|{
+DECL|method|GeoCoderComponent ()
+specifier|public
+name|GeoCoderComponent
+parameter_list|()
+block|{
+name|super
+argument_list|(
+name|GeoCoderEndpoint
+operator|.
+name|class
+argument_list|)
+expr_stmt|;
+block|}
 DECL|method|createEndpoint (String uri, String remaining, Map<String, Object> parameters)
 specifier|protected
 name|Endpoint
