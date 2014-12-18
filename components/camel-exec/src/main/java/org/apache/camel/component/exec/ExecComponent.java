642ab7e5cfcef6917c81529350a3f067589e80e4
Claus Ibsen <davsclaus@apache.org>
2014-12-18 10:02:58 +0100

    CAMEL-7999: More components include documentation
    Former-commit-id: 142f4defc0a96b26c45724d10961e114da841485

--- components/camel-exec/src/main/java/org/apache/camel/component/exec/ExecComponent.java
+++ components/camel-exec/src/main/java/org/apache/camel/component/exec/ExecComponent.java
@@ -60,7 +60,7 @@ name|camel
 operator|.
 name|impl
 operator|.
-name|DefaultComponent
+name|UriEndpointComponent
 import|;
 end_import
 
@@ -74,8 +74,21 @@ specifier|public
 class|class
 name|ExecComponent
 extends|extends
-name|DefaultComponent
+name|UriEndpointComponent
 block|{
+DECL|method|ExecComponent ()
+specifier|public
+name|ExecComponent
+parameter_list|()
+block|{
+name|super
+argument_list|(
+name|ExecEndpoint
+operator|.
+name|class
+argument_list|)
+expr_stmt|;
+block|}
 annotation|@
 name|Override
 DECL|method|createEndpoint (String uri, String remaining, Map<String, Object> parameters)
