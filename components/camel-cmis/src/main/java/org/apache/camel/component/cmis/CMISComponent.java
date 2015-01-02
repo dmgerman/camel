23af086cb3d22ef11212d0d7d34e7c81dc5c0e71
Claus Ibsen <davsclaus@apache.org>
2015-01-02 15:53:00 +0100

    CAMEL-7999: More components include documentation
    Former-commit-id: e16f0ab80693a38ebf6970e4f2da03c4fd34fb9a

--- components/camel-cmis/src/main/java/org/apache/camel/component/cmis/CMISComponent.java
+++ components/camel-cmis/src/main/java/org/apache/camel/component/cmis/CMISComponent.java
@@ -50,7 +50,7 @@ name|camel
 operator|.
 name|impl
 operator|.
-name|DefaultComponent
+name|UriEndpointComponent
 import|;
 end_import
 
@@ -64,8 +64,21 @@ specifier|public
 class|class
 name|CMISComponent
 extends|extends
-name|DefaultComponent
+name|UriEndpointComponent
 block|{
+DECL|method|CMISComponent ()
+specifier|public
+name|CMISComponent
+parameter_list|()
+block|{
+name|super
+argument_list|(
+name|CMISEndpoint
+operator|.
+name|class
+argument_list|)
+expr_stmt|;
+block|}
 DECL|method|createEndpoint (String uri, String remaining, Map<String, Object> parameters)
 specifier|protected
 name|Endpoint
