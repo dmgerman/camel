5bd5147b8ec4cd254772e31384d62ca9553c59d4
Claus Ibsen <davsclaus@apache.org>
2015-01-12 07:34:52 +0100

    Skip test
    Former-commit-id: 6cea262c68093869f904c48ede318234cd303f04

--- tests/camel-itest/src/test/java/org/apache/camel/itest/mail/SpringMailEndpointTest.java
+++ tests/camel-itest/src/test/java/org/apache/camel/itest/mail/SpringMailEndpointTest.java
@@ -64,6 +64,16 @@ name|org
 operator|.
 name|junit
 operator|.
+name|Ignore
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
 name|Test
 import|;
 end_import
@@ -133,6 +143,8 @@ end_comment
 begin_class
 annotation|@
 name|ContextConfiguration
+annotation|@
+name|Ignore
 DECL|class|SpringMailEndpointTest
 specifier|public
 class|class
