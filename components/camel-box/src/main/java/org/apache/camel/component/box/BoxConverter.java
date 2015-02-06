a8b34fa3751979ec0fac222dd61c46a689c4f497
Claus Ibsen <davsclaus@apache.org>
2015-02-06 12:34:33 +0100

    CAMEL-8272: Camel-box socks proxy implementation is incomplete. Thanks to Julian Cable for the patch.
    Former-commit-id: 984de3dd78b73132a6d740718efa92cde78e0c82

--- components/camel-box/src/main/java/org/apache/camel/component/box/BoxConverter.java
+++ components/camel-box/src/main/java/org/apache/camel/component/box/BoxConverter.java
@@ -24,6 +24,16 @@ name|java
 operator|.
 name|io
 operator|.
+name|ByteArrayInputStream
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|io
+operator|.
 name|File
 import|;
 end_import
@@ -35,16 +45,6 @@ operator|.
 name|io
 operator|.
 name|InputStream
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|io
-operator|.
-name|ByteArrayInputStream
 import|;
 end_import
 
