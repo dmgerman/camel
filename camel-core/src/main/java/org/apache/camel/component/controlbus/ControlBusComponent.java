225e1b33dd9e9d1bb1ccf9ed2ad7d81be1f2c8b9
Willem Jiang <willem.jiang@gmail.com>
2015-02-06 14:39:46 +0800

    CAMEL-8023 Polished the java doc
    Former-commit-id: 9ad074f4f099ac8ee9aee6eee78dcac803a8ff20

--- camel-core/src/main/java/org/apache/camel/component/controlbus/ControlBusComponent.java
+++ camel-core/src/main/java/org/apache/camel/component/controlbus/ControlBusComponent.java
@@ -67,7 +67,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * The<a href="http://camel.apache.org/controlbus.html">Control Bus component</a> allows sending messages to a control-bus endpoint to control routes.  */
+comment|/**  * The<a href="http://camel.apache.org/controlbus.html">Control Bus component</a> allows sending messages to a control-bus endpoint to control the lifecycle of routes.  */
 end_comment
 
 begin_class
