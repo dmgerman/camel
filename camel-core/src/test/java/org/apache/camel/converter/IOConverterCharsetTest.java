32c1841cbf2aa21d78297648354f6ac774d99a91
Willem Jiang <willem.jiang@gmail.com>
2015-02-25 12:57:37 +0800

    CAMEL-8356 Clean up the code
    Former-commit-id: e9e686ee147a946cc702830ad1eba8f952e9e7aa

--- camel-core/src/test/java/org/apache/camel/converter/IOConverterCharsetTest.java
+++ camel-core/src/test/java/org/apache/camel/converter/IOConverterCharsetTest.java
@@ -1,4 +1,8 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+
 begin_package
 DECL|package|org.apache.camel.converter
 package|package
@@ -92,6 +96,15 @@ name|IOConverterCharsetTest
 extends|extends
 name|ContextTestSupport
 block|{
+DECL|field|CONTENT
+specifier|private
+specifier|static
+specifier|final
+name|String
+name|CONTENT
+init|=
+literal|"G\u00f6tzend\u00e4mmerung,Joseph und seine Br\u00fcder"
+decl_stmt|;
 DECL|method|testToInputStreamFileWithCharsetUTF8 ()
 specifier|public
 name|void
@@ -178,7 +191,7 @@ argument_list|)
 expr_stmt|;
 name|assertEquals
 argument_list|(
-literal|"GÃ¶tzendÃ¤mmerung,Joseph und seine BrÃ¼der"
+name|CONTENT
 argument_list|,
 name|line
 argument_list|)
@@ -280,7 +293,7 @@ argument_list|)
 expr_stmt|;
 name|assertEquals
 argument_list|(
-literal|"GÃ¶tzendÃ¤mmerung,Joseph und seine BrÃ¼der"
+name|CONTENT
 argument_list|,
 name|line
 argument_list|)
@@ -466,7 +479,7 @@ argument_list|)
 expr_stmt|;
 name|assertEquals
 argument_list|(
-literal|"GÃ¶tzendÃ¤mmerung,Joseph und seine BrÃ¼der"
+name|CONTENT
 argument_list|,
 name|line
 argument_list|)
@@ -555,7 +568,7 @@ argument_list|)
 expr_stmt|;
 name|assertEquals
 argument_list|(
-literal|"GÃ¶tzendÃ¤mmerung,Joseph und seine BrÃ¼der"
+name|CONTENT
 argument_list|,
 name|line
 argument_list|)
