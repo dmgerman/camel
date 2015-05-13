4a30bc6435d027ef6cdd0d84b5897790d237a225
sebi <sebi@softvision.ro>
2015-05-13 19:30:57 +0300

    adding swagger custom paramater definition
    Former-commit-id: 96ce5691b954b5596a075fcb6625e6c9995d1a9b

--- camel-core/src/main/java/org/apache/camel/model/rest/RestDefinition.java
+++ camel-core/src/main/java/org/apache/camel/model/rest/RestDefinition.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *<p>  * http://www.apache.org/licenses/LICENSE-2.0  *<p>  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -1019,6 +1019,54 @@ return|return
 name|this
 return|;
 block|}
+DECL|method|restParam ()
+specifier|public
+name|RestParamDefinition
+name|restParam
+parameter_list|()
+block|{
+if|if
+condition|(
+name|getVerbs
+argument_list|()
+operator|.
+name|isEmpty
+argument_list|()
+condition|)
+block|{
+throw|throw
+operator|new
+name|IllegalArgumentException
+argument_list|(
+literal|"Must add verb first, such as get/post/delete"
+argument_list|)
+throw|;
+block|}
+name|VerbDefinition
+name|verb
+init|=
+name|getVerbs
+argument_list|()
+operator|.
+name|get
+argument_list|(
+name|getVerbs
+argument_list|()
+operator|.
+name|size
+argument_list|()
+operator|-
+literal|1
+argument_list|)
+decl_stmt|;
+return|return
+operator|new
+name|RestParamDefinition
+argument_list|(
+name|verb
+argument_list|)
+return|;
+block|}
 DECL|method|produces (String mediaType)
 specifier|public
 name|RestDefinition
@@ -1751,6 +1799,14 @@ name|verb
 argument_list|)
 expr_stmt|;
 block|}
+name|getVerbs
+argument_list|()
+operator|.
+name|add
+argument_list|(
+name|answer
+argument_list|)
+expr_stmt|;
 name|answer
 operator|.
 name|setRest
@@ -1763,14 +1819,6 @@ operator|.
 name|setUri
 argument_list|(
 name|uri
-argument_list|)
-expr_stmt|;
-name|getVerbs
-argument_list|()
-operator|.
-name|add
-argument_list|(
-name|answer
 argument_list|)
 expr_stmt|;
 return|return
