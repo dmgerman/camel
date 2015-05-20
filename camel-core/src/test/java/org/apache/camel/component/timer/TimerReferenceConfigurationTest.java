0b26ba88c4866ae5d13efcbd9390785935daa045
Claus Ibsen <davsclaus@apache.org>
2015-05-20 17:09:01 +0200

    Fixed CS
    Former-commit-id: 546a0d28d5a2f46407dd41874ed6a22b41f7a849

--- camel-core/src/test/java/org/apache/camel/component/timer/TimerReferenceConfigurationTest.java
+++ camel-core/src/test/java/org/apache/camel/component/timer/TimerReferenceConfigurationTest.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright 2015 The Apache Software Foundation.  *  * Licensed under the Apache License, Version 2.0 (the "License");  * you may not use this file except in compliance with the License.  * You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -89,10 +89,6 @@ operator|.
 name|JndiRegistry
 import|;
 end_import
-
-begin_comment
-comment|/**  *  * @author JCostello  */
-end_comment
 
 begin_class
 DECL|class|TimerReferenceConfigurationTest
@@ -398,7 +394,7 @@ block|}
 block|}
 return|;
 block|}
-comment|/**      * Test that the reference configuration params are correct      * @throws Exception       */
+comment|/**      * Test that the reference configuration params are correct      *      * @throws Exception      */
 DECL|method|testReferenceConfiguration ()
 specifier|public
 name|void
@@ -537,7 +533,7 @@ name|repeatCount
 argument_list|)
 expr_stmt|;
 block|}
-comment|/**      * Test that the 'value' configuration params are correct      * @throws Exception       */
+comment|/**      * Test that the 'value' configuration params are correct      *      * @throws Exception      */
 DECL|method|testValueConfiguration ()
 specifier|public
 name|void
