c8eef007286d60f0dfc3d0cdf2e2debed543b905
Claus Ibsen <davsclaus@apache.org>
2015-05-27 18:42:30 +0200

    Fixed CS
    Former-commit-id: 8e9eab858bb83a2f44c6124974cf6a9e481c1dda

--- components/camel-ftp/src/test/java/org/apache/camel/component/file/remote/FtpBadLoginMockNoopConnectionLeakTest.java
+++ components/camel-ftp/src/test/java/org/apache/camel/component/file/remote/FtpBadLoginMockNoopConnectionLeakTest.java
@@ -186,7 +186,12 @@ name|socketAudits
 init|=
 operator|new
 name|HashMap
-argument_list|<>
+argument_list|<
+name|Integer
+argument_list|,
+name|boolean
+index|[]
+argument_list|>
 argument_list|()
 decl_stmt|;
 DECL|method|getFtpUrl ()
