2d0c1ef602c6a3b0554a3c27a22d1a2ed21014f6
Claus Ibsen <davsclaus@apache.org>
2015-05-30 10:20:22 +0200

    CAMEL-8799: Make it possible for JsonPath to suppress PathNotFoundException
    Former-commit-id: 0654622f2a3e3d2e41fec9af4c2edeb0b5c34465

--- components/camel-jsonpath/src/test/java/org/apache/camel/jsonpath/JsonPathBeanTest.java
+++ components/camel-jsonpath/src/test/java/org/apache/camel/jsonpath/JsonPathBeanTest.java
@@ -116,6 +116,60 @@ expr_stmt|;
 block|}
 annotation|@
 name|Test
+DECL|method|testFullNameTwo ()
+specifier|public
+name|void
+name|testFullNameTwo
+parameter_list|()
+throws|throws
+name|Exception
+block|{
+name|String
+name|json
+init|=
+literal|"{\"person\" : {\"firstname\" : \"foo\", \"middlename\" : \"foo2\", \"lastname\" : \"bar\"}}"
+decl_stmt|;
+name|String
+name|json2
+init|=
+literal|"{\"person\" : {\"firstname\" : \"bar\", \"middlename\" : \"bar2\", \"lastname\" : \"foo\"}}"
+decl_stmt|;
+name|getMockEndpoint
+argument_list|(
+literal|"mock:result"
+argument_list|)
+operator|.
+name|expectedBodiesReceived
+argument_list|(
+literal|"foo foo2 bar"
+argument_list|,
+literal|"bar bar2 foo"
+argument_list|)
+expr_stmt|;
+name|template
+operator|.
+name|sendBody
+argument_list|(
+literal|"direct:start"
+argument_list|,
+name|json
+argument_list|)
+expr_stmt|;
+name|template
+operator|.
+name|sendBody
+argument_list|(
+literal|"direct:start"
+argument_list|,
+name|json2
+argument_list|)
+expr_stmt|;
+name|assertMockEndpointsSatisfied
+argument_list|()
+expr_stmt|;
+block|}
+annotation|@
+name|Test
 DECL|method|testFirstAndLastName ()
 specifier|public
 name|void
