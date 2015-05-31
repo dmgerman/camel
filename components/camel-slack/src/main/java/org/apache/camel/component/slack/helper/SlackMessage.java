3b9a666e89fedbf5bf1058730e313ce79e237307
Mike Kennedy <michael.kennedy@ucr.edu>
2015-05-30 23:28:18 -0700

    Added slack message helper class
    Former-commit-id: 62e44a693cd1122f4bcca680c50667fb58ef7147

---
+++ components/camel-slack/src/main/java/org/apache/camel/component/slack/helper/SlackMessage.java
@@ -0,0 +1,32 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_package
+DECL|package|org.apache.camel.component.slack.helper
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|camel
+operator|.
+name|component
+operator|.
+name|slack
+operator|.
+name|helper
+package|;
+end_package
+
+begin_comment
+comment|/**  * Created by mikek on 5/30/15.  */
+end_comment
+
+begin_class
+DECL|class|SlackMessage
+specifier|public
+class|class
+name|SlackMessage
+block|{ }
+end_class
+
+end_unit
+
