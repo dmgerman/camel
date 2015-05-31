1c15f7da099aa81f2f0b29efba20e55c73652e3c
Mike Kennedy <michael.kennedy@ucr.edu>
2015-05-30 23:46:10 -0700

    Added camel blueprint test support
    Former-commit-id: ee05713759bf4821ee39073a3de2e6e51cfe5d99

---
+++ components/camel-slack/src/test/java/org/apache/camel/component/slack/SlackProducerTest.java
@@ -0,0 +1,30 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_package
+DECL|package|org.apache.camel.component.slack
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|camel
+operator|.
+name|component
+operator|.
+name|slack
+package|;
+end_package
+
+begin_comment
+comment|/**  * Created by mikek on 5/30/15.  */
+end_comment
+
+begin_class
+DECL|class|SlackProducerTest
+specifier|public
+class|class
+name|SlackProducerTest
+block|{ }
+end_class
+
+end_unit
+
