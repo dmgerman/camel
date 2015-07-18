5b13d99bb985cefd90c664b7f7aa53d176e9566d
Franz Forsthofer <franz.forsthofer@sap.com>
2015-07-18 08:52:33 +0200

    CAMEL-8905 encoding problems in json
    The actual correction is contained in the
    Change 53ddca7a6f29c8735090cec519d123ff499f850c [formerly 604d2d8c2f5b6176608f7dad827237f465980ac4].
    
    I forgot in this prvious change to add the JIRA ID.
    
    Change-Id: Iac8a6343a18f65986e9ba7f38c545eab0db321f4
    
    Former-commit-id: 8cb2e8468934ca1839a708ad1323e7143b246ab7

--- components/camel-jsonpath/src/main/java/org/apache/camel/jsonpath/JsonPathEngine.java
+++ components/camel-jsonpath/src/main/java/org/apache/camel/jsonpath/JsonPathEngine.java
@@ -504,7 +504,7 @@ block|}
 else|else
 block|{
 comment|// no json encoding specified --> assume json encoding is unicode and determine the specific unicode encoding according to RFC-4627
-comment|// this is a temporary solution, it can be removed as sson as jsonpath offers the encoding detection
+comment|// this is a temporary solution, it can be removed as soon as jsonpath offers the encoding detection
 name|JsonStream
 name|jsonStream
 init|=
