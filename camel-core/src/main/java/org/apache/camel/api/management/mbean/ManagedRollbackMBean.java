5c9a50d518735e9b4b0a763a2e37984ddb85e150
Claus Ibsen <davsclaus@apache.org>
2015-07-22 10:44:53 +0200

    CAMEL-8526: Add more EIP as specialized mbeans
    Former-commit-id: 9eadc03d4139596c85ffbdcdbd247ddb62fad808

---
+++ camel-core/src/main/java/org/apache/camel/api/management/mbean/ManagedRollbackMBean.java
@@ -0,0 +1,87 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+
+begin_package
+DECL|package|org.apache.camel.api.management.mbean
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|camel
+operator|.
+name|api
+operator|.
+name|management
+operator|.
+name|mbean
+package|;
+end_package
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|camel
+operator|.
+name|api
+operator|.
+name|management
+operator|.
+name|ManagedAttribute
+import|;
+end_import
+
+begin_interface
+DECL|interface|ManagedRollbackMBean
+specifier|public
+interface|interface
+name|ManagedRollbackMBean
+extends|extends
+name|ManagedProcessorMBean
+block|{
+annotation|@
+name|ManagedAttribute
+argument_list|(
+name|description
+operator|=
+literal|"Message to use in rollback exception"
+argument_list|)
+DECL|method|getMessage ()
+name|String
+name|getMessage
+parameter_list|()
+function_decl|;
+annotation|@
+name|ManagedAttribute
+argument_list|(
+name|description
+operator|=
+literal|"Mark the transaction for rollback only (cannot be overruled to commit)"
+argument_list|)
+DECL|method|isMarkRollbackOnly ()
+name|Boolean
+name|isMarkRollbackOnly
+parameter_list|()
+function_decl|;
+annotation|@
+name|ManagedAttribute
+argument_list|(
+name|description
+operator|=
+literal|"Mark only last sub transaction for rollback only"
+argument_list|)
+DECL|method|isMarkRollbackOnlyLast ()
+name|Boolean
+name|isMarkRollbackOnlyLast
+parameter_list|()
+function_decl|;
+block|}
+end_interface
+
+end_unit
+
