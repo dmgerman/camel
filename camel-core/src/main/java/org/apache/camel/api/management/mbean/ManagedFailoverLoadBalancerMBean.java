e371830b5d4ee735411a0c2f2e78cc0abbdbf7a3
Claus Ibsen <davsclaus@apache.org>
2015-07-24 13:16:36 +0200

    CAMEL-8526: Add more EIP as specialized mbeans
    Former-commit-id: 7093be11afbebdec0689f6fdedda6167e8a39391

---
+++ camel-core/src/main/java/org/apache/camel/api/management/mbean/ManagedFailoverLoadBalancerMBean.java
@@ -0,0 +1,99 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+
+begin_package
+DECL|package|org.apache.camel.api.management.mbean
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|camel
+operator|.
+name|api
+operator|.
+name|management
+operator|.
+name|mbean
+package|;
+end_package
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|camel
+operator|.
+name|api
+operator|.
+name|management
+operator|.
+name|ManagedAttribute
+import|;
+end_import
+
+begin_interface
+DECL|interface|ManagedFailoverLoadBalancerMBean
+specifier|public
+interface|interface
+name|ManagedFailoverLoadBalancerMBean
+extends|extends
+name|ManagedProcessorMBean
+block|{
+annotation|@
+name|ManagedAttribute
+argument_list|(
+name|description
+operator|=
+literal|"Number of processors in the load balancer"
+argument_list|)
+DECL|method|getSize ()
+name|Integer
+name|getSize
+parameter_list|()
+function_decl|;
+annotation|@
+name|ManagedAttribute
+argument_list|(
+name|description
+operator|=
+literal|"Whether or not the failover load balancer should operate in round robin mode or not."
+argument_list|)
+DECL|method|isRoundRobin ()
+name|Boolean
+name|isRoundRobin
+parameter_list|()
+function_decl|;
+annotation|@
+name|ManagedAttribute
+argument_list|(
+name|description
+operator|=
+literal|"Whether or not the failover load balancer should operate in sticky mode or not."
+argument_list|)
+DECL|method|isSticky ()
+name|Boolean
+name|isSticky
+parameter_list|()
+function_decl|;
+annotation|@
+name|ManagedAttribute
+argument_list|(
+name|description
+operator|=
+literal|"A value to indicate after X failover attempts we should exhaust (give up)."
+argument_list|)
+DECL|method|getMaximumFailoverAttempts ()
+name|Integer
+name|getMaximumFailoverAttempts
+parameter_list|()
+function_decl|;
+block|}
+end_interface
+
+end_unit
+
