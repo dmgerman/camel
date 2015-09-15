fcc98280d2806f00bdd9fa7dbb0d1b3350e47175
Andrea Cosentino <ancosen@gmail.com>
2015-09-15 21:04:46 +0200

    Fixed CS
    Former-commit-id: ea53dce2d072816bdf73607cb02184dad2d1fc6f

--- components/camel-facebook/src/test/java/org/apache/camel/component/facebook/FacebookComponentPageIdProducerTest.java
+++ components/camel-facebook/src/test/java/org/apache/camel/component/facebook/FacebookComponentPageIdProducerTest.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *<p/>  * http://www.apache.org/licenses/LICENSE-2.0  *<p/>  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -115,13 +115,6 @@ name|APACHE_FOUNDATION_PAGE_ID
 init|=
 literal|"6538157161"
 decl_stmt|;
-DECL|method|FacebookComponentPageIdProducerTest ()
-specifier|public
-name|FacebookComponentPageIdProducerTest
-parameter_list|()
-throws|throws
-name|Exception
-block|{     }
 DECL|field|lastTimestamp
 name|long
 name|lastTimestamp
@@ -129,6 +122,13 @@ init|=
 operator|-
 literal|1
 decl_stmt|;
+DECL|method|FacebookComponentPageIdProducerTest ()
+specifier|public
+name|FacebookComponentPageIdProducerTest
+parameter_list|()
+throws|throws
+name|Exception
+block|{     }
 annotation|@
 name|Test
 DECL|method|testProducers ()
