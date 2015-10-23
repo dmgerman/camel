d381f224abf2128d043f2eaaedbafcef4be41580
Claus Ibsen <davsclaus@apache.org>
2015-10-23 07:47:40 +0200

    CAMEL-9145: Remove camel-hbase feature as it does not really work in OSGi. And would also need a total new set of OSGi bundles for Hadoop 2.x and its sister projects.
    Former-commit-id: 0706d4702326368bfac325221cdab8215902724c

--- tests/camel-itest-karaf/src/test/java/org/apache/camel/itest/karaf/CamelHbaseTest.java
+++ tests/camel-itest-karaf/src/test/java/org/apache/camel/itest/karaf/CamelHbaseTest.java
@@ -24,6 +24,16 @@ name|org
 operator|.
 name|junit
 operator|.
+name|Ignore
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
 name|Test
 import|;
 end_import
@@ -91,6 +101,11 @@ argument_list|(
 name|PaxExam
 operator|.
 name|class
+argument_list|)
+annotation|@
+name|Ignore
+argument_list|(
+literal|"Need OSGi bundles of hadoop, and it does not run well in OSGi neither"
 argument_list|)
 DECL|class|CamelHbaseTest
 specifier|public
