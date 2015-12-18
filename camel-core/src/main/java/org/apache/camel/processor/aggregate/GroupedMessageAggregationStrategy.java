78d36168bee97707a7c764b165cdf233ea84e361
Claus Ibsen <davsclaus@apache.org>
2015-12-18 17:59:12 +0100

    Fixed CS. Closes #656
    Former-commit-id: f3c4fc2a215e7f79e46b889490892bae9477dab4

--- camel-core/src/main/java/org/apache/camel/processor/aggregate/GroupedMessageAggregationStrategy.java
+++ camel-core/src/main/java/org/apache/camel/processor/aggregate/GroupedMessageAggregationStrategy.java
@@ -1,4 +1,8 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+
 begin_package
 DECL|package|org.apache.camel.processor.aggregate
 package|package
@@ -63,7 +67,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * Aggregate all {@link Message} into a single combined Exchange holding all the  * aggregated messages in a {@link List} of {@link Message} as the message body.  *   * This aggregation strategy can used in combination with @{link  * org.apache.camel.processor.Splitter} to batch messages  *   * @version  */
+comment|/**  * Aggregate all {@link Message} into a single combined Exchange holding all the  * aggregated messages in a {@link List} of {@link Message} as the message body.  *   * This aggregation strategy can used in combination with {@link org.apache.camel.processor.Splitter} to batch messages  *   * @version  */
 end_comment
 
 begin_class
