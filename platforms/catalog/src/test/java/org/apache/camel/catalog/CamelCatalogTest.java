fa504f3914306952c6988a06fbe0e23409c19228
Claus Ibsen <davsclaus@apache.org>
2016-01-03 12:49:20 +0100

    Camel catalog - Validate uris with lookup value should be valid for any kind of type.
    Former-commit-id: e49eb8f4ee3189f8342f79a4f76e5378e897726d

--- platforms/catalog/src/test/java/org/apache/camel/catalog/CamelCatalogTest.java
+++ platforms/catalog/src/test/java/org/apache/camel/catalog/CamelCatalogTest.java
@@ -3051,6 +3051,24 @@ name|isSuccess
 argument_list|()
 argument_list|)
 expr_stmt|;
+comment|// reference lookup
+name|result
+operator|=
+name|catalog
+operator|.
+name|validateEndpointProperties
+argument_list|(
+literal|"timer://foo?fixedRate=#fixed&delay=#myDelay"
+argument_list|)
+expr_stmt|;
+name|assertTrue
+argument_list|(
+name|result
+operator|.
+name|isSuccess
+argument_list|()
+argument_list|)
+expr_stmt|;
 block|}
 annotation|@
 name|Test
