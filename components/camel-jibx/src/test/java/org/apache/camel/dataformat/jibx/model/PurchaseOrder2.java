af577de059d8fa79a07f2f6581d8267149ead32b
lburgazzoli <lburgazzoli@gmail.com>
2016-01-27 12:24:37 +0100

    CAMEL-6913 - Add dynamic unmarshalling capability to JiBX data format
    Former-commit-id: 8a6c87a44ef5a67a1d1f382b0d4a39f33881b060

--- components/camel-jibx/src/test/java/org/apache/camel/dataformat/jibx/model/PurchaseOrder2.java
+++ components/camel-jibx/src/test/java/org/apache/camel/dataformat/jibx/model/PurchaseOrder2.java
@@ -1,10 +1,10 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+comment|/*  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
-DECL|package|org.apache.camel.dataformat.jibx
+DECL|package|org.apache.camel.dataformat.jibx.model
 package|package
 name|org
 operator|.
@@ -15,6 +15,8 @@ operator|.
 name|dataformat
 operator|.
 name|jibx
+operator|.
+name|model
 package|;
 end_package
 
@@ -37,10 +39,10 @@ comment|/**  * @version   */
 end_comment
 
 begin_class
-DECL|class|PurchaseOrder
+DECL|class|PurchaseOrder2
 specifier|public
 class|class
-name|PurchaseOrder
+name|PurchaseOrder2
 block|{
 DECL|field|name
 specifier|private
@@ -96,14 +98,14 @@ if|if
 condition|(
 name|o
 operator|instanceof
-name|PurchaseOrder
+name|PurchaseOrder2
 condition|)
 block|{
-name|PurchaseOrder
+name|PurchaseOrder2
 name|that
 init|=
 operator|(
-name|PurchaseOrder
+name|PurchaseOrder2
 operator|)
 name|o
 decl_stmt|;
