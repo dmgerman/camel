82dd5d5d2fcbd02c4707010feca2ae4dc2938287
Claus Ibsen <davsclaus@apache.org>
2016-02-08 17:09:42 +0100

    Add to javadoc about thread safety in a processor in a route
    Former-commit-id: c4f97a1cc3c80b9e961e6c98d8b14486b2cc0456

--- camel-core/src/main/java/org/apache/camel/Processor.java
+++ camel-core/src/main/java/org/apache/camel/Processor.java
@@ -15,7 +15,7 @@ package|;
 end_package
 
 begin_comment
-comment|/**  * A<a href="http://camel.apache.org/processor.html">processor</a> is used to implement the  *<a href="http://camel.apache.org/event-driven-consumer.html"> Event Driven Consumer</a>  * and<a href="http://camel.apache.org/message-translator.html"> Message Translator</a>  * patterns and to process message exchanges.  *   * @version   */
+comment|/**  * A<a href="http://camel.apache.org/processor.html">processor</a> is used to implement the  *<a href="http://camel.apache.org/event-driven-consumer.html"> Event Driven Consumer</a>  * and<a href="http://camel.apache.org/message-translator.html"> Message Translator</a>  * patterns and to process message exchanges.  *<p/>  * Notice if you use a {@link Processor} in a Camel route, then make sure to write the {@link Processor}  * in a thread-safe way, as the Camel routes can potentially be executed by concurrent threads, and therefore  * multiple threads can call the same {@link Processor} instance.  *   * @version   */
 end_comment
 
 begin_interface
