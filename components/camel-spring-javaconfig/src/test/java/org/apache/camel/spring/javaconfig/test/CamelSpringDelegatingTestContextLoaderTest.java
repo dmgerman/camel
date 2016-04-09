d738c62a2d5d58e5f5e58bb9cf97741c50d389cf
Claus Ibsen <davsclaus@apache.org>
2016-04-09 10:45:14 +0200

    CAMEL-9650: camel-spring-javaconfig polished example and make it more like spring-boot
    Former-commit-id: 18a169c6534dc5730d16a564ad638572367e82d4

--- components/camel-spring-javaconfig/src/test/java/org/apache/camel/spring/javaconfig/test/CamelSpringDelegatingTestContextLoaderTest.java
+++ components/camel-spring-javaconfig/src/test/java/org/apache/camel/spring/javaconfig/test/CamelSpringDelegatingTestContextLoaderTest.java
@@ -156,16 +156,6 @@ name|org
 operator|.
 name|junit
 operator|.
-name|Ignore
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|junit
-operator|.
 name|Test
 import|;
 end_import
@@ -266,8 +256,6 @@ name|class
 argument_list|)
 annotation|@
 name|MockEndpoints
-annotation|@
-name|Ignore
 DECL|class|CamelSpringDelegatingTestContextLoaderTest
 specifier|public
 class|class
