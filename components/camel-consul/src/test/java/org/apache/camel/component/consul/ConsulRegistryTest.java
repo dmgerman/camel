627e6f527c232ed39176b075e0685032fcb4892a
Bernd Prager <bernd@prager.ws>
2016-05-27 11:49:37 -0400

    formatting
    Former-commit-id: aea4ddc692de21ee9baf06341eb92457e8c9bc38

--- components/camel-consul/src/test/java/org/apache/camel/component/consul/ConsulRegistryTest.java
+++ components/camel-consul/src/test/java/org/apache/camel/component/consul/ConsulRegistryTest.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/**  * Copyright (C) 2015 Bernd Prager<http://prager.ws>  *  * Licensed under the Apache License, Version 2.0 (the "License");  * you may not use this file except in compliance with the License.  * You may obtain a copy of the License at  *  *         http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -17,42 +17,6 @@ operator|.
 name|consul
 package|;
 end_package
-
-begin_import
-import|import static
-name|org
-operator|.
-name|junit
-operator|.
-name|Assert
-operator|.
-name|assertEquals
-import|;
-end_import
-
-begin_import
-import|import static
-name|org
-operator|.
-name|junit
-operator|.
-name|Assert
-operator|.
-name|assertNotNull
-import|;
-end_import
-
-begin_import
-import|import static
-name|org
-operator|.
-name|junit
-operator|.
-name|Assert
-operator|.
-name|assertNull
-import|;
-end_import
 
 begin_import
 import|import
@@ -149,7 +113,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  *   * @author Bernd Prager Unit test for Camel Registry implementation for Consul  */
+comment|/**  * Unit test for Camel Registry implementation for Consul  */
 end_comment
 
 begin_class
@@ -692,7 +656,7 @@ specifier|public
 name|void
 name|checkFailedFindByTypeWithName
 parameter_list|()
-block|{  	}
+block|{      }
 annotation|@
 name|Test
 DECL|method|storeObject ()
@@ -869,7 +833,7 @@ specifier|public
 name|void
 name|notFindByType
 parameter_list|()
-block|{  	}
+block|{      }
 annotation|@
 name|Test
 argument_list|(
