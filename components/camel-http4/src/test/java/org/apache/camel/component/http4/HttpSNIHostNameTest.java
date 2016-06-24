5e2790154cf8d3b574cf7738507a69b610bca10e
Arno Noordover <anoordover@users.noreply.github.com>
2016-06-24 19:15:50 +0200

    CAMEL-9638: ignored test for external SNIHostName site
    Former-commit-id: 8ba68e34a397c69e78f461b4fb274f457cd81e37

--- components/camel-http4/src/test/java/org/apache/camel/component/http4/HttpSNIHostNameTest.java
+++ components/camel-http4/src/test/java/org/apache/camel/component/http4/HttpSNIHostNameTest.java
@@ -68,6 +68,16 @@ name|org
 operator|.
 name|junit
 operator|.
+name|Ignore
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
 name|Test
 import|;
 end_import
@@ -115,6 +125,11 @@ import|;
 end_import
 
 begin_class
+annotation|@
+name|Ignore
+argument_list|(
+literal|"Ignored test because of external dependency."
+argument_list|)
 DECL|class|HttpSNIHostNameTest
 specifier|public
 class|class
