269bfd11a2b3d03cd786fd5e72ca0ae861812d7b
Leo Prince <leoprince.francisxavier@target.com>
2016-06-29 09:06:18 -0500

    Removed unused imports.
    Former-commit-id: a81e4d7e35254e6dd39503f341bc240af99f291e

--- components/camel-kafka/src/main/java/org/apache/camel/component/kafka/KafkaProducer.java
+++ components/camel-kafka/src/main/java/org/apache/camel/component/kafka/KafkaProducer.java
@@ -84,16 +84,6 @@ name|java
 operator|.
 name|util
 operator|.
-name|Vector
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|util
-operator|.
 name|concurrent
 operator|.
 name|ExecutorService
