e5de84604d2544bd3ad5b159c77e83916c874cb5
Claus Ibsen <davsclaus@apache.org>
2016-07-29 09:29:06 +0200

    Update generated source code
    Former-commit-id: 558cb0ac7b5ab8ecce70d34aec81c42ab26a8d9f

--- components/camel-jgroups/src/main/java/org/apache/camel/component/jgroups/springboot/JGroupsComponentAutoConfiguration.java
+++ components/camel-jgroups/src/main/java/org/apache/camel/component/jgroups/springboot/JGroupsComponentAutoConfiguration.java
@@ -179,6 +179,11 @@ name|JGroupsComponentAutoConfiguration
 block|{
 annotation|@
 name|Bean
+argument_list|(
+name|name
+operator|=
+literal|"jgroups-component"
+argument_list|)
 annotation|@
 name|ConditionalOnClass
 argument_list|(
