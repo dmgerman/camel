14d93b23823b6e023b4ad4d9479b73d57cd9f84a
eidottermihi <eidottermihi@gmail.com>
2016-10-21 08:38:36 +0200

    Add review remarks.
    Store FTP reply code after storing the file and before chmod operation.
    Remove author tag, add apache license header.
    
    Former-commit-id: 5c09c641bb872ca994ad454b406820c69666466e

--- components/camel-ftp/src/test/java/org/apache/camel/component/file/remote/FromFileToFtpWithChmodTest.java
+++ components/camel-ftp/src/test/java/org/apache/camel/component/file/remote/FromFileToFtpWithChmodTest.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/*  * Copyright (c): it@M - Dienstleister fÃ¼r Informations- und Telekommunikationstechnik  * der Landeshauptstadt MÃ¼nchen, 2016  */
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -59,10 +59,6 @@ operator|.
 name|Test
 import|;
 end_import
-
-begin_comment
-comment|/**  * @author eidottermihi  */
-end_comment
 
 begin_class
 DECL|class|FromFileToFtpWithChmodTest
