c7a88fe4e9c635a7f1d127560371a14bcb56cd06
gilfernandes <gil.fernandes@gmail.com>
2016-11-18 17:59:58 +0000

    Fixed style and documentation issues. The documentation is not yet complete.
    Former-commit-id: cec7b30f3395529dae6dc5b5d46ff0e64cb1d209

--- components/camel-firebase/src/main/java/org/apache/camel/component/firebase/exception/FirebaseException.java
+++ components/camel-firebase/src/main/java/org/apache/camel/component/firebase/exception/FirebaseException.java
@@ -1,4 +1,8 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+
 begin_package
 DECL|package|org.apache.camel.component.firebase.exception
 package|package
@@ -16,20 +20,6 @@ name|exception
 package|;
 end_package
 
-begin_import
-import|import
-name|com
-operator|.
-name|google
-operator|.
-name|firebase
-operator|.
-name|database
-operator|.
-name|DatabaseError
-import|;
-end_import
-
 begin_comment
 comment|/**  * Used to mark an exception occurred in the Firebase Camel processor.  */
 end_comment
@@ -42,32 +32,6 @@ name|FirebaseException
 extends|extends
 name|RuntimeException
 block|{
-DECL|field|databaseError
-specifier|private
-name|DatabaseError
-name|databaseError
-decl_stmt|;
-comment|/**      * Constructs a new runtime exception with {@code null} as its      * detail message.  The cause is not initialized, and may subsequently be      * initialized by a call to {@link #initCause}.      */
-DECL|method|FirebaseException ()
-specifier|public
-name|FirebaseException
-parameter_list|()
-block|{     }
-comment|/**      * Constructs a new runtime exception with the specified detail message.      * The cause is not initialized, and may subsequently be initialized by a      * call to {@link #initCause}.      *      * @param message the detail message. The detail message is saved for      *                later retrieval by the {@link #getMessage()} method.      */
-DECL|method|FirebaseException (String message)
-specifier|public
-name|FirebaseException
-parameter_list|(
-name|String
-name|message
-parameter_list|)
-block|{
-name|super
-argument_list|(
-name|message
-argument_list|)
-expr_stmt|;
-block|}
 comment|/**      * Constructs a new runtime exception with the specified detail message and      * cause.<p>Note that the detail message associated with      * {@code cause} is<i>not</i> automatically incorporated in      * this runtime exception's detail message.      *      * @param message the detail message (which is saved for later retrieval      *                by the {@link #getMessage()} method).      * @param cause   the cause (which is saved for later retrieval by the      *                {@link #getCause()} method).  (A<tt>null</tt> value is      *                permitted, and indicates that the cause is nonexistent or      *                unknown.)      * @since 1.4      */
 DECL|method|FirebaseException (String message, Throwable cause)
 specifier|public
@@ -87,32 +51,6 @@ argument_list|,
 name|cause
 argument_list|)
 expr_stmt|;
-block|}
-DECL|method|setDatabaseError (DatabaseError databaseError)
-specifier|public
-name|void
-name|setDatabaseError
-parameter_list|(
-name|DatabaseError
-name|databaseError
-parameter_list|)
-block|{
-name|this
-operator|.
-name|databaseError
-operator|=
-name|databaseError
-expr_stmt|;
-block|}
-DECL|method|getDatabaseError ()
-specifier|public
-name|DatabaseError
-name|getDatabaseError
-parameter_list|()
-block|{
-return|return
-name|databaseError
-return|;
 block|}
 block|}
 end_class
