4c93d07bf57f36c793b7a035d4c4118ed6056453
Claus Ibsen <davsclaus@apache.org>
2016-12-21 08:47:23 +0100

    CAMEL-10530: Fixed test after new mbean added
    Former-commit-id: 0f6e2715777bdcd2830b14f00f0821c01237e1bd

--- camel-core/src/test/java/org/apache/camel/impl/MultipleLifecycleStrategyTest.java
+++ camel-core/src/test/java/org/apache/camel/impl/MultipleLifecycleStrategyTest.java
@@ -266,6 +266,8 @@ literal|"onServiceAdd"
 argument_list|,
 literal|"onServiceAdd"
 argument_list|,
+literal|"onServiceAdd"
+argument_list|,
 literal|"onComponentAdd"
 argument_list|,
 literal|"onEndpointAdd"
