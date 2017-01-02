039d5c4a18c104e990015650afbbaa9150cb68f4
lburgazzoli <lburgazzoli@gmail.com>
2017-01-02 17:19:16 +0100

    Fix CS
    Former-commit-id: 11a78fd8d50174cfd27bdcba13d0ce3711971f2c

--- components/camel-xmljson/src/test/java/org/apache/camel/dataformat/xmljson/XmlJsonDataFormatTest.java
+++ components/camel-xmljson/src/test/java/org/apache/camel/dataformat/xmljson/XmlJsonDataFormatTest.java
@@ -90,18 +90,6 @@ begin_import
 import|import
 name|org
 operator|.
-name|apache
-operator|.
-name|camel
-operator|.
-name|Exchange
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
 name|w3c
 operator|.
 name|dom
@@ -155,6 +143,18 @@ operator|.
 name|json
 operator|.
 name|JSONSerializer
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|camel
+operator|.
+name|Exchange
 import|;
 end_import
 
