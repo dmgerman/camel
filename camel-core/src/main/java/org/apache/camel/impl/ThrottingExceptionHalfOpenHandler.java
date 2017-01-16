6aae0c444d8be8dd472e44aad28b3b7940ff4b14
CodeSmell <mbarlotta@gmail.com>
2017-01-16 10:24:22 -0500

    Add Apache License to classes
    Former-commit-id: 72438fd8140b531fb633e9d22760fffd8bcb59bb

--- camel-core/src/main/java/org/apache/camel/impl/ThrottingExceptionHalfOpenHandler.java
+++ camel-core/src/main/java/org/apache/camel/impl/ThrottingExceptionHalfOpenHandler.java
@@ -1,4 +1,8 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+
 begin_package
 DECL|package|org.apache.camel.impl
 package|package
@@ -12,12 +16,17 @@ name|impl
 package|;
 end_package
 
+begin_comment
+comment|/**  * used by the {@link ThrottlingExceptionRoutePolicy} to allow custom code  * to handle the half open circuit state and how to determine if a route  * should be closed  *  */
+end_comment
+
 begin_interface
 DECL|interface|ThrottingExceptionHalfOpenHandler
 specifier|public
 interface|interface
 name|ThrottingExceptionHalfOpenHandler
 block|{
+comment|/**      * check the state of the Camel route      * @return true to close the route and false to leave open      */
 DECL|method|isReadyToBeClosed ()
 name|boolean
 name|isReadyToBeClosed
