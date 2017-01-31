889a7e685076e1a7283248f860b1be366580886b
Nicola Ferraro <ni.ferraro@gmail.com>
2017-01-31 11:33:49 +0100

    CAMEL-10612: forwarding lifecycle event
    Former-commit-id: ce80e9bbad2e5a44ab1b5aa1143c2f67b5a7c79b

---
+++ components/camel-reactive-streams/src/main/java/org/apache/camel/component/reactive/streams/ReactiveStreamsConstants.java
@@ -0,0 +1,53 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+
+begin_package
+DECL|package|org.apache.camel.component.reactive.streams
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|camel
+operator|.
+name|component
+operator|.
+name|reactive
+operator|.
+name|streams
+package|;
+end_package
+
+begin_comment
+comment|/**  * Useful constants used in the Camel Reactive Streams component.  */
+end_comment
+
+begin_class
+DECL|class|ReactiveStreamsConstants
+specifier|public
+specifier|final
+class|class
+name|ReactiveStreamsConstants
+block|{
+comment|/**      * Every exchange consumed by Camel has this header set to indicate if the exchange      * contains an item (value="onNext"), an error (value="onError") or a completion event (value="onComplete").      * Errors and completion notification are not forwarded by default.      */
+DECL|field|REACTIVE_STREAMS_EVENT_TYPE
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|REACTIVE_STREAMS_EVENT_TYPE
+init|=
+literal|"CamelReactiveStreamsEventType"
+decl_stmt|;
+DECL|method|ReactiveStreamsConstants ()
+specifier|private
+name|ReactiveStreamsConstants
+parameter_list|()
+block|{     }
+block|}
+end_class
+
+end_unit
+
