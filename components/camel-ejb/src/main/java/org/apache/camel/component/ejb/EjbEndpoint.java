a733b652db6ab8e52f35414d160fb2e197b6b907
Claus Ibsen <davsclaus@apache.org>
2017-02-02 14:21:31 +0100

    CAMEL-10775: Add information to components which version they were added to Camel
    Former-commit-id: 163f1e0d1374858ad108691fff164aaa86fb2941

--- components/camel-ejb/src/main/java/org/apache/camel/component/ejb/EjbEndpoint.java
+++ components/camel-ejb/src/main/java/org/apache/camel/component/ejb/EjbEndpoint.java
@@ -68,6 +68,10 @@ begin_class
 annotation|@
 name|UriEndpoint
 argument_list|(
+name|firstVersion
+operator|=
+literal|"2.4.0"
+argument_list|,
 name|scheme
 operator|=
 literal|"ejb"
--- components/camel-reactive-streams/src/main/java/org/apache/camel/component/reactive/streams/ReactiveStreamsEndpoint.java
+++ components/camel-reactive-streams/src/main/java/org/apache/camel/component/reactive/streams/ReactiveStreamsEndpoint.java
@@ -120,6 +120,10 @@ begin_class
 annotation|@
 name|UriEndpoint
 argument_list|(
+name|firstVersion
+operator|=
+literal|"2.19.0"
+argument_list|,
 name|scheme
 operator|=
 literal|"reactive-streams"
