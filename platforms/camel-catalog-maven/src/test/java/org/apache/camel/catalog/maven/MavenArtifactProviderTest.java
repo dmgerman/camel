863f3c7768a1326073901082970f9b461c15e75f
Claus Ibsen <davsclaus@apache.org>
2017-03-04 09:51:06 +0100

    Fixed tests
    Former-commit-id: f361fb772593dfc79cc9150eda341a85a15f4c00

--- platforms/camel-catalog-maven/src/test/java/org/apache/camel/catalog/maven/MavenArtifactProviderTest.java
+++ platforms/camel-catalog-maven/src/test/java/org/apache/camel/catalog/maven/MavenArtifactProviderTest.java
@@ -130,11 +130,26 @@ name|org
 operator|.
 name|junit
 operator|.
+name|Ignore
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
 name|Test
 import|;
 end_import
 
 begin_class
+annotation|@
+name|Ignore
+argument_list|(
+literal|"Cannot run on CI servers so run manually"
+argument_list|)
 DECL|class|MavenArtifactProviderTest
 specifier|public
 class|class
