02e320015ad4c5aa729b924632256c0ec9b9dfcd
Paolo Antinori <pantinor@redhat.com>
2017-03-13 13:44:50 +0100

    CAMEL-11002 - Camel-Blueprint, prevent JMX re-registration in case of failed Context
    Former-commit-id: 274ae6827f5776de555add1ba3afd546e7c29a3e

--- components/camel-blueprint/src/main/java/org/apache/camel/blueprint/BlueprintCamelContext.java
+++ components/camel-blueprint/src/main/java/org/apache/camel/blueprint/BlueprintCamelContext.java
@@ -68,18 +68,6 @@ name|apache
 operator|.
 name|camel
 operator|.
-name|FailedToCreateRouteException
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|camel
-operator|.
 name|TypeConverter
 import|;
 end_import
@@ -1284,7 +1272,7 @@ expr_stmt|;
 block|}
 catch|catch
 parameter_list|(
-name|FailedToCreateRouteException
+name|Exception
 name|e
 parameter_list|)
 block|{
@@ -1295,6 +1283,9 @@ argument_list|(
 literal|false
 argument_list|)
 expr_stmt|;
+throw|throw
+name|e
+throw|;
 block|}
 finally|finally
 block|{
