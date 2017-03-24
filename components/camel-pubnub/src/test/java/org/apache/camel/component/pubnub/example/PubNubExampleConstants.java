706e72e71547deb575a91f0cc0a6b0a584073ed0
Preben Asmussen <preben.asmussen@gmail.com>
2017-03-24 22:49:13 +0100

    initial commit of camel-pubnub component
    Former-commit-id: 289a67285a21e6f48cfd8857953ba88b1a4993e9

---
+++ components/camel-pubnub/src/test/java/org/apache/camel/component/pubnub/example/PubNubExampleConstants.java
@@ -0,0 +1,47 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+
+begin_package
+DECL|package|org.apache.camel.component.pubnub.example
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|camel
+operator|.
+name|component
+operator|.
+name|pubnub
+operator|.
+name|example
+package|;
+end_package
+
+begin_interface
+DECL|interface|PubNubExampleConstants
+specifier|public
+interface|interface
+name|PubNubExampleConstants
+block|{
+comment|// replace subscriber+publisher key with one obtained from PubNub.
+comment|// http://www.pubnub.com
+DECL|field|PUBNUB_SUBSCRIBER_KEY
+name|String
+name|PUBNUB_SUBSCRIBER_KEY
+init|=
+literal|"mysubkey"
+decl_stmt|;
+DECL|field|PUBNUB_PUBLISHER_KEY
+name|String
+name|PUBNUB_PUBLISHER_KEY
+init|=
+literal|"mypubkey"
+decl_stmt|;
+block|}
+end_interface
+
+end_unit
+
