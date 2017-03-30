32fdeed31a2d56453f4d797d432fb691042dbb3b
lburgazzoli <lburgazzoli@gmail.com>
2017-03-30 18:04:04 +0200

    Fix CS
    Former-commit-id: 891dc979df0f0d49d53cd4a26bd88cc953736673

--- camel-core/src/test/java/org/apache/camel/LoggingLevelTest.java
+++ camel-core/src/test/java/org/apache/camel/LoggingLevelTest.java
@@ -34,16 +34,6 @@ name|Test
 import|;
 end_import
 
-begin_import
-import|import
-name|org
-operator|.
-name|junit
-operator|.
-name|Test
-import|;
-end_import
-
 begin_class
 DECL|class|LoggingLevelTest
 specifier|public
