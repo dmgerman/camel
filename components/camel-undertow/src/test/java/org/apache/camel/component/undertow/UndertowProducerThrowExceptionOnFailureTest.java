8f01a144dfd78dc37de03ae72ed9e2e59ec03e7f
Claus Ibsen <davsclaus@apache.org>
2017-04-07 13:23:52 +0200

    CAMEL-11111: Disabled test until its implemented
    Former-commit-id: 2f3b023e5ad12a1d068bd2ebcd5189ed0f20ec5f

--- components/camel-undertow/src/test/java/org/apache/camel/component/undertow/UndertowProducerThrowExceptionOnFailureTest.java
+++ components/camel-undertow/src/test/java/org/apache/camel/component/undertow/UndertowProducerThrowExceptionOnFailureTest.java
@@ -50,6 +50,16 @@ name|org
 operator|.
 name|junit
 operator|.
+name|Ignore
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
 name|Test
 import|;
 end_import
@@ -67,6 +77,11 @@ import|;
 end_import
 
 begin_class
+annotation|@
+name|Ignore
+argument_list|(
+literal|"CAMEL-11111"
+argument_list|)
 DECL|class|UndertowProducerThrowExceptionOnFailureTest
 specifier|public
 class|class
