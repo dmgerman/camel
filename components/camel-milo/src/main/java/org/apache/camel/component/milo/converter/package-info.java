e5a17c81186937beb225353ffeaf14a6bf87f0a0
Jens Reimann <jreimann@redhat.com>
2017-04-10 15:48:30 +0200

    Initial import of Camel Milo
    Signed-off-by: Jens Reimann <jreimann@redhat.com>
    
    Former-commit-id: 7b3837fae7acf115885da3ea3958b79042240d26

---
+++ components/camel-milo/src/main/java/org/apache/camel/component/milo/converter/package-info.java
@@ -0,0 +1,24 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/**  * Data conversion  */
+end_comment
+
+begin_package
+DECL|package|org.apache.camel.component.milo.converter
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|camel
+operator|.
+name|component
+operator|.
+name|milo
+operator|.
+name|converter
+package|;
+end_package
+
+end_unit
+
