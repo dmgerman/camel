a51e68b9dceffea42524fb68a169db94bf51a901
Thomas Demande <tdemande@gmail.com>
2017-07-01 15:19:14 +0200

    CAMEL-11489: Remove mandatory nature of amazonS3Client when accessKey and secretKey are provided
    Former-commit-id: 819f8f4eb61d6b548dbf93a434fc69a7506c24ff

--- components/camel-aws/src/main/java/org/apache/camel/component/aws/s3/S3Component.java
+++ components/camel-aws/src/main/java/org/apache/camel/component/aws/s3/S3Component.java
@@ -215,13 +215,29 @@ name|getAmazonS3Client
 argument_list|()
 operator|==
 literal|null
+operator|&&
+operator|(
+name|configuration
+operator|.
+name|getAccessKey
+argument_list|()
+operator|==
+literal|null
+operator|||
+name|configuration
+operator|.
+name|getSecretKey
+argument_list|()
+operator|==
+literal|null
+operator|)
 condition|)
 block|{
 throw|throw
 operator|new
 name|IllegalArgumentException
 argument_list|(
-literal|"AmazonS3Client must be specified"
+literal|"AmazonS3Client or accessKey and secretKey must be specified"
 argument_list|)
 throw|;
 block|}
