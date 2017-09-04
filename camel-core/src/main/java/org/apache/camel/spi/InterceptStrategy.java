586d4d877b44254e2adda6672337d18c4bd0d95f
Claus Ibsen <davsclaus@apache.org>
2017-09-04 09:04:44 +0200

    Add javadoc that this API is not for Camel end users
    Former-commit-id: 917ad4bcfe86e69770e91d87f33ec9a0a6ed82b0

--- camel-core/src/main/java/org/apache/camel/spi/InterceptStrategy.java
+++ camel-core/src/main/java/org/apache/camel/spi/InterceptStrategy.java
@@ -55,7 +55,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * The purpose of this interface is to allow an implementation to wrap  * processors in a route with interceptors.  For example, a possible  * usecase is to gather performance statistics at the processor's level.  *<p/>  * Its<b>strongly</b> adviced to use an {@link org.apache.camel.AsyncProcessor} as the returned wrapped  * {@link Processor} which ensures the interceptor works well with the asynchronous routing engine.  * You can use the {@link org.apache.camel.processor.DelegateAsyncProcessor} to easily return an  * {@link org.apache.camel.AsyncProcessor} and override the  * {@link org.apache.camel.AsyncProcessor#process(org.apache.camel.Exchange, org.apache.camel.AsyncCallback)} to  * implement your interceptor logic. And just invoke the super method to<b>continue</b> routing.  *  * @version   */
+comment|/**  * The purpose of this interface is to allow an implementation to wrap  * processors in a route with interceptors.  For example, a possible  * usecase is to gather performance statistics at the processor's level.  *<p/>  *<b>Important:</b> This SPI is not intended to be used by Camel end users.  *<p/>  * Its<b>strongly</b> adviced to use an {@link org.apache.camel.AsyncProcessor} as the returned wrapped  * {@link Processor} which ensures the interceptor works well with the asynchronous routing engine.  * You can use the {@link org.apache.camel.processor.DelegateAsyncProcessor} to easily return an  * {@link org.apache.camel.AsyncProcessor} and override the  * {@link org.apache.camel.AsyncProcessor#process(org.apache.camel.Exchange, org.apache.camel.AsyncCallback)} to  * implement your interceptor logic. And just invoke the super method to<b>continue</b> routing.  *  * @version   */
 end_comment
 
 begin_interface
@@ -64,6 +64,7 @@ specifier|public
 interface|interface
 name|InterceptStrategy
 block|{
+comment|// TODO: Camel 3.0 make this an internal API
 comment|/**      * This method is invoked by      * {@link ProcessorDefinition#wrapProcessor(RouteContext, Processor)}      * to give the implementor an opportunity to wrap the target processor      * in a route.      *<p/>      *<b>Important:</b> See the class javadoc for advice on letting interceptor be compatible with the      * asynchronous routing engine.      *      * @param context       Camel context      * @param definition    the model this interceptor represents      * @param target        the processor to be wrapped      * @param nextTarget    the next processor to be routed to      * @return processor    wrapped with an interceptor or not wrapped.      * @throws Exception can be thrown      */
 DECL|method|wrapProcessorInInterceptors (CamelContext context, ProcessorDefinition<?> definition, Processor target, Processor nextTarget)
 name|Processor
