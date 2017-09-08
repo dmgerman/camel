6f35cce2c797237033a102071357b933967f1dcc
Claus Ibsen <davsclaus@apache.org>
2017-09-08 15:32:43 +0200

    CAMEL-11765: camel-undertow - Consumer adds duplicate headers
    Former-commit-id: de223d4380b21c27b574ae155c63f817b302e841

--- components/camel-undertow/src/test/java/org/apache/camel/component/undertow/rest/RestUndertowProducerVerbUpperCaseTest.java
+++ components/camel-undertow/src/test/java/org/apache/camel/component/undertow/rest/RestUndertowProducerVerbUpperCaseTest.java
@@ -80,26 +80,11 @@ name|org
 operator|.
 name|junit
 operator|.
-name|Ignore
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|junit
-operator|.
 name|Test
 import|;
 end_import
 
 begin_class
-annotation|@
-name|Ignore
-argument_list|(
-literal|"CAME-11765"
-argument_list|)
 DECL|class|RestUndertowProducerVerbUpperCaseTest
 specifier|public
 class|class
