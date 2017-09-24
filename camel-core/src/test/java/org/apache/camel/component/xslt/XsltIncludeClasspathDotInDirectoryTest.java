e3dc9a0334795735bd020d424a32e14381ba8df6
Claus Ibsen <davsclaus@apache.org>
2017-09-24 12:23:09 +0200

    Do not include test data in classes as it will be included in the final camel-core JAR
    Former-commit-id: 4e7da3f3353f0455a49df71070dc2f8d7dcca721

--- camel-core/src/test/java/org/apache/camel/component/xslt/XsltIncludeClasspathDotInDirectoryTest.java
+++ camel-core/src/test/java/org/apache/camel/component/xslt/XsltIncludeClasspathDotInDirectoryTest.java
@@ -157,6 +157,27 @@ name|setUp
 argument_list|()
 expr_stmt|;
 block|}
+annotation|@
+name|Override
+DECL|method|tearDown ()
+specifier|protected
+name|void
+name|tearDown
+parameter_list|()
+throws|throws
+name|Exception
+block|{
+name|deleteDirectory
+argument_list|(
+literal|"target/classes/com.mycompany"
+argument_list|)
+expr_stmt|;
+name|super
+operator|.
+name|tearDown
+argument_list|()
+expr_stmt|;
+block|}
 DECL|method|testXsltIncludeClasspath ()
 specifier|public
 name|void
