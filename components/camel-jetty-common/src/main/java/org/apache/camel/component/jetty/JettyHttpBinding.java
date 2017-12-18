78528263244528232525ea287e987d6196a0c693
Claus Ibsen <claus.ibsen@gmail.com>
2017-12-18 18:20:45 +0100

    CAMEL-12074: Let okStatusCodeRange support multiple ranges. Thanks to Niels Ull Harremoes for the patch.
    Former-commit-id: 45b01813edf3531efcb07ac509e348abb0b772a3

--- components/camel-jetty-common/src/main/java/org/apache/camel/component/jetty/JettyHttpBinding.java
+++ components/camel-jetty-common/src/main/java/org/apache/camel/component/jetty/JettyHttpBinding.java
@@ -128,13 +128,13 @@ name|boolean
 name|isAllowJavaSerializedObject
 parameter_list|()
 function_decl|;
-comment|/**      * The status codes which is considered a success response. The values are inclusive. The range must be defined as from-to with the dash included.      *<p/>      * The default range is<tt>200-299</tt>      */
+comment|/**      * The status codes which are considered a success response. The values are inclusive. Multiple ranges can be      * defined, separated by comma, e.g.<tt>200-204,209,301-304</tt>. Each range must be a single number or from-to      * with the dash included.      *<p/>      * The default range is<tt>200-299</tt>      */
 DECL|method|getOkStatusCodeRange ()
 name|String
 name|getOkStatusCodeRange
 parameter_list|()
 function_decl|;
-comment|/**      * The status codes which is considered a success response. The values are inclusive. The range must be defined as from-to with the dash included.      *<p/>      * The default range is<tt>200-299</tt>      */
+comment|/**      * The status codes which are considered a success response. The values are inclusive. Multiple ranges can be      * defined, separated by comma, e.g.<tt>200-204,209,301-304</tt>. Each range must be a single number or from-to      * with the dash included.      *<p/>      * The default range is<tt>200-299</tt>      */
 DECL|method|setOkStatusCodeRange (String okStatusCodeRange)
 name|void
 name|setOkStatusCodeRange
