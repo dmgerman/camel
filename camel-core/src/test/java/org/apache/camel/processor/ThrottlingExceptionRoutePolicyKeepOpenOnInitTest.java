d20e5f28f106800a5c5cf7a008049889f5daafd6
CodeSmell <mbarlotta@gmail.com>
2018-01-09 08:54:30 -0500

    address PR comments
    Former-commit-id: 4f65a942465d82acea52a5012c00bec81d1183e6

--- camel-core/src/test/java/org/apache/camel/processor/ThrottlingExceptionRoutePolicyKeepOpenOnInitTest.java
+++ camel-core/src/test/java/org/apache/camel/processor/ThrottlingExceptionRoutePolicyKeepOpenOnInitTest.java
@@ -1,4 +1,8 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+
 begin_package
 DECL|package|org.apache.camel.processor
 package|package
@@ -186,7 +190,7 @@ decl_stmt|;
 name|long
 name|halfOpenAfter
 init|=
-literal|1000
+literal|100
 decl_stmt|;
 name|boolean
 name|keepOpen
@@ -267,7 +271,7 @@ name|Thread
 operator|.
 name|sleep
 argument_list|(
-literal|2000
+literal|500
 argument_list|)
 expr_stmt|;
 comment|// gives time for policy half open check to run every second
@@ -340,7 +344,7 @@ name|Thread
 operator|.
 name|sleep
 argument_list|(
-literal|2000
+literal|500
 argument_list|)
 expr_stmt|;
 name|result
--- camel-core/src/test/java/org/apache/camel/processor/ThrottlingExceptionRoutePolicyOpenViaConfigTest.java
+++ camel-core/src/test/java/org/apache/camel/processor/ThrottlingExceptionRoutePolicyOpenViaConfigTest.java
@@ -1,4 +1,8 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+
 begin_package
 DECL|package|org.apache.camel.processor
 package|package
@@ -186,7 +190,7 @@ decl_stmt|;
 name|long
 name|halfOpenAfter
 init|=
-literal|1000
+literal|100
 decl_stmt|;
 name|boolean
 name|keepOpen
@@ -267,7 +271,7 @@ name|result
 operator|.
 name|setResultWaitTime
 argument_list|(
-literal|2000
+literal|1000
 argument_list|)
 expr_stmt|;
 name|assertMockEndpointsSatisfied
@@ -297,7 +301,7 @@ name|Thread
 operator|.
 name|sleep
 argument_list|(
-literal|1000
+literal|500
 argument_list|)
 expr_stmt|;
 comment|// send next set of messages
@@ -342,7 +346,7 @@ name|Thread
 operator|.
 name|sleep
 argument_list|(
-literal|2000
+literal|500
 argument_list|)
 expr_stmt|;
 name|result
@@ -358,7 +362,7 @@ name|result
 operator|.
 name|setResultWaitTime
 argument_list|(
-literal|2000
+literal|1000
 argument_list|)
 expr_stmt|;
 name|assertMockEndpointsSatisfied
@@ -389,7 +393,7 @@ name|result
 operator|.
 name|setResultWaitTime
 argument_list|(
-literal|2000
+literal|1000
 argument_list|)
 expr_stmt|;
 name|assertMockEndpointsSatisfied
@@ -420,35 +424,6 @@ parameter_list|()
 throws|throws
 name|Exception
 block|{
-name|int
-name|threshold
-init|=
-literal|2
-decl_stmt|;
-name|long
-name|failureWindow
-init|=
-literal|30
-decl_stmt|;
-name|long
-name|halfOpenAfter
-init|=
-literal|1000
-decl_stmt|;
-name|policy
-operator|=
-operator|new
-name|ThrottlingExceptionRoutePolicy
-argument_list|(
-name|threshold
-argument_list|,
-name|failureWindow
-argument_list|,
-name|halfOpenAfter
-argument_list|,
-literal|null
-argument_list|)
-expr_stmt|;
 name|from
 argument_list|(
 name|url
