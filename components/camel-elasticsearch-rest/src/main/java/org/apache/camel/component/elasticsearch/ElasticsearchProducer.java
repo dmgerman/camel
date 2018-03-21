521e986a7770181e91e6d41ea298a774c1a4de21
Andrea Cosentino <ancosen@gmail.com>
2018-03-21 10:51:40 +0100

    Polished
    Former-commit-id: 2821cb7eb27ba627351173446e6f5917176858ac

--- components/camel-elasticsearch-rest/src/main/java/org/apache/camel/component/elasticsearch/ElasticsearchProducer.java
+++ components/camel-elasticsearch-rest/src/main/java/org/apache/camel/component/elasticsearch/ElasticsearchProducer.java
@@ -76,16 +76,6 @@ name|java
 operator|.
 name|util
 operator|.
-name|Map
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|util
-operator|.
 name|stream
 operator|.
 name|Collectors
