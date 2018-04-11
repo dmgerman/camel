96338f584dc8098328f8638e9938733ca3d60594
Claus Ibsen <claus.ibsen@gmail.com>
2018-04-11 16:29:31 +0200

    CAMEL-12427: camel-netty4 - Add SPI to plugin custom correlation state for request/reply in producer
    Former-commit-id: 6f99ee6c48c757414aa5b6aaf29556a6b81115ac

--- platforms/spring-boot/components-starter/camel-netty4-starter/src/main/java/org/apache/camel/component/netty4/springboot/NettyComponentConfiguration.java
+++ platforms/spring-boot/components-starter/camel-netty4-starter/src/main/java/org/apache/camel/component/netty4/springboot/NettyComponentConfiguration.java
@@ -746,7 +746,7 @@ name|producerPoolMinEvictableIdle
 init|=
 literal|300000L
 decl_stmt|;
-comment|/**          * Whether producer pool is enabled or not. Important: Do not turn this          * off, as the pooling is needed for handling concurrency and reliable          * request/reply.          */
+comment|/**          * Whether producer pool is enabled or not. Important: If you turn this          * off then a single shared connection is used for the producer, also if          * you are doing request/reply. That means there is a potential issue          * with interleaved responses if replies comes back out-of-order.          * Therefore you need to have a correlation id in both the request and          * reply messages so you can properly correlate the replies to the Camel          * callback that is responsible for continue processing the message in          * Camel. To do this you need to implement          * {@link NettyCamelStateCorrelationManager} as correlation manager and          * configure it via the<tt>correlationManager</tt> option.          *<p/>          * See also the<tt>correlationManager</tt> option for more details.          */
 DECL|field|producerPoolEnabled
 specifier|private
 name|Boolean
@@ -794,7 +794,7 @@ name|reuseChannel
 init|=
 literal|false
 decl_stmt|;
-comment|/**          * To use a custom correlation manager to manage how request and reply          * messages are mapped when using request/reply with the netty producer.          * This should only be used if you have a way to map requests together          * with replies such as if there is correlation ids in both the request          * and reply messages. This can be used if you want to multiplex          * concurrent messages on the same channel (aka connection) in netty.          * When doing this you must have a way to correlate the request and          * reply messages so you can store the right reply on the inflight Camel          * Exchange before its continued routed.          */
+comment|/**          * To use a custom correlation manager to manage how request and reply          * messages are mapped when using request/reply with the netty producer.          * This should only be used if you have a way to map requests together          * with replies such as if there is correlation ids in both the request          * and reply messages. This can be used if you want to multiplex          * concurrent messages on the same channel (aka connection) in netty.          * When doing this you must have a way to correlate the request and          * reply messages so you can store the right reply on the inflight Camel          * Exchange before its continued routed.          *<p/>          * See also the<tt>producerPoolEnabled</tt> option for more details.          */
 DECL|field|correlationManager
 specifier|private
 name|NettyCamelStateCorrelationManager
