ac72d0e7e5ababa6da5dc758d9374a6fb52f43ca
William Collins <wcollins@redhat.com>
2018-04-19 03:27:06 -0400

    [CAMEL-11257] First Phase of AS2 Camel Component (#2300)
    * [CAMEL-11257] Initial commit AS2 Component
    
    * [CAMEL-11257] Refactored connection logic into 
    AS2 client and server connection objects
    
    * Updated Camel version for AS2 Component
    
    * [CAMEL-11257] Updated Integration Tests
    
    * [ENTESB-6202] Refactored client and server connection
    code and added api unit test.
    
    * [CAMEL-11257] Added unit test
    
    * [CAMEL-11257] Updated api layer to receive parameters via HTTP context.
    
    * [CAMEL-11257] Updated AS2MessageTest
    
    * [CAMEL-11257] Updated unit tests
    
    * [CAMEL-11257] Updated Client Send Test, refactored Application EDI Entity parsing and added partial implementation of Multipart Signed Entity parsing.
    
    * [CAMEL-11257] Partial implementation of signature validation
    
    * [CAMEL-11257] Fixes and testing of signature validation
    
    * [CAMEL-11257] Fixes for signature validation
    
    * [CAMEL-11257] Refactored Entity parsing logic
    
    * [CAMEL-11257] Updated tests and configuration.
    
    * [CAMEL-11257] Updated component tests
    
    * [CAMEL-11257] Refactored ConnectionHelper and updated Server Manager Integration test.
    
    * [CAMEL-11257] Updated Server Manager integration tests.
    
    * [CAMEL-11257] Added MDN request protocol support
    
    * [CAMEL-11257] Initial work on MDN entities
    
    * [CAMEL-11257] Continuing work on MDN entities
    
    * [CAMEL-11257] Continuing work on MDN entities
    
    * [CAMEL-11257] Continuing work on MDN entities
    
    * [CAMEL-11257] Continuing work on MDN entities
    
    * [CAMEL-11257] Continuing work on MDN entities
    
    * [CAMEL-11257] Added unit test.
    
    * [CAMEL-11257] Added and updated unit tests.
    
    * [CAMEL-11257] Adding MDN response handling to AS2 Server connections
    
    * [CAMEL-11257] Further work on MDN response handling for AS2 Server connections
    
    * [CAMEL-11257] Further work on MDN response handling for AS2 Server connections and refactoring
    
    * [CAMEL-11257] Further work on MDN response handling for AS2 Server connections
    
    * [CAMEL-11257] Added MDN entity parsing logic
    
    * [CAMEL-11257] Updated Disposition Notification Content Utils Tests
    
    * [CAMEL-11257] Refactored MDN parsing logic
    
    * [CAMEL-11257] Added Entity Parser Test
    
    * [CAMEL-11257] Updating component integration tests
    
    * [CAMEL-11257] Updating component integration tests
    
    * [CAMEL-11257] Updating component integration tests
    
    * [CAMEL-11257] Fixed unit test.
    
    * [CAMEL-11257] Further refactoring
    
    * [CAMEL-11257] Further refactoring
    
    * [CAMEL-11257] Further refactoring
    
    * [CAMEL-11257] Moved HTTP connection class to io package.
    
    * [CAMEL-11257] Adding initial code for signed receipts
    
    * [CAMEL-11257] Added code for signed receipts
    
    * [CAMEL-11257] Clean up after rebase with upstream.
    
    * [CAMEL-11257] Checkstyle clean up
    
    * [CAMEL-11257] Updated poms
    
    * [CAMEL-11257] Added comments as placeholders for future code.
    
    
    Former-commit-id: 486bae5cda200f4c8b0e74f18ed25af3b4bbb988

---
+++ components/camel-as2/camel-as2-api/src/main/java/org/apache/camel/component/as2/api/AS2Constants.java
@@ -0,0 +1,169 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+
+begin_package
+DECL|package|org.apache.camel.component.as2.api
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|camel
+operator|.
+name|component
+operator|.
+name|as2
+operator|.
+name|api
+package|;
+end_package
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|http
+operator|.
+name|protocol
+operator|.
+name|HttpCoreContext
+import|;
+end_import
+
+begin_comment
+comment|/**  * Constants for AS2 component.  */
+end_comment
+
+begin_interface
+DECL|interface|AS2Constants
+specifier|public
+interface|interface
+name|AS2Constants
+block|{
+comment|/**      * The Value of User Agent Header used by AS2 Camel Component.      */
+DECL|field|HTTP_USER_AGENT
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|HTTP_USER_AGENT
+init|=
+literal|"Camel AS2 Component"
+decl_stmt|;
+comment|/**      * The Value of Origin Server Header used by AS2 Camel Component.      */
+DECL|field|HTTP_ORIGIN_SERVER
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|HTTP_ORIGIN_SERVER
+init|=
+literal|"Camel AS2 Component"
+decl_stmt|;
+comment|/**      * Fully Qualified Domain Name used by AS2 Camel Component in Message ID Header.      */
+DECL|field|HTTP_MESSAGE_ID_FQDN
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|HTTP_MESSAGE_ID_FQDN
+init|=
+literal|"camel.apache.org"
+decl_stmt|;
+comment|/**      * The Value of User Agent Header used by AS2 Camel Component.      */
+DECL|field|MIME_VERSION
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|MIME_VERSION
+init|=
+literal|"1.0"
+decl_stmt|;
+comment|//
+comment|// HTTP Context Attribute Names
+comment|//
+comment|/**      * HTTP Context Attribute Name for HTTP Client Connection object stored in context.      */
+DECL|field|HTTP_CLIENT_CONNECTION
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|HTTP_CLIENT_CONNECTION
+init|=
+name|HttpCoreContext
+operator|.
+name|HTTP_CONNECTION
+decl_stmt|;
+comment|/**      * HTTP Context Attribute Name for HTTP Client Processor object stored in context.      */
+DECL|field|HTTP_CLIENT_PROCESSOR
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|HTTP_CLIENT_PROCESSOR
+init|=
+literal|"http.processor"
+decl_stmt|;
+comment|/**      * HTTP Context Attribute Name for HTTP Client Fully Qualified Domain Name (FQDN) stored in context.      */
+DECL|field|HTTP_CLIENT_FQDN
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|HTTP_CLIENT_FQDN
+init|=
+literal|"client.fqdn"
+decl_stmt|;
+comment|/**      * HTTP Context Attribute Name for HTTP Server Connection object stored in context.      */
+DECL|field|HTTP_SERVER_CONNECTION
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|HTTP_SERVER_CONNECTION
+init|=
+literal|"http.server.connection"
+decl_stmt|;
+comment|/**      * HTTP Context Attribute Name for HTTP Server Processor object stored in context.      */
+DECL|field|HTTP_SERVER_PROCESSOR
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|HTTP_SERVER_PROCESSOR
+init|=
+literal|"http.server.processor"
+decl_stmt|;
+comment|/**      * HTTP Context Attribute Name for HTTP Server Service object stored in context.      */
+DECL|field|HTTP_SERVER_SERVICE
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|HTTP_SERVER_SERVICE
+init|=
+literal|"http.server.service"
+decl_stmt|;
+comment|//
+comment|// AS2 MIME Content Types
+comment|//
+comment|/**      * Application EDIFACT content type      */
+DECL|field|APPLICATION_EDIFACT_MIME_TYPE
+specifier|public
+specifier|static
+specifier|final
+name|String
+name|APPLICATION_EDIFACT_MIME_TYPE
+init|=
+literal|"Application/EDIFACT"
+decl_stmt|;
+block|}
+end_interface
+
+end_unit
+
