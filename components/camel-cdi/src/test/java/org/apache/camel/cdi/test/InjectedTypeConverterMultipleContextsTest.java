d589a7e9b8114f2f14751e54bf8374580f81c1e3
Claus Ibsen <claus.ibsen@gmail.com>
2018-08-17 11:19:11 +0200

    CAMEL-12739: Fixed CS
    Former-commit-id: cd7b5bb65f7d7caebee4c9a30f6609b7b5afca3a

--- components/camel-cdi/src/test/java/org/apache/camel/cdi/test/InjectedTypeConverterMultipleContextsTest.java
+++ components/camel-cdi/src/test/java/org/apache/camel/cdi/test/InjectedTypeConverterMultipleContextsTest.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *<p>  * http://www.apache.org/licenses/LICENSE-2.0  *<p>  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -17,6 +17,42 @@ operator|.
 name|test
 package|;
 end_package
+
+begin_import
+import|import
+name|java
+operator|.
+name|util
+operator|.
+name|concurrent
+operator|.
+name|TimeUnit
+import|;
+end_import
+
+begin_import
+import|import
+name|javax
+operator|.
+name|enterprise
+operator|.
+name|context
+operator|.
+name|ApplicationScoped
+import|;
+end_import
+
+begin_import
+import|import
+name|javax
+operator|.
+name|enterprise
+operator|.
+name|inject
+operator|.
+name|Produces
+import|;
+end_import
 
 begin_import
 import|import
@@ -305,42 +341,6 @@ import|;
 end_import
 
 begin_import
-import|import
-name|javax
-operator|.
-name|enterprise
-operator|.
-name|context
-operator|.
-name|ApplicationScoped
-import|;
-end_import
-
-begin_import
-import|import
-name|javax
-operator|.
-name|enterprise
-operator|.
-name|inject
-operator|.
-name|Produces
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|util
-operator|.
-name|concurrent
-operator|.
-name|TimeUnit
-import|;
-end_import
-
-begin_import
 import|import static
 name|org
 operator|.
@@ -572,7 +572,7 @@ parameter_list|)
 throws|throws
 name|InterruptedException
 block|{
-name|_sendMessageToInbound
+name|sendMessageToInbound
 argument_list|(
 name|inbound
 argument_list|,
@@ -618,7 +618,7 @@ parameter_list|)
 throws|throws
 name|InterruptedException
 block|{
-name|_sendMessageToInbound
+name|sendMessageToInbound
 argument_list|(
 name|inbound
 argument_list|,
@@ -628,10 +628,10 @@ literal|"second"
 argument_list|)
 expr_stmt|;
 block|}
-DECL|method|_sendMessageToInbound (ProducerTemplate inbound, MockEndpoint outbound, String contextName)
+DECL|method|sendMessageToInbound (ProducerTemplate inbound, MockEndpoint outbound, String contextName)
 specifier|private
 name|void
-name|_sendMessageToInbound
+name|sendMessageToInbound
 parameter_list|(
 name|ProducerTemplate
 name|inbound
