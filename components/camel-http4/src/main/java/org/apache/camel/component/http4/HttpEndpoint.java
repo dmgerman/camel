699efd0a4f42be093b6044e2c5cac871ccdf566e
Bob Paulin <bob@bobpaulin.com>
2018-08-29 22:24:43 -0500

    CAMEL-12751 - Default http4 producer to ignore content-length header.
    Former-commit-id: 2147d7b0b504e5cc44484390c1b6761455ff2b5c

--- components/camel-http4/src/main/java/org/apache/camel/component/http4/HttpEndpoint.java
+++ components/camel-http4/src/main/java/org/apache/camel/component/http4/HttpEndpoint.java
@@ -810,6 +810,24 @@ specifier|private
 name|HostnameVerifier
 name|x509HostnameVerifier
 decl_stmt|;
+annotation|@
+name|UriParam
+argument_list|(
+name|label
+operator|=
+literal|"producer,proxy"
+argument_list|,
+name|description
+operator|=
+literal|"Ignore Content-Length Header"
+argument_list|)
+DECL|field|ignoreContentLengthHeader
+specifier|private
+name|boolean
+name|ignoreContentLengthHeader
+init|=
+literal|true
+decl_stmt|;
 DECL|method|HttpEndpoint ()
 specifier|public
 name|HttpEndpoint
@@ -1896,6 +1914,33 @@ operator|.
 name|socketTimeout
 operator|=
 name|socketTimeout
+expr_stmt|;
+block|}
+comment|/**      * Ignore Content-Length Header      */
+DECL|method|isIgnoreContentLengthHeader ()
+specifier|public
+name|boolean
+name|isIgnoreContentLengthHeader
+parameter_list|()
+block|{
+return|return
+name|ignoreContentLengthHeader
+return|;
+block|}
+DECL|method|setIgnoreContentLengthHeader (boolean ignoreContentLengthHeader)
+specifier|public
+name|void
+name|setIgnoreContentLengthHeader
+parameter_list|(
+name|boolean
+name|ignoreContentLengthHeader
+parameter_list|)
+block|{
+name|this
+operator|.
+name|ignoreContentLengthHeader
+operator|=
+name|ignoreContentLengthHeader
 expr_stmt|;
 block|}
 block|}
