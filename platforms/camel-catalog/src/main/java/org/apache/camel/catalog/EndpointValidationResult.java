9004a5dd2b2e163492ceb611db91cafca56d2951
Andrea Cosentino <ancosen@gmail.com>
2018-09-06 14:52:42 +0200

    CAMEL-12784 - Regen
    Former-commit-id: 4900e20e690078813a57f12427bce6556ab0795c

--- platforms/camel-catalog/src/main/java/org/apache/camel/catalog/EndpointValidationResult.java
+++ platforms/camel-catalog/src/main/java/org/apache/camel/catalog/EndpointValidationResult.java
@@ -17,6 +17,22 @@ package|;
 end_package
 
 begin_import
+import|import static
+name|org
+operator|.
+name|apache
+operator|.
+name|camel
+operator|.
+name|catalog
+operator|.
+name|URISupport
+operator|.
+name|isEmpty
+import|;
+end_import
+
+begin_import
 import|import
 name|java
 operator|.
@@ -93,22 +109,6 @@ operator|.
 name|util
 operator|.
 name|Set
-import|;
-end_import
-
-begin_import
-import|import static
-name|org
-operator|.
-name|apache
-operator|.
-name|camel
-operator|.
-name|catalog
-operator|.
-name|URISupport
-operator|.
-name|isEmpty
 import|;
 end_import
 
