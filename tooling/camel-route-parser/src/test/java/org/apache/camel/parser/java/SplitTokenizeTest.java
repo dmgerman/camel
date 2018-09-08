2e1a4a17d198c194dc7560507bdd99c4440e9bff
Claus Ibsen <claus.ibsen@gmail.com>
2018-09-08 17:35:21 +0200

    Fixed CS
    Former-commit-id: 9566b95ef4f7b0d0f3e4d366408b0949fdf481a7

--- tooling/camel-route-parser/src/test/java/org/apache/camel/parser/java/SplitTokenizeTest.java
+++ tooling/camel-route-parser/src/test/java/org/apache/camel/parser/java/SplitTokenizeTest.java
@@ -22,16 +22,6 @@ begin_import
 import|import
 name|org
 operator|.
-name|junit
-operator|.
-name|Test
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
 name|apache
 operator|.
 name|camel
@@ -71,6 +61,16 @@ operator|.
 name|junit4
 operator|.
 name|CamelTestSupport
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
+name|Test
 import|;
 end_import
 
