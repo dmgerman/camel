ea86cef30682e5a0a5c02e0b4ca535c2695d9d1c
Andrea Cosentino <ancosen@gmail.com>
2018-09-13 12:07:20 +0200

    CAMEL-12806 - Fixed CS
    Former-commit-id: d0c96158861d2a88b840802a468564082564d6cc

--- components/camel-slack/src/main/java/org/apache/camel/component/slack/SlackConsumer.java
+++ components/camel-slack/src/main/java/org/apache/camel/component/slack/SlackConsumer.java
@@ -19,26 +19,6 @@ package|;
 end_package
 
 begin_import
-import|import static
-name|org
-operator|.
-name|apache
-operator|.
-name|camel
-operator|.
-name|component
-operator|.
-name|slack
-operator|.
-name|utils
-operator|.
-name|SlackUtils
-operator|.
-name|readResponse
-import|;
-end_import
-
-begin_import
 import|import
 name|java
 operator|.
@@ -321,6 +301,26 @@ operator|.
 name|slf4j
 operator|.
 name|LoggerFactory
+import|;
+end_import
+
+begin_import
+import|import static
+name|org
+operator|.
+name|apache
+operator|.
+name|camel
+operator|.
+name|component
+operator|.
+name|slack
+operator|.
+name|utils
+operator|.
+name|SlackUtils
+operator|.
+name|readResponse
 import|;
 end_import
 
