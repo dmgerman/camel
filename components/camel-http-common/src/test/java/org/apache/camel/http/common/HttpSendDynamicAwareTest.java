91c512a5f8a2ca9a6493b958b6cc425a617dc64a
Claus Ibsen <claus.ibsen@gmail.com>
2018-09-23 16:26:09 +0200

    CAMEL-12827: Fixed CS
    Former-commit-id: dc16f483a4e380c373073af3d167d80bbe6c0828

--- components/camel-http-common/src/test/java/org/apache/camel/http/common/HttpSendDynamicAwareTest.java
+++ components/camel-http-common/src/test/java/org/apache/camel/http/common/HttpSendDynamicAwareTest.java
@@ -19,14 +19,18 @@ package|;
 end_package
 
 begin_import
-import|import static
+import|import
 name|org
 operator|.
-name|junit
+name|apache
 operator|.
-name|Assert
+name|camel
 operator|.
-name|*
+name|spi
+operator|.
+name|SendDynamicAware
+operator|.
+name|DynamicAwareEntry
 import|;
 end_import
 
@@ -51,18 +55,14 @@ import|;
 end_import
 
 begin_import
-import|import
+import|import static
 name|org
 operator|.
-name|apache
+name|junit
 operator|.
-name|camel
+name|Assert
 operator|.
-name|spi
-operator|.
-name|SendDynamicAware
-operator|.
-name|DynamicAwareEntry
+name|assertEquals
 import|;
 end_import
 
