d5ce02ff7f7f9212b0d2401a0f4df517d24ce84f
Guillaume Nodet <gnodet@gmail.com>
2018-10-03 10:38:46 +0200

    Typo
    # Conflicts:
    #	camel-core/src/main/java/org/apache/camel/model/TransactedDefinition.java
    
    
    Former-commit-id: 39ff2f8a300bd3517734964df8137e0a264344e9

--- camel-core/src/main/java/org/apache/camel/support/ProcessorEndpoint.java
+++ camel-core/src/main/java/org/apache/camel/support/ProcessorEndpoint.java
@@ -89,7 +89,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * An endpoint which allows exchanges to be sent into it which just invokes a  * given {@link Processor}. This component does not support the use of  * consumers.  *<p/>  *<br/>Implementors beware that this endpoint creates producers and consumers which  * do not allow full control of their lifecycle as {@link org.apache.camel.Service}  * or {@link org.apache.camel.SuspendableService} would do.  * If your producers/consumers need more control oover their lifecycle it is advised  * instead to extend {@link DefaultEndpoint}, {@link DefaultProducer}  * and {@link DefaultConsumer}.  */
+comment|/**  * An endpoint which allows exchanges to be sent into it which just invokes a  * given {@link Processor}. This component does not support the use of  * consumers.  *<p/>  *<br/>Implementors beware that this endpoint creates producers and consumers which  * do not allow full control of their lifecycle as {@link org.apache.camel.Service}  * or {@link org.apache.camel.SuspendableService} would do.  * If your producers/consumers need more control over their lifecycle it is advised  * instead to extend {@link DefaultEndpoint}, {@link DefaultProducer}  * and {@link DefaultConsumer}.  */
 end_comment
 
 begin_class
