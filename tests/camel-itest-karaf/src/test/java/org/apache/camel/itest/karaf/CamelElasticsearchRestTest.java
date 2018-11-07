9d2b7151cc9e6e7ebe33c4c6ad10dd32d1aea008
Andrea Cosentino <ancosen@gmail.com>
2018-11-07 11:35:44 +0100

    Since camel-elasticsearch-rest doesn't work in Karaf, skipping the Karaf itest
    Former-commit-id: e78364d272cab073da09715aae0d5473c051e7d7

--- tests/camel-itest-karaf/src/test/java/org/apache/camel/itest/karaf/CamelElasticsearchRestTest.java
+++ tests/camel-itest-karaf/src/test/java/org/apache/camel/itest/karaf/CamelElasticsearchRestTest.java
@@ -24,6 +24,16 @@ name|org
 operator|.
 name|junit
 operator|.
+name|Ignore
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
 name|Test
 import|;
 end_import
@@ -99,6 +109,8 @@ import|;
 end_import
 
 begin_class
+annotation|@
+name|Ignore
 annotation|@
 name|RunWith
 argument_list|(
