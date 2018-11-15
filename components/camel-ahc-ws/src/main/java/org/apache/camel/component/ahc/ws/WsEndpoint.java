1ae7fb8811b30a3879f3833699a3d2cc125763ab
Andrea Cosentino <ancosen@gmail.com>
2018-11-15 09:46:50 +0100

    CAMEL-12932 - Camel-AHC-WS: Consumer parameters are not set
    Former-commit-id: 5eb1d5fa37f80c0d56cbfe620f054e3252d016e1

--- components/camel-ahc-ws/src/main/java/org/apache/camel/component/ahc/ws/WsEndpoint.java
+++ components/camel-ahc-ws/src/main/java/org/apache/camel/component/ahc/ws/WsEndpoint.java
@@ -385,7 +385,9 @@ parameter_list|)
 throws|throws
 name|Exception
 block|{
-return|return
+name|WsConsumer
+name|consumer
+init|=
 operator|new
 name|WsConsumer
 argument_list|(
@@ -393,6 +395,14 @@ name|this
 argument_list|,
 name|processor
 argument_list|)
+decl_stmt|;
+name|configureConsumer
+argument_list|(
+name|consumer
+argument_list|)
+expr_stmt|;
+return|return
+name|consumer
 return|;
 block|}
 DECL|method|getWebSocket ()
