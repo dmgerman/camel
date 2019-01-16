0874745b77e786a3ef524bb082180ef53730e1c4
Dmitry Volodin <dmvolod@gmail.com>
2019-01-16 13:34:08 +0300

    CAMEL-13063: Olingo2Endpoint swallowing consumer. options Former-commit-id: d0d7e34ebf395ac2b01c1281db3d37e02add581f

--- components/camel-olingo2/camel-olingo2-component/src/main/java/org/apache/camel/component/olingo2/Olingo2Endpoint.java
+++ components/camel-olingo2/camel-olingo2-component/src/main/java/org/apache/camel/component/olingo2/Olingo2Endpoint.java
@@ -1064,6 +1064,19 @@ operator|.
 name|getKey
 argument_list|()
 decl_stmt|;
+comment|/**              * Avoid swallowing consumer scheduler properties, which              * get processed in configureProperties()              */
+if|if
+condition|(
+name|paramName
+operator|.
+name|startsWith
+argument_list|(
+literal|"consumer."
+argument_list|)
+condition|)
+block|{
+continue|continue;
+block|}
 if|if
 condition|(
 operator|!
