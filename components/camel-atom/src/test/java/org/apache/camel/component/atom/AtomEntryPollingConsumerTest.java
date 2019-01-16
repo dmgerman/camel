af16b82b582a54c8a73dee4abc04a776452f88be
Andrea Cosentino <ancosen@gmail.com>
2019-01-16 07:42:50 +0100

    Fixed CS for Camel-Atom
    Former-commit-id: ae72539a07bd8f618762412e5a7dbfc3ebbaa1d7

--- components/camel-atom/src/test/java/org/apache/camel/component/atom/AtomEntryPollingConsumerTest.java
+++ components/camel-atom/src/test/java/org/apache/camel/component/atom/AtomEntryPollingConsumerTest.java
@@ -76,11 +76,11 @@ name|apache
 operator|.
 name|camel
 operator|.
-name|test
+name|support
 operator|.
-name|junit4
+name|jndi
 operator|.
-name|CamelTestSupport
+name|JndiContext
 import|;
 end_import
 
@@ -92,11 +92,11 @@ name|apache
 operator|.
 name|camel
 operator|.
-name|support
+name|test
 operator|.
-name|jndi
+name|junit4
 operator|.
-name|JndiContext
+name|CamelTestSupport
 import|;
 end_import
 
