feb51770fb4c735e93fee197592abb1a4d4ad42d
Andrea Cosentino <ancosen@gmail.com>
2019-01-17 09:16:57 +0100

    Fixed CS for Camel-docker
    Former-commit-id: 00026ef1b16b51145c44200099f110dea62b5464

--- components/camel-docker/src/test/java/org/apache/camel/component/docker/it/DockerITTestSupport.java
+++ components/camel-docker/src/test/java/org/apache/camel/component/docker/it/DockerITTestSupport.java
@@ -102,11 +102,9 @@ name|apache
 operator|.
 name|camel
 operator|.
-name|test
+name|support
 operator|.
-name|junit4
-operator|.
-name|CamelTestSupport
+name|IntrospectionSupport
 import|;
 end_import
 
@@ -118,9 +116,11 @@ name|apache
 operator|.
 name|camel
 operator|.
-name|support
+name|test
 operator|.
-name|IntrospectionSupport
+name|junit4
+operator|.
+name|CamelTestSupport
 import|;
 end_import
 
