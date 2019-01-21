4e9d803979461ece24bc354c0e337caece282cc2
Andrea Cosentino <ancosen@gmail.com>
2019-01-21 08:28:15 +0100

    Fixed CS for Camel-jetty9
    Former-commit-id: 001a5bcbd32d839bb39b7a1ffd52156d55495b85

--- components/camel-jetty9/src/test/java/org/apache/camel/component/jetty/JettyHandle404Test.java
+++ components/camel-jetty9/src/test/java/org/apache/camel/component/jetty/JettyHandle404Test.java
@@ -26,6 +26,18 @@ name|apache
 operator|.
 name|camel
 operator|.
+name|AggregationStrategy
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|camel
+operator|.
 name|Exchange
 import|;
 end_import
@@ -85,18 +97,6 @@ operator|.
 name|common
 operator|.
 name|HttpOperationFailedException
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|apache
-operator|.
-name|camel
-operator|.
-name|AggregationStrategy
 import|;
 end_import
 
