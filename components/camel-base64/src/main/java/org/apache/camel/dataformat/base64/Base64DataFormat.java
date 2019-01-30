b3d2889ad00f5cb12aa64748091cd6747001373c
Andrea Cosentino <ancosen@gmail.com>
2019-01-30 10:18:45 +0100

    Fixed CS for Camel-Base64
    Former-commit-id: baa71832d4dcf604511f87b9a9c400bd10c99927

--- components/camel-base64/src/main/java/org/apache/camel/dataformat/base64/Base64DataFormat.java
+++ components/camel-base64/src/main/java/org/apache/camel/dataformat/base64/Base64DataFormat.java
@@ -104,9 +104,7 @@ name|camel
 operator|.
 name|support
 operator|.
-name|service
-operator|.
-name|ServiceSupport
+name|ExchangeHelper
 import|;
 end_import
 
@@ -120,7 +118,9 @@ name|camel
 operator|.
 name|support
 operator|.
-name|ExchangeHelper
+name|service
+operator|.
+name|ServiceSupport
 import|;
 end_import
 
