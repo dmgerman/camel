3e99be8ad98dbea0b9bea9fcd0e0d1c378c53f69
Guillaume Nodet <gnodet@gmail.com>
2019-01-31 10:12:38 +0100

    [CAMEL-13129] Move osgi out of camel-core
    Former-commit-id: e65d06954105e0b74b49735dc892739e1b10cfb1

--- components/camel-core-osgi/src/main/java/org/apache/camel/core/osgi/impl/Activator.java
+++ components/camel-core-osgi/src/main/java/org/apache/camel/core/osgi/impl/Activator.java
@@ -4,7 +4,7 @@ comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or mor
 end_comment
 
 begin_package
-DECL|package|org.apache.camel.impl.osgi
+DECL|package|org.apache.camel.core.osgi.impl
 package|package
 name|org
 operator|.
@@ -12,9 +12,11 @@ name|apache
 operator|.
 name|camel
 operator|.
-name|impl
+name|core
 operator|.
 name|osgi
+operator|.
+name|impl
 package|;
 end_package
 
