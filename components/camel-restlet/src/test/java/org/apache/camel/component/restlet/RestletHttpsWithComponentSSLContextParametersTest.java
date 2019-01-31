06e7c69d64151ac2e768b18c274dd942fcc3f832
Andrea Cosentino <ancosen@gmail.com>
2019-01-31 07:40:38 +0100

    CAMEL-12748 - Fixed test name and CS
    Former-commit-id: 1f27c2fef0786dd1bbccbe18a969d981bb0af1d1

--- components/camel-restlet/src/test/java/org/apache/camel/component/restlet/RestletHttpsWithComponentSSLContextParametersTest.java
+++ components/camel-restlet/src/test/java/org/apache/camel/component/restlet/RestletHttpsWithComponentSSLContextParametersTest.java
@@ -1,4 +1,8 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *<p>  * http://www.apache.org/licenses/LICENSE-2.0  *<p>  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+
 begin_package
 DECL|package|org.apache.camel.component.restlet
 package|package
@@ -13,6 +17,16 @@ operator|.
 name|restlet
 package|;
 end_package
+
+begin_import
+import|import
+name|java
+operator|.
+name|net
+operator|.
+name|URL
+import|;
+end_import
 
 begin_import
 import|import
@@ -152,25 +166,11 @@ name|Test
 import|;
 end_import
 
-begin_import
-import|import
-name|java
-operator|.
-name|net
-operator|.
-name|URL
-import|;
-end_import
-
-begin_comment
-comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *<p>  * http://www.apache.org/licenses/LICENSE-2.0  *<p>  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
-end_comment
-
 begin_class
-DECL|class|RestletHttpsWithComponentSSLContextParametrsTest
+DECL|class|RestletHttpsWithComponentSSLContextParametersTest
 specifier|public
 class|class
-name|RestletHttpsWithComponentSSLContextParametrsTest
+name|RestletHttpsWithComponentSSLContextParametersTest
 extends|extends
 name|RestletTestSupport
 block|{
@@ -394,7 +394,7 @@ parameter_list|()
 throws|throws
 name|Exception
 block|{
-comment|//test that ssl context set for component
+comment|// test that ssl context set for component
 name|postRequestMessage
 argument_list|(
 name|REQUEST_MESSAGE
@@ -415,7 +415,8 @@ parameter_list|()
 throws|throws
 name|Exception
 block|{
-comment|//test that ssl context set for component has bigger priority than global context
+comment|// test that ssl context set for component has bigger priority than
+comment|// global context
 name|postRequestMessage
 argument_list|(
 name|REQUEST_MESSAGE
