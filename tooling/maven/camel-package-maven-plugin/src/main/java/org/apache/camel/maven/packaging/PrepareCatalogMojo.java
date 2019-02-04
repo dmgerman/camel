a0dab5660d79d5610c74bb29619b3af8fe56199a
Guillaume Nodet <gnodet@gmail.com>
2019-02-04 16:09:15 +0100

    Fix package maven plugin for camel-core location
    Former-commit-id: 9b5ae46b597e61ea5047936910c7377392259f1e

--- tooling/maven/camel-package-maven-plugin/src/main/java/org/apache/camel/maven/packaging/PrepareCatalogMojo.java
+++ tooling/maven/camel-package-maven-plugin/src/main/java/org/apache/camel/maven/packaging/PrepareCatalogMojo.java
@@ -598,7 +598,7 @@ name|Parameter
 argument_list|(
 name|defaultValue
 operator|=
-literal|"${project.build.directory}/../../../camel-core"
+literal|"${project.build.directory}/../../../core/camel-core"
 argument_list|)
 DECL|field|coreDir
 specifier|protected
