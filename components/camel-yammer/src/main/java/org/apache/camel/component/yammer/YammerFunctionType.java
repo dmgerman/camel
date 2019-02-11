64c2c3b584a4e7a21f20e2683600a7e0cff4ddaf
Radovan Netuka <rnetuka@redhat.com>
2019-02-11 06:58:13 +0100

    [CAMEL-13176] camel yammer component is accessing non-existent page (#2754)
    Former-commit-id: 3af00d642326991063e3132b40e60ed2b6a49132

--- components/camel-yammer/src/main/java/org/apache/camel/component/yammer/YammerFunctionType.java
+++ components/camel-yammer/src/main/java/org/apache/camel/component/yammer/YammerFunctionType.java
@@ -50,9 +50,6 @@ DECL|enumConstant|CURRENT
 name|USERS
 block|,
 name|CURRENT
-block|,
-DECL|enumConstant|RELATIONSHIPS
-name|RELATIONSHIPS
 block|;
 DECL|method|fromUri (String uri)
 specifier|public
