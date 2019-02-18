fbdd3c99d0c9e01701508af30a1c51456af05776
Gregor Zurowski <gregor@zurowski.org>
2019-02-18 07:16:12 +0000

    [Minor] Remove unused imports
    Former-commit-id: 9f8135680113b331997df7d90aa7554260cb61cb

--- core/camel-api/src/main/java/org/apache/camel/AggregationStrategy.java
+++ core/camel-api/src/main/java/org/apache/camel/AggregationStrategy.java
@@ -20,16 +20,6 @@ name|org
 operator|.
 name|slf4j
 operator|.
-name|Logger
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|slf4j
-operator|.
 name|LoggerFactory
 import|;
 end_import
