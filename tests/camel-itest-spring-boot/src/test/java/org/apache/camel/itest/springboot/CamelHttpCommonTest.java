d7bfc1578bcf640b71b89a045eb5ef7d147f5bec
Andrea Cosentino <ancosen@gmail.com>
2019-03-19 08:59:38 +0100

    Fixed Camel-Http-Common Spring Boot Integration Test
    Former-commit-id: 0846b754d7e287c98b7b505712f8885c6cee70ac

--- tests/camel-itest-spring-boot/src/test/java/org/apache/camel/itest/springboot/CamelHttpCommonTest.java
+++ tests/camel-itest-spring-boot/src/test/java/org/apache/camel/itest/springboot/CamelHttpCommonTest.java
@@ -165,6 +165,11 @@ name|class
 argument_list|)
 argument_list|)
 operator|.
+name|dependency
+argument_list|(
+literal|"javax.servlet:javax.servlet-api"
+argument_list|)
+operator|.
 name|build
 argument_list|()
 return|;
