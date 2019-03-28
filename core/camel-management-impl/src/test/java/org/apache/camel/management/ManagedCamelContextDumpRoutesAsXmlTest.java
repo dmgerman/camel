8d77541308f8ac07343831b28539c7f836d216ff
Claus Ibsen <claus.ibsen@gmail.com>
2019-03-28 12:40:38 +0100

    CAMEL-13370: resolve delegating endpoint when routes are dumped
    Former-commit-id: b78a75d239ea34d294e15cfd23c13c48666e7cbc

--- core/camel-management-impl/src/test/java/org/apache/camel/management/ManagedCamelContextDumpRoutesAsXmlTest.java
+++ core/camel-management-impl/src/test/java/org/apache/camel/management/ManagedCamelContextDumpRoutesAsXmlTest.java
@@ -18,6 +18,16 @@ end_package
 
 begin_import
 import|import
+name|java
+operator|.
+name|util
+operator|.
+name|Properties
+import|;
+end_import
+
+begin_import
+import|import
 name|javax
 operator|.
 name|management
@@ -69,16 +79,6 @@ operator|.
 name|junit
 operator|.
 name|Test
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|util
-operator|.
-name|Properties
 import|;
 end_import
 
--- core/camel-management-impl/src/test/java/org/apache/camel/management/ManagedRouteDumpRouteAsXmlTest.java
+++ core/camel-management-impl/src/test/java/org/apache/camel/management/ManagedRouteDumpRouteAsXmlTest.java
@@ -671,15 +671,6 @@ argument_list|(
 name|json
 argument_list|)
 expr_stmt|;
-name|System
-operator|.
-name|out
-operator|.
-name|println
-argument_list|(
-name|json
-argument_list|)
-expr_stmt|;
 name|assertTrue
 argument_list|(
 name|json
