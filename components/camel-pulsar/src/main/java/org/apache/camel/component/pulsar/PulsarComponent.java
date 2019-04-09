3d7777e7d4a090dfffe148941d5952d12253c3c5
Andrea Cosentino <ancosen@gmail.com>
2019-04-09 16:22:38 +0200

    CAMEL-12665 - Ported the component to Camel 3
    Former-commit-id: 000775ffae617e61ee57901d26c8dfb607809ab6

--- components/camel-pulsar/src/main/java/org/apache/camel/component/pulsar/PulsarComponent.java
+++ components/camel-pulsar/src/main/java/org/apache/camel/component/pulsar/PulsarComponent.java
@@ -86,6 +86,22 @@ name|apache
 operator|.
 name|camel
 operator|.
+name|spi
+operator|.
+name|annotations
+operator|.
+name|Component
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|camel
+operator|.
 name|support
 operator|.
 name|DefaultComponent
@@ -119,6 +135,11 @@ import|;
 end_import
 
 begin_class
+annotation|@
+name|Component
+argument_list|(
+literal|"pulsar"
+argument_list|)
 DECL|class|PulsarComponent
 specifier|public
 class|class
