7576fa4d5045e16597cd63c62fec9e4e4f16e165
Claus Ibsen <claus.ibsen@gmail.com>
2019-04-25 12:23:52 +0200

    CAMEL-13449: camel3 - Move bean component out of camel-core
    Former-commit-id: f1a1dfe90af71f17625611f8518407d8b32ec878

--- components/camel-bean/src/main/java/org/apache/camel/component/beanclass/ClassEndpoint.java
+++ components/camel-bean/src/main/java/org/apache/camel/component/beanclass/ClassEndpoint.java
@@ -61,7 +61,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * The<a href="http://camel.apache.org/class.html">Class Component</a> is for invoking Java Classes (Java beans) from Camel.  */
+comment|/**  * The<a href="http://camel.apache.org/class.html">class component</a> is for invoking Java classes (Java beans) from Camel.  */
 end_comment
 
 begin_class
