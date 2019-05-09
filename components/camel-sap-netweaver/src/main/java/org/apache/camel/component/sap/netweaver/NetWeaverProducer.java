e8def01a7c744ac462130f99aa96a92c51939604
Andrea Cosentino <ancosen@gmail.com>
2019-05-09 11:52:02 +0200

    CAMEL-13494 - Camel-SAP-Netweaver: Move to Jackson 2.x
    Former-commit-id: 042a9ecfb621f2e1b6165d5fc1805676d68954fa

--- components/camel-sap-netweaver/src/main/java/org/apache/camel/component/sap/netweaver/NetWeaverProducer.java
+++ components/camel-sap-netweaver/src/main/java/org/apache/camel/component/sap/netweaver/NetWeaverProducer.java
@@ -32,6 +32,20 @@ end_import
 
 begin_import
 import|import
+name|com
+operator|.
+name|fasterxml
+operator|.
+name|jackson
+operator|.
+name|databind
+operator|.
+name|ObjectMapper
+import|;
+end_import
+
+begin_import
+import|import
 name|org
 operator|.
 name|apache
@@ -121,20 +135,6 @@ operator|.
 name|util
 operator|.
 name|URISupport
-import|;
-end_import
-
-begin_import
-import|import
-name|org
-operator|.
-name|codehaus
-operator|.
-name|jackson
-operator|.
-name|map
-operator|.
-name|ObjectMapper
 import|;
 end_import
 
