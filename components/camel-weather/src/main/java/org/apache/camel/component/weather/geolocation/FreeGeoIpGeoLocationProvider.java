b82ad44cb4c5a59876656f03a7b21aab8e57af9e
Andrea Cosentino <ancosen@gmail.com>
2019-05-09 08:18:41 +0200

    CAMEL-13492 - Camel-Weather: Move to Jackson 2.x
    Former-commit-id: 65d842b56119eb3b06ee151035a4023436955422

--- components/camel-weather/src/main/java/org/apache/camel/component/weather/geolocation/FreeGeoIpGeoLocationProvider.java
+++ components/camel-weather/src/main/java/org/apache/camel/component/weather/geolocation/FreeGeoIpGeoLocationProvider.java
@@ -96,11 +96,13 @@ end_import
 
 begin_import
 import|import
-name|org
+name|com
 operator|.
-name|codehaus
+name|fasterxml
 operator|.
 name|jackson
+operator|.
+name|databind
 operator|.
 name|JsonNode
 import|;
@@ -108,13 +110,13 @@ end_import
 
 begin_import
 import|import
-name|org
+name|com
 operator|.
-name|codehaus
+name|fasterxml
 operator|.
 name|jackson
 operator|.
-name|map
+name|databind
 operator|.
 name|ObjectMapper
 import|;
