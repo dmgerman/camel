1b3965fd47b828b00ba6f59f8912023ba5bd556e
Claus Ibsen <claus.ibsen@gmail.com>
2019-05-21 12:45:24 +0200

    [CAMEL-13371] Move languages to camel-base
    Former-commit-id: 06f5217126208f71bd40a202ccabd05fbb36586a

--- tooling/maven/camel-package-maven-plugin/src/main/java/org/apache/camel/maven/packaging/PrepareCatalogMojo.java
+++ tooling/maven/camel-package-maven-plugin/src/main/java/org/apache/camel/maven/packaging/PrepareCatalogMojo.java
@@ -602,7 +602,7 @@ specifier|protected
 name|File
 name|componentsDir
 decl_stmt|;
-comment|/**      * The camel-core directory where camel-core components are      */
+comment|/**      * The camel-core directory      */
 annotation|@
 name|Parameter
 argument_list|(
@@ -614,6 +614,19 @@ DECL|field|coreDir
 specifier|protected
 name|File
 name|coreDir
+decl_stmt|;
+comment|/**      * The camel-base directory      */
+annotation|@
+name|Parameter
+argument_list|(
+name|defaultValue
+operator|=
+literal|"${project.build.directory}/../../../core/camel-base"
+argument_list|)
+DECL|field|baseDir
+specifier|protected
+name|File
+name|baseDir
 decl_stmt|;
 comment|/**      * The directory where the camel-spring XML models are      */
 annotation|@
@@ -3988,11 +4001,11 @@ block|}
 block|}
 if|if
 condition|(
-name|coreDir
+name|baseDir
 operator|!=
 literal|null
 operator|&&
-name|coreDir
+name|baseDir
 operator|.
 name|isDirectory
 argument_list|()
@@ -4004,7 +4017,7 @@ init|=
 operator|new
 name|File
 argument_list|(
-name|coreDir
+name|baseDir
 argument_list|,
 literal|"target/classes"
 argument_list|)
@@ -4028,7 +4041,7 @@ operator|=
 operator|new
 name|File
 argument_list|(
-name|coreDir
+name|baseDir
 argument_list|,
 literal|"../camel-jaxp/target/classes"
 argument_list|)
