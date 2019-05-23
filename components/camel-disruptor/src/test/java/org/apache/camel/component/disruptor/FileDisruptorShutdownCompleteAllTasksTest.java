e4596f491de705086e944f2ee6b68fd2aab8df36
Andrea Cosentino <ancosen@gmail.com>
2019-05-23 11:37:28 +0200

    Camel-Disruptor: Fixed test
    Former-commit-id: d23c53545b112008e0cc3ccadb70adf69f0c6009

--- components/camel-disruptor/src/test/java/org/apache/camel/component/disruptor/FileDisruptorShutdownCompleteAllTasksTest.java
+++ components/camel-disruptor/src/test/java/org/apache/camel/component/disruptor/FileDisruptorShutdownCompleteAllTasksTest.java
@@ -361,18 +361,6 @@ argument_list|()
 argument_list|)
 expr_stmt|;
 block|}
-annotation|@
-name|Override
-DECL|method|isUseRouteBuilder ()
-specifier|public
-name|boolean
-name|isUseRouteBuilder
-parameter_list|()
-block|{
-return|return
-literal|false
-return|;
-block|}
 block|}
 end_class
 
