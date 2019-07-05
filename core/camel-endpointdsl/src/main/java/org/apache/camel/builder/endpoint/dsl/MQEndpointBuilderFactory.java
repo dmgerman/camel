3f531c632ae48f509f7102c91455cdbed39233bc
Andrea Cosentino <ancosen@gmail.com>
2019-07-05 12:50:23 +0200

    CAMEL-13723 - Final regen
    Former-commit-id: 60fdabe131313598d2f9ca0a37fd74de2aaba04f

--- core/camel-endpointdsl/src/main/java/org/apache/camel/builder/endpoint/dsl/MQEndpointBuilderFactory.java
+++ core/camel-endpointdsl/src/main/java/org/apache/camel/builder/endpoint/dsl/MQEndpointBuilderFactory.java
@@ -110,27 +110,6 @@ operator|)
 name|this
 return|;
 block|}
-comment|/**          * Logical name.          *           * The option is a:<code>java.lang.String</code> type.          *           * Group: producer          */
-DECL|method|label (String label)
-specifier|default
-name|MQEndpointBuilder
-name|label
-parameter_list|(
-name|String
-name|label
-parameter_list|)
-block|{
-name|setProperty
-argument_list|(
-literal|"label"
-argument_list|,
-name|label
-argument_list|)
-expr_stmt|;
-return|return
-name|this
-return|;
-block|}
 comment|/**          * The region in which MQ client needs to work. When using this          * parameter, the configuration will expect the capitalized name of the          * region (for example AP_EAST_1) You'll need to use the name          * Regions.EU_WEST_1.name().          *           * The option is a:<code>java.lang.String</code> type.          *           * Group: producer          */
 DECL|method|region (String region)
 specifier|default
