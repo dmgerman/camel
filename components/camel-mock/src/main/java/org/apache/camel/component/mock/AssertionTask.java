c94e20fcdabe76f6adf79642c02c9d7174e18ba5
Claus Ibsen <claus.ibsen@gmail.com>
2019-08-02 07:24:31 +0200

    CAMEL-12003: Add failFast option to mock endpoint
    Former-commit-id: d8627e2a9ad60131cc71753ef25ae77e5ae3f51a

---
+++ components/camel-mock/src/main/java/org/apache/camel/component/mock/AssertionTask.java
@@ -0,0 +1,48 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/*  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+
+begin_package
+DECL|package|org.apache.camel.component.mock
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|camel
+operator|.
+name|component
+operator|.
+name|mock
+package|;
+end_package
+
+begin_comment
+comment|/**  * Assertion task that supports fail fast mode by running the assertion asap on the n'th received message.  */
+end_comment
+
+begin_class
+DECL|class|AssertionTask
+specifier|public
+specifier|abstract
+class|class
+name|AssertionTask
+implements|implements
+name|Runnable
+block|{
+comment|/**      * Asserts on the n'th received message      *      * @param index the n'th received message      */
+DECL|method|assertOnIndex (int index)
+specifier|abstract
+name|void
+name|assertOnIndex
+parameter_list|(
+name|int
+name|index
+parameter_list|)
+function_decl|;
+block|}
+end_class
+
+end_unit
+
