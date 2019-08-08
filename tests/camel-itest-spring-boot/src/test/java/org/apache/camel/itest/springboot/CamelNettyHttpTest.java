f2ba497092f68f24c029a9dd00ccd9298a650bd7
Andrea Cosentino <ancosen@gmail.com>
2019-08-08 11:28:49 +0200

    CAMEL-13792 - Rename components to default names, Camel-netty4-http to camel-netty-http - SB integration test updated
    Former-commit-id: 0db4b35163a8a04e5e6181444d66082c15874400

--- tests/camel-itest-spring-boot/src/test/java/org/apache/camel/itest/springboot/CamelNettyHttpTest.java
+++ tests/camel-itest-spring-boot/src/test/java/org/apache/camel/itest/springboot/CamelNettyHttpTest.java
@@ -112,10 +112,10 @@ name|Arquillian
 operator|.
 name|class
 argument_list|)
-DECL|class|CamelNetty4HttpTest
+DECL|class|CamelNettyHttpTest
 specifier|public
 class|class
-name|CamelNetty4HttpTest
+name|CamelNettyHttpTest
 extends|extends
 name|AbstractSpringBootTestSupport
 block|{
@@ -159,7 +159,7 @@ name|module
 argument_list|(
 name|inferModuleName
 argument_list|(
-name|CamelNetty4HttpTest
+name|CamelNettyHttpTest
 operator|.
 name|class
 argument_list|)
