b20adf5921d5e57011d894ca0d7cdab75422287d
Andrea Cosentino <ancosen@gmail.com>
2019-11-05 12:47:34 +0100

    CAMEL-14141 - Camel-AWS: Add proxyProtocol as endpoint configuration for all the components - Lambda Component
    Former-commit-id: d512fcc035164ace1d14f3f22901a346fc30d41a

--- core/camel-endpointdsl/src/main/java/org/apache/camel/builder/endpoint/dsl/LambdaEndpointBuilderFactory.java
+++ core/camel-endpointdsl/src/main/java/org/apache/camel/builder/endpoint/dsl/LambdaEndpointBuilderFactory.java
@@ -194,6 +194,48 @@ return|return
 name|this
 return|;
 block|}
+comment|/**          * To define a proxy protocol when instantiating the Lambda client.          *           * The option is a:<code>com.amazonaws.Protocol</code> type.          *           * Group: producer          */
+DECL|method|proxyProtocol (Protocol proxyProtocol)
+specifier|default
+name|LambdaEndpointBuilder
+name|proxyProtocol
+parameter_list|(
+name|Protocol
+name|proxyProtocol
+parameter_list|)
+block|{
+name|doSetProperty
+argument_list|(
+literal|"proxyProtocol"
+argument_list|,
+name|proxyProtocol
+argument_list|)
+expr_stmt|;
+return|return
+name|this
+return|;
+block|}
+comment|/**          * To define a proxy protocol when instantiating the Lambda client.          *           * The option will be converted to a<code>com.amazonaws.Protocol</code>          * type.          *           * Group: producer          */
+DECL|method|proxyProtocol (String proxyProtocol)
+specifier|default
+name|LambdaEndpointBuilder
+name|proxyProtocol
+parameter_list|(
+name|String
+name|proxyProtocol
+parameter_list|)
+block|{
+name|doSetProperty
+argument_list|(
+literal|"proxyProtocol"
+argument_list|,
+name|proxyProtocol
+argument_list|)
+expr_stmt|;
+return|return
+name|this
+return|;
+block|}
 comment|/**          * Amazon AWS Region. When using this parameter, the configuration will          * expect the capitalized name of the region (for example AP_EAST_1)          * You'll need to use the name Regions.EU_WEST_1.name().          *           * The option is a:<code>java.lang.String</code> type.          *           * Group: producer          */
 DECL|method|region (String region)
 specifier|default
@@ -515,6 +557,17 @@ name|publishVersion
 block|,
 DECL|enumConstant|listVersions
 name|listVersions
+block|;     }
+comment|/**      * Proxy enum for<code>com.amazonaws.Protocol</code> enum.      */
+DECL|enum|Protocol
+enum|enum
+name|Protocol
+block|{
+DECL|enumConstant|http
+name|http
+block|,
+DECL|enumConstant|https
+name|https
 block|;     }
 comment|/**      * AWS Lambda (camel-aws-lambda)      * The aws-lambda is used for managing and invoking functions from Amazon      * Lambda.      *       * Category: cloud,computing,serverless      * Available as of version: 2.20      * Maven coordinates: org.apache.camel:camel-aws-lambda      *       * Syntax:<code>aws-lambda:function</code>      *       * Path parameter: function (required)      * Name of the Lambda function.      */
 DECL|method|awsLambda (String path)
