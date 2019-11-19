c580e257a81e047fea4ad237fbe51255aa0b6352
Andrea Cosentino <ancosen@gmail.com>
2019-11-19 09:17:33 +0100

    Fixed CS for camel-core-engine
    Former-commit-id: 7699c215c5e4cb3121a10ec4ce6389b0b22a0969

--- core/camel-core-engine/src/main/java/org/apache/camel/builder/AdviceWithRouteBuilder.java
+++ core/camel-core-engine/src/main/java/org/apache/camel/builder/AdviceWithRouteBuilder.java
@@ -241,6 +241,25 @@ name|logRouteAsXml
 init|=
 literal|true
 decl_stmt|;
+DECL|method|AdviceWithRouteBuilder ()
+specifier|public
+name|AdviceWithRouteBuilder
+parameter_list|()
+block|{     }
+DECL|method|AdviceWithRouteBuilder (CamelContext context)
+specifier|public
+name|AdviceWithRouteBuilder
+parameter_list|(
+name|CamelContext
+name|context
+parameter_list|)
+block|{
+name|super
+argument_list|(
+name|context
+argument_list|)
+expr_stmt|;
+block|}
 comment|/**      * Advices this route with the route builder using a lambda expression. It      * can be used as following:      *       *<pre>      * AdviceWithRouteBuilder.adviceWith(context, "myRoute", a ->      *     a.weaveAddLast().to("mock:result");      *</pre>      *<p/>      *<b>Important:</b> It is recommended to only advice a given route once      * (you can of course advice multiple routes). If you do it multiple times,      * then it may not work as expected, especially when any kind of error      * handling is involved. The Camel team plan for Camel 3.0 to support this      * as internal refactorings in the routing engine is needed to support this      * properly.      *<p/>      * The advice process will add the interceptors, on exceptions, on      * completions etc. configured from the route builder to this route.      *<p/>      * This is mostly used for testing purpose to add interceptors and the likes      * to an existing route.      *<p/>      * Will stop and remove the old route from camel context and add and start      * this new advised route.      *      * @param camelContext the camel context      * @param routeId either the route id as a string value, or<tt>null</tt> to      *            chose the 1st route, or you can specify a number for the n'th      *            route, or provide the route definition instance directly as well.      * @param builder the advice with route builder      * @return a new route which is this route merged with the route builder      * @throws Exception can be thrown from the route builder      */
 DECL|method|adviceWith (CamelContext camelContext, Object routeId, ThrowingConsumer<AdviceWithRouteBuilder, Exception> builder)
 specifier|public
@@ -486,25 +505,6 @@ block|}
 block|}
 argument_list|)
 return|;
-block|}
-DECL|method|AdviceWithRouteBuilder ()
-specifier|public
-name|AdviceWithRouteBuilder
-parameter_list|()
-block|{     }
-DECL|method|AdviceWithRouteBuilder (CamelContext context)
-specifier|public
-name|AdviceWithRouteBuilder
-parameter_list|(
-name|CamelContext
-name|context
-parameter_list|)
-block|{
-name|super
-argument_list|(
-name|context
-argument_list|)
-expr_stmt|;
 block|}
 comment|/**      * Sets the original route to be adviced.      *      * @param originalRoute the original route.      */
 DECL|method|setOriginalRoute (RouteDefinition originalRoute)
