dmgerman/camel

commit 4b7884ab50b6ffc28d2b8ab2d133a9ea19c94aac

Add an option to set the default application.properties location as weel as disabling it
Former-commit-id: d5020bea38833547c7a2c6634cd7232bc333917e

core/camel-main/src/main/java/org/apache/camel/main/BaseMainSupport.java
@@ -777,6 +777,13 @@ specifier|protected
 name|String
 name|propertyPlaceholderLocations
 decl_stmt|;
+DECL|field|defaultPropertyPlaceholderLocation
+specifier|protected
+name|String
+name|defaultPropertyPlaceholderLocation
+init|=
+literal|"classpath:application.properties;optional=true"
+decl_stmt|;
 DECL|field|initialProperties
 specifier|protected
 name|Properties
@@ -1607,6 +1614,33 @@ operator|.
 name|propertyPlaceholderLocations
 operator|=
 name|location
+expr_stmt|;
+block|}
+DECL|method|getDefaultPropertyPlaceholderLocation ()
+specifier|public
+name|String
+name|getDefaultPropertyPlaceholderLocation
+parameter_list|()
+block|{
+return|return
+name|defaultPropertyPlaceholderLocation
+return|;
+block|}
+comment|/**      * Set the default location for application properties if no locations have been set.      * If the value is set to "false" or empty, the default location is not taken into account.      *<p/>      * Default value is "classpath:application.properties;optional=true".      */
+DECL|method|setDefaultPropertyPlaceholderLocation (String defaultPropertyPlaceholderLocation)
+specifier|public
+name|void
+name|setDefaultPropertyPlaceholderLocation
+parameter_list|(
+name|String
+name|defaultPropertyPlaceholderLocation
+parameter_list|)
+block|{
+name|this
+operator|.
+name|defaultPropertyPlaceholderLocation
+operator|=
+name|defaultPropertyPlaceholderLocation
 expr_stmt|;
 block|}
 annotation|@
@@ -2558,10 +2592,29 @@ name|propertyPlaceholderLocations
 argument_list|)
 expr_stmt|;
 block|}
-else|else
+elseif|else
+if|if
+condition|(
+name|ObjectHelper
+operator|.
+name|isNotEmpty
+argument_list|(
+name|defaultPropertyPlaceholderLocation
+argument_list|)
+operator|&&
+operator|!
+name|ObjectHelper
+operator|.
+name|equal
+argument_list|(
+literal|"false"
+argument_list|,
+name|defaultPropertyPlaceholderLocation
+argument_list|)
+condition|)
 block|{
-comment|// lets default to application.properties and ignore if its missing
-comment|// if there are no existing locations configured
+comment|// lets default to defaultPropertyPlaceholderLocation if
+comment|// there are no existing locations configured
 name|PropertiesComponent
 name|pc
 init|=
@@ -2585,7 +2638,7 @@ name|pc
 operator|.
 name|addLocation
 argument_list|(
-literal|"classpath:application.properties;optional=true"
+name|defaultPropertyPlaceholderLocation
 argument_list|)
 expr_stmt|;
 block|}
@@ -2623,7 +2676,9 @@ name|LOG
 operator|.
 name|info
 argument_list|(
-literal|"Using properties from classpath:application.properties"
+literal|"Using properties from {}"
+argument_list|,
+name|defaultPropertyPlaceholderLocation
 argument_list|)
 expr_stmt|;
 block|}

core/camel-main/src/test/java/org/apache/camel/main/MainPropertyPlaceholderLocationsTest.java
@@ -1,0 +1,208 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/*  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+
+begin_package
+DECL|package|org.apache.camel.main
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|camel
+operator|.
+name|main
+package|;
+end_package
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
+name|Assert
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|junit
+operator|.
+name|Test
+import|;
+end_import
+
+begin_class
+DECL|class|MainPropertyPlaceholderLocationsTest
+specifier|public
+class|class
+name|MainPropertyPlaceholderLocationsTest
+extends|extends
+name|Assert
+block|{
+annotation|@
+name|Test
+argument_list|(
+name|expected
+operator|=
+name|IllegalArgumentException
+operator|.
+name|class
+argument_list|)
+DECL|method|testDefaultPropertyPlaceholderLocationDisabled ()
+specifier|public
+name|void
+name|testDefaultPropertyPlaceholderLocationDisabled
+parameter_list|()
+block|{
+name|Main
+name|main
+init|=
+operator|new
+name|Main
+argument_list|()
+decl_stmt|;
+try|try
+block|{
+name|main
+operator|.
+name|setDefaultPropertyPlaceholderLocation
+argument_list|(
+literal|"false"
+argument_list|)
+expr_stmt|;
+name|main
+operator|.
+name|start
+argument_list|()
+expr_stmt|;
+name|main
+operator|.
+name|getCamelContext
+argument_list|()
+operator|.
+name|resolvePropertyPlaceholders
+argument_list|(
+literal|"{{hello}}"
+argument_list|)
+expr_stmt|;
+block|}
+finally|finally
+block|{
+name|main
+operator|.
+name|stop
+argument_list|()
+expr_stmt|;
+block|}
+block|}
+annotation|@
+name|Test
+DECL|method|testDefaultPropertyPlaceholderLocationEnabled ()
+specifier|public
+name|void
+name|testDefaultPropertyPlaceholderLocationEnabled
+parameter_list|()
+block|{
+name|Main
+name|main
+init|=
+operator|new
+name|Main
+argument_list|()
+decl_stmt|;
+try|try
+block|{
+name|main
+operator|.
+name|start
+argument_list|()
+expr_stmt|;
+name|assertEquals
+argument_list|(
+literal|"World"
+argument_list|,
+name|main
+operator|.
+name|getCamelContext
+argument_list|()
+operator|.
+name|resolvePropertyPlaceholders
+argument_list|(
+literal|"{{hello}}"
+argument_list|)
+argument_list|)
+expr_stmt|;
+block|}
+finally|finally
+block|{
+name|main
+operator|.
+name|stop
+argument_list|()
+expr_stmt|;
+block|}
+block|}
+annotation|@
+name|Test
+DECL|method|testCustomPropertyPlaceholderLocationEnabled ()
+specifier|public
+name|void
+name|testCustomPropertyPlaceholderLocationEnabled
+parameter_list|()
+block|{
+name|Main
+name|main
+init|=
+operator|new
+name|Main
+argument_list|()
+decl_stmt|;
+try|try
+block|{
+name|main
+operator|.
+name|setDefaultPropertyPlaceholderLocation
+argument_list|(
+literal|"classpath:default.properties"
+argument_list|)
+expr_stmt|;
+name|main
+operator|.
+name|start
+argument_list|()
+expr_stmt|;
+name|assertEquals
+argument_list|(
+literal|"default"
+argument_list|,
+name|main
+operator|.
+name|getCamelContext
+argument_list|()
+operator|.
+name|resolvePropertyPlaceholders
+argument_list|(
+literal|"{{hello}}"
+argument_list|)
+argument_list|)
+expr_stmt|;
+block|}
+finally|finally
+block|{
+name|main
+operator|.
+name|stop
+argument_list|()
+expr_stmt|;
+block|}
+block|}
+block|}
+end_class
+
+end_unit
+

core/camel-main/src/test/resources/default.properties
@@ -1,0 +1,17 @@
+## ---------------------------------------------------------------------------
+## Licensed to the Apache Software Foundation (ASF) under one or more
+## contributor license agreements.  See the NOTICE file distributed with
+## this work for additional information regarding copyright ownership.
+## The ASF licenses this file to You under the Apache License, Version 2.0
+## (the "License"); you may not use this file except in compliance with
+## the License.  You may obtain a copy of the License at
+##
+##      http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+## ---------------------------------------------------------------------------
+hello=default

commit 27535ca3fa9c5ff33dacd80aac9ff2a36913be65

Fix regression caused by 4b7884ab50b [formerly d5020bea388]
Former-commit-id: dca5840eab4c6412eadd1268e73ff5a0c5d0feb5

core/camel-main/src/main/java/org/apache/camel/main/BaseMainSupport.java
@@ -2552,36 +2552,6 @@ argument_list|(
 name|propertyPlaceholderLocations
 argument_list|)
 expr_stmt|;
-if|if
-condition|(
-name|initialProperties
-operator|!=
-literal|null
-condition|)
-block|{
-name|pc
-operator|.
-name|setInitialProperties
-argument_list|(
-name|initialProperties
-argument_list|)
-expr_stmt|;
-block|}
-if|if
-condition|(
-name|overrideProperties
-operator|!=
-literal|null
-condition|)
-block|{
-name|pc
-operator|.
-name|setOverrideProperties
-argument_list|(
-name|overrideProperties
-argument_list|)
-expr_stmt|;
-block|}
 name|LOG
 operator|.
 name|info
@@ -2642,6 +2612,24 @@ name|defaultPropertyPlaceholderLocation
 argument_list|)
 expr_stmt|;
 block|}
+name|LOG
+operator|.
+name|info
+argument_list|(
+literal|"Using properties from {}"
+argument_list|,
+name|defaultPropertyPlaceholderLocation
+argument_list|)
+expr_stmt|;
+block|}
+name|PropertiesComponent
+name|pc
+init|=
+name|camelContext
+operator|.
+name|getPropertiesComponent
+argument_list|()
+decl_stmt|;
 if|if
 condition|(
 name|initialProperties
@@ -2669,16 +2657,6 @@ operator|.
 name|setOverrideProperties
 argument_list|(
 name|overrideProperties
-argument_list|)
-expr_stmt|;
-block|}
-name|LOG
-operator|.
-name|info
-argument_list|(
-literal|"Using properties from {}"
-argument_list|,
-name|defaultPropertyPlaceholderLocation
 argument_list|)
 expr_stmt|;
 block|}