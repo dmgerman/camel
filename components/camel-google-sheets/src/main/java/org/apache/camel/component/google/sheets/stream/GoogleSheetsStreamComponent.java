58ac3acd0a5f522c2564b3159608dd22538b78d3
Andrea Cosentino <ancosen@gmail.com>
2019-12-12 10:21:42 +0100

    CAMEL-14284: Configuring endpoint should set properties on endpoint and not configuration object - google-sheets-stream
    Former-commit-id: bd2b17da53ff6ae4aa0ee77e7e3976ad4a503d87

--- components/camel-google-sheets/src/main/java/org/apache/camel/component/google/sheets/stream/GoogleSheetsStreamComponent.java
+++ components/camel-google-sheets/src/main/java/org/apache/camel/component/google/sheets/stream/GoogleSheetsStreamComponent.java
@@ -432,13 +432,6 @@ operator|.
 name|copy
 argument_list|()
 decl_stmt|;
-name|setProperties
-argument_list|(
-name|configuration
-argument_list|,
-name|parameters
-argument_list|)
-expr_stmt|;
 name|GoogleSheetsStreamEndpoint
 name|endpoint
 init|=
