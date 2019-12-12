40fc5c3361404515b285da6501fff27de6bd40e1
Andrea Cosentino <ancosen@gmail.com>
2019-12-12 10:45:04 +0100

    CAMEL-14284: Configuring endpoint should set properties on endpoint and not configuration object - google-calendar-stream
    Former-commit-id: 97eef36a0a8064c551a7c2049d4893790a99f46e

--- components/camel-google-calendar/src/main/java/org/apache/camel/component/google/calendar/stream/GoogleCalendarStreamComponent.java
+++ components/camel-google-calendar/src/main/java/org/apache/camel/component/google/calendar/stream/GoogleCalendarStreamComponent.java
@@ -406,13 +406,6 @@ operator|.
 name|copy
 argument_list|()
 decl_stmt|;
-name|setProperties
-argument_list|(
-name|configuration
-argument_list|,
-name|parameters
-argument_list|)
-expr_stmt|;
 name|GoogleCalendarStreamEndpoint
 name|endpoint
 init|=
