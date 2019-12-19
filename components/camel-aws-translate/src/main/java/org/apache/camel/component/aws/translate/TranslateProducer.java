54e0acc73d6852e1d92d615584af252a53189f41
Andrea Cosentino <ancosen@gmail.com>
2019-12-19 14:42:47 +0100

    Camel-Aws-Translate: Fixed CS
    Former-commit-id: d9414fd336a77cdda42611cf6799b810719d8ae0

--- components/camel-aws-translate/src/main/java/org/apache/camel/component/aws/translate/TranslateProducer.java
+++ components/camel-aws-translate/src/main/java/org/apache/camel/component/aws/translate/TranslateProducer.java
@@ -22,6 +22,16 @@ end_package
 
 begin_import
 import|import
+name|java
+operator|.
+name|util
+operator|.
+name|Collection
+import|;
+end_import
+
+begin_import
+import|import
 name|com
 operator|.
 name|amazonaws
@@ -73,16 +83,6 @@ operator|.
 name|model
 operator|.
 name|TranslateTextResult
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|util
-operator|.
-name|Collection
 import|;
 end_import
 
